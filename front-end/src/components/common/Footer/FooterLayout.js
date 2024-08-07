f9dcc1226f13f37123ecdbab8eec1422a694ef4d
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-07 19:38:06 +0900

    Feat: Footer 레이아웃 작성

---
+++ front-end/src/components/common/Footer/FooterLayout.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+
+export const FooterWrapper = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 200px;
+`
+export const FooterMenuOuter = styled.div`
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #d9d9d9;
+  border-top: 1px solid #d9d9d9;
+`
+export const FooterMenuInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 60%;
+`
+export const FooterMenuLeftUl = styled.ul`
+  padding: 15px 20px;
+  width: 70%;
+  display: flex;
+  margin-right: 150px;
+  justify-content: space-between;
+`
+
+export const FooterMenuRightUl = styled.ul`
+  padding: 15px 20px;
+  width: 20%;
+  display: flex;
+  justify-content: space-between;
+`
+export const FooterInfoOuter = styled.div`
+  display: flex;
+  justify-content: center;`
+
+export const FooterInfoInner = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+`
+export const FooterAsideInfo = styled.div`
+  margin-top: 20px;
+`
