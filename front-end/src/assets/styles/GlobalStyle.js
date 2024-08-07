e696dcc4c6093758580c76026f25108f69eaada7
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-07 19:38:36 +0900

    Feat: 기본 css 초기화 코드 작성

--- front-end/src/assets/styles/GlobalStyle.js
+++ front-end/src/assets/styles/GlobalStyle.js
@@ -5,6 +5,14 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
   }
 
+  li {
+    list-style: none;
+  }
+
+  ul {
+    padding-inline-start: 0;
+  }
+
   #root {
     height: 100%;
   }
