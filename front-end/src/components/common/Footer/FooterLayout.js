954075a73351eebbed26584e1c93fbe854d079d4
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-08 16:22:23 +0900

    Feat: mixin 함수 사용으로 인한 코드 단축

--- front-end/src/components/common/Footer/FooterLayout.js
+++ front-end/src/components/common/Footer/FooterLayout.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {flexLayout} from "../../../assets/styles/Mixin,js";
 
 
 export const FooterWrapper = styled.footer`
@@ -8,38 +9,32 @@ export const FooterWrapper = styled.footer`
   height: 200px;
 `
 export const FooterMenuOuter = styled.div`
-  display: flex;
-  justify-content: center;
+  ${flexLayout("center")}
   border-bottom: 1px solid #d9d9d9;
   border-top: 1px solid #d9d9d9;
 `
 export const FooterMenuInner = styled.div`
-  display: flex;
-  justify-content: space-between;
+  ${flexLayout("center")}
   width: 60%;
 `
 export const FooterMenuLeftUl = styled.ul`
   padding: 15px 20px;
   width: 70%;
-  display: flex;
   margin-right: 150px;
-  justify-content: space-between;
+  ${flexLayout("space-between")}
 `
 
 export const FooterMenuRightUl = styled.ul`
   padding: 15px 20px;
   width: 20%;
-  display: flex;
-  justify-content: space-between;
+  ${flexLayout("space-between")}
 `
 export const FooterInfoOuter = styled.div`
-  display: flex;
-  justify-content: center;`
+  ${flexLayout("center")}`
 
 export const FooterInfoInner = styled.div`
   width: 50%;
-  display: flex;
-  justify-content: space-between;
+  ${flexLayout("space-between")}
 `
 export const FooterAsideInfo = styled.div`
   margin-top: 20px;
