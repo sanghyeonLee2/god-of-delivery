9815e8fd916ea9fa9c004ea88928ca0ca15b5d70
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-13 22:55:24 +0900

    Feat: 버튼 컴포넌트 onClick 이벤트 기능 추가

--- front-end/src/components/common/Button/Button.jsx
+++ front-end/src/components/common/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {ButtonLayout} from "./ButtonLayout";
 
-export function Button({type, text}) {
+export function Button({type, text, onClick}) {
     return (
-        <ButtonLayout type={type}>
+        <ButtonLayout type={type} onClick={onClick}>
             <span> {text}</span>
         </ButtonLayout>
     );
