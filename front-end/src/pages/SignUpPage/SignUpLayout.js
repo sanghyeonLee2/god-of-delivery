6a62353e9ca598a62eec16d1225ccffb1f61130f
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-13 23:07:31 +0900

    Design: 회원가입 페이지 스타일 적용

---
+++ front-end/src/pages/SignUpPage/SignUpLayout.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import {flexLayout} from "../../assets/styles/Mixin";
+
+export const SignUpOuter = styled.div`
+  height: 800px;
+  ${flexLayout("center")}
+`
+
+export const SignUpInner = styled.div`
+  margin-top: 70px;
+
+  h1 {
+    text-align: center;
+  }
+`
