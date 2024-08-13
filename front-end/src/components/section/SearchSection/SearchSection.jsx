8133903b63545b8784deea404bd296467ca1a440
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-13 20:13:16 +0900

    Feat: 검색창 섹션 컴포넌트 생성

---
+++ front-end/src/components/section/SearchSection/SearchSection.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+    HeaderMainText,
+    HeaderTextWrapper,
+    LocationBtn,
+    LocationBtnIcon,
+    ResetIcon,
+    SearchBoxInner,
+    SearchBoxMainText,
+    SearchBoxOuter,
+    SearchBoxTextOuter,
+    SearchForm,
+    SearchInput,
+    SearchInputWrapper
+} from "./SearchSectionLayout";
+import locationIcon from "../../../assets/img/my_location.png";
+import resetIcon from "../../../assets/img/reset.png";
+import {Button} from "../../common/Button/Button";
+
+function SearchSection(props) {
+    return (
+        <SearchBoxOuter>
+            <SearchBoxInner>
+                <SearchBoxTextOuter>
+                    <HeaderTextWrapper>
+                        <HeaderMainText>
+                            헤더 메인 텍스트 입니다
+                        </HeaderMainText>
+                        <SearchBoxMainText>
+                            배달 받으실 위치를 입력해 주세요
+                        </SearchBoxMainText>
+                    </HeaderTextWrapper>
+                </SearchBoxTextOuter>
+                <SearchInputWrapper>
+                    <LocationBtn type={"button"}>
+                        <LocationBtnIcon src={locationIcon} alt="구글 아이콘"/>
+                    </LocationBtn>
+                    <SearchForm>
+                        <SearchInput/>
+                        <ResetIcon src={resetIcon} alt="구글 아이콘"/>
+                        <Button type={"submit"} text={"검색"}/>
+                    </SearchForm>
+                </SearchInputWrapper>
+            </SearchBoxInner>
+        </SearchBoxOuter>
+    );
+}
+
+export default SearchSection;
