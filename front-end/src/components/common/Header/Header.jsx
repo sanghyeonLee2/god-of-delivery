782228d51ef66c96b4cfa4694cbbb30ec74492a3
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-13 23:02:20 +0900

    Feat: 홈화면 이동 라우트, 회원가입 페이지 이동 라우트 추가

--- front-end/src/components/common/Header/Header.jsx
+++ front-end/src/components/common/Header/Header.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import {HeaderInner, HeaderOuter, HeaderWrapper} from "./HeaderLayout";
+import {HeaderInner, HeaderOuter} from "./HeaderLayout";
 import {Button} from "../Button/Button";
 import {Logo} from "../../../assets/styles/CommonStyle";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import SearchSection from "../../section/SearchSection/SearchSection";
 
 export function Header(props) {
+    const navigate = useNavigate()
     return (
-        <HeaderWrapper>
+        <>
             <HeaderOuter>
                 <HeaderInner>
-                    <Logo>배달의 신</Logo>
-                    <Link to={"/sign-up"}>
-                        <Button type={"button"} text={"회원가입"}/>
+                    <Link to={"/"}>
+                        <Logo>배달의 신</Logo>
                     </Link>
+                    <Button type={"button"} text={"회원가입"} onClick={() => navigate("sign-up")}/>
                     <Button type={"button"} text={"로그인"}/>
                 </HeaderInner>
             </HeaderOuter>
             {window.location.pathname === "/" && <SearchSection/>}
-        </HeaderWrapper>
+        </>
     );
 }
