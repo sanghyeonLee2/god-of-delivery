da4dcfe3a8431677eee3f6512ac4d5c095c19277
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-19 20:58:44 +0900

    Feat: useNavigate 재사용 커스텀 훅 적용, 카테고리 onClick 이벤트 적용

--- front-end/src/pages/HomePage.js/HomePage.jsx
+++ front-end/src/pages/HomePage.js/HomePage.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {CategoryBox, CategoryImg, CategoryInner, CategoryOuter, CategoryText} from "./HomePageLayout";
 import categoryDummy from "../../assets/data/categoryDummy.json"
 import testImg from "../../assets/img/category_text_img.png"
+import {useMove} from "../../hooks/useMove";
 
 function HomePage(props) {
+    const navigate = useMove()
     return (
         <CategoryOuter>
             <CategoryInner>
                 {categoryDummy.map((ele) =>
-                    <CategoryBox key={ele.id}>
+                    <CategoryBox key={ele.id} onClick={() => navigate(`category-info/${ele.id}`)}>
                         <CategoryText>{ele.text}</CategoryText>
                         <CategoryImg src={testImg} alt={"img"} width={160}/>
                     </CategoryBox>)}
