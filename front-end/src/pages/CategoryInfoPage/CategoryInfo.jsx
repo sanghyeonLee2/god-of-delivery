ee3b140483bfd040d14716200805dbbee6f6e527
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-19 20:54:33 +0900

    Feat: 카테고리 메뉴 컴포넌트 생성

---
+++ front-end/src/pages/CategoryInfoPage/CategoryInfo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CategoryList from "../../components/section/CategoryList/CategoryList";
+
+function CategoryInfo(props) {
+    return (
+        <CategoryList/>
+    );
+}
+
+export default CategoryInfo;
