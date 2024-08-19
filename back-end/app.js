2d994095cefe887b78af64a7ce5cbc9e83fd963c
yuseong <dbtjd7282@gmail.com>
2024-08-19 21:49:31 +0900

    feat : module 연결, 에러 처리 미들웨어 설정

--- back-end/app.js
+++ back-end/app.js
@@ -1,41 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config();
+const app = express();
+app.set('port', process.env.PORT || 3000);
 
-var app = express();
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie'
+}))
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.send("접속 성공");
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((req,res,next)=>{
+    const err = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    err.status = 404;
+    next(err)
+})
+app.use((err,req,res,next)=>{
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.render('error');
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(app.get('port'), () => {
+   console.log(`${app.get('port')}번 포트에서 대기 중`);
+})
