011a5dfd255a8124aed8b0e030a056cab736c45b
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-20 16:58:06 +0900

    Feat: 카테고리

--- front-end/src/pages/CategoryInfoPage/CategoryInfo.jsx
+++ front-end/src/pages/CategoryInfoPage/CategoryInfo.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import CategoryList from "../../components/section/CategoryList/CategoryList";
+import {useLocation} from "react-router-dom";
+import SortingSection from "../../components/section/SortingSection/SortingSection";
+import RestaurantList from "../../components/section/RestaurantList/RestaurantList";
+import {RestaurantsWrapper} from "./CategoryInfoLayout";
 
 function CategoryInfo(props) {
+    const {
+        state: {
+            categoryId
+        }
+    } = useLocation()
     return (
-        <CategoryList/>
+        <>
+            <CategoryList categoryId={categoryId}/>
+            <SortingSection/>
+            <RestaurantsWrapper>
+                <RestaurantList listType={"추천 맛집"} categoryId={categoryId}/>
+                <RestaurantList listType={"등록 음식점"} categoryId={categoryId}/>
+            </RestaurantsWrapper>
+        </>
     );
 }
 
