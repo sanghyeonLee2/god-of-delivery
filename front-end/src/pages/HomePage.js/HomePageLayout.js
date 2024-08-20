9df1a129b96228b90cd9e22b6e5d59ac8ca9027f
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-21 00:45:33 +0900

    Design: 카테고리 목록 반응형 레이아웃으로 수정 최대 넓이 % -> px 고정 및 크기 조정

--- front-end/src/pages/HomePage.js/HomePageLayout.js
+++ front-end/src/pages/HomePage.js/HomePageLayout.js
@@ -2,21 +2,22 @@ import styled from "styled-components";
 import {elementSize, flexLayout} from "../../assets/styles/Mixin";
 
 export const CategoryOuter = styled.main`
-  margin-top: 20px;
-  ${elementSize("100%", "1280px;")}
+  margin-top: 18px;
+  min-height: 1190px;
 `
 export const CategoryInner = styled.div`
   margin: 0 auto;
-  max-width: 60%;
+  max-width: 1180px;
   height: 100%;
   ${flexLayout("space-between")};
   flex-wrap: wrap;
 `
 export const CategoryBox = styled.div`
-  ${elementSize("300px", "300px")}
+  ${elementSize("280px", "280px")}
   border: lightgray 1px solid;
   position: relative;
   cursor: pointer;
+  margin-bottom: 18px;
 `
 export const CategoryText = styled.p`
   margin-top: 25px;
@@ -24,6 +25,6 @@ export const CategoryText = styled.p`
 `
 export const CategoryImg = styled.img`
   position: absolute;
-  left: 130px;
-  top: 130px;
+  left: 106px;
+  top: 108px;
 `
