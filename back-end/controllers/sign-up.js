dd83e3b9d7600f366eac6292187b618298fc6bca
yuseong <dbtjd7282@gmail.com>
2024-08-22 20:35:42 +0900

    Feat : /check-id 라우터 생성, POST 라우터 : 회원가입 데이터 DB 추가

--- back-end/controllers/sign-up.js
+++ back-end/controllers/sign-up.js
@@ -3,28 +3,27 @@ const User = require('../models/user');
 
 const router = express.Router();
 
-router.route('/sign-up')
-    .post(async (req, res) => {
+router.get('/check-id', async (req, res) => {
+    // console.log(req.query.userId);
     try{
         const tempUser = await User.findOne({
             where:{
-                userId: req.body.userId
+                userId: req.query.userId
             }
         })
         if(tempUser){
-            res.status(500).send({
-                status:500,
-                message: "중복된 아이디입니다."
+            res.status(401).send({
+                available: false,
+                message: "중복된 ID 입니다. 다른 ID를 입력해주세요."
             })
         }
         else{
-            const user = await User.create({
-                userId: req.body.userId,
-                name: req.body.userName,
-                password: req.body.userPw,
-                phone: req.body.userPhoneNum,
+            res.status(200).send({
+                available: true,
+                message: "사용 가능한 ID 입니다."
             })
         }
+
     }
     catch(err){
         res.status(500).send({
@@ -33,3 +32,38 @@ router.route('/sign-up')
         })
     }
 })
+
+router.post('/', async (req, res) => {
+    const date = new Date();
+    try{
+        await User.create({
+            userId: req.body.userId,
+            name: req.body.userName,
+            password: req.body.userPw,
+            phone: req.body.userPhoneNum,
+            currentAddress:null,
+            createdDate: date,
+            modifiedDate: date,
+        })
+        const user = await User.findOne({
+            where:{
+                userId: req.body.userId,
+            },
+            attributes:['userId','password','name','phone','currentAddress','grade','role'],
+        })
+        res.status(201).send({
+            status: 201,
+            message: "Success",
+            data: user
+        })
+    }
+    catch (err){
+        res.status(500).send({
+            status:500,
+            message: err.message
+        })
+    }
+
+})
+
+module.exports = router;
