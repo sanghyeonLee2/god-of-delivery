714d7333299057032056af1af639ae916b74ff56
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-23 19:51:28 +0900

    Feat: api 요청 코드 분리

--- front-end/src/apis/api/user.js
+++ front-end/src/apis/api/user.js
@@ -1,9 +1,31 @@
-import {instance} from "../utils/instance";
+import {authInstance, instance} from "../utils/instance";
 
 export const getUserApi = async (url) => {
-    return await instance.get(url)
+    console.log(url)
+    try {
+        console.log("Asd")
+        const res = await instance.get(url)
+        console.log(res)
+        if (res.data.status === 200) {
+            console.log(res.data)
+        }
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 export const postUserApi = async (url) => {
-    return await instance.post(url)
+    try {
+        return await authInstance.post(url)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const postReissue = async (url) => {
+    try {
+        return await authInstance.post(url)
+    } catch (err) {
+        console.log(err)
+    }
 }
