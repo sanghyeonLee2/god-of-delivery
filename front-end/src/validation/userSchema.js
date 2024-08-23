79117e28f009d2a00b83b3e64422af0b5ba5cd89
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-23 19:50:45 +0900

    Refactor: 리액트 훅 폼 onblur 모드 변경으로 인한 아이디 중복확인 직접 요청 방식으로 수정

--- front-end/src/validation/userSchema.js
+++ front-end/src/validation/userSchema.js
@@ -1,6 +1,7 @@
 import * as yup from "yup"
+import {getUserApi} from "../apis/api/user";
 
-export const userAuthValid = yup.object({
+export const signUpValid = yup.object({
     nickname: yup.string()
         .required("이름을 입력해주세요.")
         .min(6, "6자 이상 입력해주세요.")
@@ -9,12 +10,13 @@ export const userAuthValid = yup.object({
         .required("아이디을 입력해주세요.")
         .min(2, "2자 이상 입력해주세요.")
         .max(8, "8자 이하 입력해주세요.")
-        .test("requiredDuplication", "아이디 중복확인이 필요합니다.",
-            (value, context) => {
-                console.log(value, "l", context.options.context.status)
-                const {status} = context.options.context;
-                return status === "success"; // test : 리턴 값이 불리언
-            }),
+        .test("requiredCheck", "아이디가 이미 존재합니다",
+            ((value) => {
+                if (value) {
+                    return getUserApi(`/auth/sign-up/check-id/123`)
+                }
+            }))
+    ,
     password: yup.string()
         .required("비밀번호를 입력해주세요.")
         .min(8, "8자 이상 입력해주세요.")
@@ -25,3 +27,15 @@ export const userAuthValid = yup.object({
     phoneNum: yup.string()
         .required("휴대폰 번호를 입력해주세요.")
 })
+
+export const signInValid = yup.object({
+        userId: yup.string()
+            .required("아이디을 입력해주세요.")
+            .min(2, "2자 이상 입력해주세요.")
+            .max(8, "8자 이하 입력해주세요."),
+        password: yup.string()
+            .required("비밀번호를 입력해주세요.")
+            .min(8, "8자 이상 입력해주세요.")
+            .max(20, "20자 이하 입력해주세요."),
+    }
+)
