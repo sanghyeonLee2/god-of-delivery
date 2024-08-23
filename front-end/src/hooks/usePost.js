c791dbd81837145ef5c4754de344182b1e49b22e
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-23 19:52:20 +0900

    Feat: 로그인 성공에 대한 응답 토큰 로컬스토리지 저장 코드 추가

--- front-end/src/hooks/usePost.js
+++ front-end/src/hooks/usePost.js
@@ -1,17 +1,26 @@
 import {useMutation} from "react-query"
 import {useNavigate} from "react-router-dom";
 import {postUserApi} from "../apis/api/user";
+import {useSetRecoilState} from "recoil";
+import {isSigInState} from "../recoil/user/atoms";
 
 export const usePost = (url) => {
+    const setSignIn = useSetRecoilState(isSigInState)
     const navigate = useNavigate()
     return useMutation(
         () => postUserApi(url), {
-            onSuccess: () => {
+            onSuccess: (key, value) => {
                 console.log("성공")
                 switch (url) {
-                    case "/posts":
+                    case "/signup":
                         navigate("/sign-in")
                         alert("회원가입 성공")
+                        break
+                    case "/sign-in":
+                        setSignIn(true)
+                        localStorage.setItem("access-token", value)
+                        localStorage.setItem("refresh-token", value)
+                        navigate("/")
                         break
                     default:
                         alert("알 수 없는 오류")
