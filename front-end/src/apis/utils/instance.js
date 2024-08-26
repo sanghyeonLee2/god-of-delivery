ce4a11dc8902d6cb2bca161da399eee6f2d64fb5
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-26 20:58:38 +0900

    Refactor: 재발급 api axios interceptors 로직 config 에 반환 값 추가

--- front-end/src/apis/utils/instance.js
+++ front-end/src/apis/utils/instance.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import {postReissue} from "../api/user";
-import {useNavigate} from "react-router-dom";
 
 //const API_KEY = process.env.REACT_APP_API
 const API_KEY = "https://44dc058b-cade-4265-891c-9cfed7476d1b.mock.pstmn.io"
@@ -22,19 +21,20 @@ export const authInstance = axios.create({
 })
 
 
-const useInterceptor = () => {
-    const navigate = useNavigate()
-    authInstance.interceptors.response.use(
-        async (config) => {
-            if (config.data.status === 200) {
-                return config
-            } else if (config.data.status === 401) {
-                const result = await postReissue("reissue")
-            } else {
-                localStorage.clear()
-                navigate("/")
-                alert("토큰 만료 인해 로그아웃 되었습니다.")
-            }
+authInstance.interceptors.response.use(
+    async (config) => {
+        if (config.data.status === 200) {
+            return config
+        } else if (config.data.status === 401) {
+            const result = await postReissue("/auth/sign-up/reissue")
+            /*localStorage.setItem("access-token", result.data.accessToken)
+               localStorage.setItem("refresh-token", result.data.refreshToken)*/
+            config.data.accessToken = result.data.accessToken
+            config.data.refreshToken = result.data.refreshToken
+            return config
+        } else {
+            localStorage.clear()
+            alert("토큰 만료 인해 로그아웃 되었습니다.")
+        }
+    })
 
-        })
-}
