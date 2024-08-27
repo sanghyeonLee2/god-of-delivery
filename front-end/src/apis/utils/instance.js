8c7dce319802b3c2010f01289e727d15b29132db
sanghyeonLee2 <hyeon012366@gmail.com>
2024-08-27 22:26:57 +0900

    refactor: 엑세스 토큰 재발급 인터셉터 로직 401 응답일 경우 err 일때 발동 되도록 수정

--- front-end/src/apis/utils/instance.js
+++ front-end/src/apis/utils/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import {postReissue} from "../api/user";
 
-//const API_KEY = process.env.REACT_APP_API
-const API_KEY = "https://44dc058b-cade-4265-891c-9cfed7476d1b.mock.pstmn.io"
+const API_KEY = process.env.REACT_APP_API
+//const API_KEY = "https://44dc058b-cade-4265-891c-9cfed7476d1b.mock.pstmn.io" // 테스트 용
 
 
 export const instance = axios.create({
@@ -22,16 +22,20 @@ export const authInstance = axios.create({
 
 
 authInstance.interceptors.response.use(
-    async (config) => {
-        if (config.data.status === 200) {
-            return config
-        } else if (config.data.status === 401) {
-            const result = await postReissue("/auth/sign-up/reissue")
+    async (response) => {
+        if (response.data.status === 200) {
+            return response
+        }
+    }, async (err) => {
+        if (err.response.status === 401) {
+            const result = await postReissue("/auth/sign-in/reissue")
             /*localStorage.setItem("access-token", result.data.accessToken)
-               localStorage.setItem("refresh-token", result.data.refreshToken)*/
-            config.data.accessToken = result.data.accessToken
-            config.data.refreshToken = result.data.refreshToken
-            return config
+              localStorage.setItem("refresh-token", result.data.refreshToken)*/
+            /*         err.response.accessToken = result.data.accessToken
+                     err.response.refreshToken = result.data.refreshToken*/
+            return Promise.resolve(
+                {"accessToken": result.data.accessToken, "refreshToken": result.data.refreshToken}
+            )
         } else {
             localStorage.clear()
             alert("토큰 만료 인해 로그아웃 되었습니다.")
