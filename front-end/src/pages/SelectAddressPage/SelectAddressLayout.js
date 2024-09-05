d3d45a30b74844d3354c6b5a15922a6513dee471
sanghyeonLee <hyeon012366@gmail.com>
2024-09-05 15:40:47 +0900

    Design: 주소 설정 페이지 컴포넌트 스타일 코드 추가

---
+++ front-end/src/pages/SelectAddressPage/SelectAddressLayout.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SelectLocationOuter = styled.div`
+  min-height: 1190px;
+  background-color: brown;
+`
+export const SelectLocationInner = styled.div`
+  margin: 0 auto;
+  height: 100%;
+  max-width: 580px;
+`
