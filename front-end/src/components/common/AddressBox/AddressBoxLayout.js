92bfde8d4a1801ec7f946e73bc4e928e4ac5e3a5
sanghyeonLee <hyeon012366@gmail.com>
2024-09-05 15:18:43 +0900

    Design: 고객 주소 컴포넌트 스타일 작성

---
+++ front-end/src/components/common/AddressBox/AddressBoxLayout.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import {elementSize} from "../../../assets/styles/Mixin";
+
+export const AddressBoxOuter = styled.div`
+  margin-top: 20px;
+  ${elementSize("100%", "150px")};
+  background-color: blue;
+`
