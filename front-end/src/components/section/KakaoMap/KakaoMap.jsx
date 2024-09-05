0638b9325ea60847e27b595f4887590ad216bd8c
sanghyeonLee <hyeon012366@gmail.com>
2024-09-05 15:32:06 +0900

    Feat: 카카오 맵 지도 화면 컴포넌트 추가

---
+++ front-end/src/components/section/KakaoMap/KakaoMap.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Map, MapMarker} from "react-kakao-maps-sdk";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {addressState, coordsState} from "../../../recoil/map/atoms";
+
+function KakaoMap() {
+    const address = useRecoilValue(addressState)
+    const [coords, setCoords] = useRecoilState(coordsState)
+    console.log(address)
+    return (
+        <Map
+            center={coords.center}
+            style={{width: '80%', height: '60%', margin: "0 auto"}}
+            level={3}
+            onClick={(_, e) => {
+                const latLng = e.latLng
+                setCoords(
+                    {
+                        center: {
+                            lat: latLng.getLat(),
+                            lng: latLng.getLng()
+                        }, isPanto: false
+                    }
+                )
+            }
+            }>
+            <MapMarker position={coords.center}/>
+        </Map>
+    );
+}
+
+export default KakaoMap;
