476498293440cd3cdb544a950dad29418814a2b3
sanghyeonLee <hyeon012366@gmail.com>
2024-09-05 15:28:26 +0900

    Design: 아이콘 버튼 컴포넌트 스타일 코드 작성

---
+++ front-end/src/components/common/Button/icon/IconButtonLayout.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const IconButtonWrapper = styled.button`
+  width: 40px;
+  height: 40px;
+  border: 0;
+  padding: 3px;
+  margin: 3px 8px;
+  background-color: transparent;
+  float: right;
+`
