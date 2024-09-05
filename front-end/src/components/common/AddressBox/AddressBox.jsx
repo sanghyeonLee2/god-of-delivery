3328ac4ab3939b64194d495e36d59290e5e30bd6
sanghyeonLee <hyeon012366@gmail.com>
2024-09-05 15:18:01 +0900

    Feat: 고객 저장된 주소 컴포넌트 추가

---
+++ front-end/src/components/common/AddressBox/AddressBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {AddressBoxOuter} from "./AddressBoxLayout";
+
+
+function AddressBox(props) {
+    return (
+        <AddressBoxOuter>
+
+        </AddressBoxOuter>
+    );
+}
+
+export default AddressBox;
