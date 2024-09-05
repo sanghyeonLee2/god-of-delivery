ebc21811b50e4b8d894bd7fdbc8bab5918c1e1d3
sanghyeonLee <hyeon012366@gmail.com>
2024-09-05 15:37:09 +0900

    Feat: 검색 폼 컴포넌트 스타일 코드 추가

---
+++ front-end/src/components/forms/SearchForm/SearchFormLayout.js
@@ -0,0 +1,44 @@
+import {elementSize, flexLayout} from "../../../assets/styles/Mixin";
+import styled from "styled-components";
+
+export const SearchInputForm = styled.form`
+  position: relative;
+  ${flexLayout("center")};
+  width: 500px;
+
+  input {
+    border-bottom-right-radius: 0;
+    border-top-right-radius: 0;
+    border: 0;
+    ${elementSize("86%", "100%")};
+  }
+
+  button {
+    border-bottom-left-radius: 0;
+    border-top-left-radius: 0;
+    ${elementSize("14%", "100%")};
+  }
+
+  img {
+    position: absolute;
+    left: 390px;
+    top: 10px;
+    cursor: pointer;
+    ${elementSize("31px", "31px")}
+  }
+`
+export const SearchInputWrapper = styled.div`
+  margin-top: 33px;
+  height: 50px;
+  ${flexLayout("space-between")}
+`
+
+export const LocationBtn = styled.button`
+  background-color: white;
+  width: 11%;
+
+  img {
+    ${elementSize("33px", "33px")}
+  }
+`
+
