e902645e48968bfd61d9da0785d96b5f205edd55
sanghyeonLee <hyeon012366@gmail.com>
2024-09-05 15:34:00 +0900

    Feat: 메인 버튼 컴포넌트 스타일 추가

---
+++ front-end/src/components/common/Button/main/MainButtonLayout.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import {elementSize} from "../../../../assets/styles/Mixin";
+
+export const MainButtonLayout = styled.button`
+  ${elementSize("130px", "45px")}
+  background-color: #FF4F6D;
+  border-style: none;
+  border-radius: 4px;
+`
