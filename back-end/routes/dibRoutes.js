38ea8d6dd6713996549cd7bbf0cd9bd0cfc570f6
yuseong <dbtjd7282@gmail.com>
2024-09-06 13:57:11 +0900

    Feat: authMiddleWare의 verifyToken 추가

--- back-end/routes/dibRoutes.js
+++ back-end/routes/dibRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {getDibStoreList, postDibFilled, postDibCancel} = require("../controllers/dibController");
+const {verifyToken} = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 //찜 목록 가져오기
-router.get('/', getDibStoreList)
+router.get('/',verifyToken, getDibStoreList)
 //찜 하기
 router.post('/', postDibFilled)
-router.post('/cancel', postDibCancel)
+router.post('/cancel',verifyToken, postDibCancel)
 
 module.exports = router
