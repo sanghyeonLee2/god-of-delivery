4d07dcf25f59b4f526f838610aa591c66ba90cdb
sanghyeonLee <hyeon012366@gmail.com>
2024-09-08 19:40:41 +0900

    Feat: 고객 주소 저장시 부가 정보 컴포넌트 추가

---
+++ front-end/src/components/section/AdditionalAddressInfo/AdditionalAddress.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {AdditionalAddressWrapper} from "./AdditionalAddressLayout";
+import MainInput from "../../common/Input/MainInput/MainInput";
+import Select from "../../common/Select/Select";
+import Option from "../../common/Select/options/Option";
+
+function AdditionalAddress() {
+    return (
+        <AdditionalAddressWrapper>
+            <p>라이더님께</p>
+            <Select>
+                <Option text={"문 앞에 두고 벨 눌러주세요."}/>
+                <Option text={"문 앞에 두고 노크 해주세요."}/>
+                <Option text={"문 앞에 두면 가져갈께요 (벨X,노크X)"}/>
+                <Option text={"직접 받을게요"}/>
+                <Option text={"전화주시면 마중 나갈게요"}/>
+                <Option text={"직접 입력"}/>
+            </Select>
+            <p>길 안내</p>
+            <MainInput reigterName={"roadInfo"}/>
+        </AdditionalAddressWrapper>
+    );
+}
+
+export default AdditionalAddress;
