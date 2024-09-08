492d29d9f73db32b6697131dd43f962e4be51c16
sanghyeonLee <hyeon012366@gmail.com>
2024-09-08 19:53:33 +0900

    Refactor: 헤더에 토큰 포함을 위한 api 요청 instance 분리

--- front-end/src/hooks/usePost.js
+++ front-end/src/hooks/usePost.js
@@ -1,6 +1,6 @@
 import {useMutation} from "react-query"
 import {useNavigate} from "react-router-dom";
-import {postUserApi} from "../apis/api/user";
+import {postAuthUserApi, postUserApi} from "../apis/api/user";
 import {useSetRecoilState} from "recoil";
 import {isSignInState, userInfoState} from "../recoil/user/atoms";
 
@@ -23,6 +23,31 @@ export const usePost = (url) => {
                         localStorage.setItem("access-token", key.data.accessToken)
                         localStorage.setItem("refresh-token", key.data.refreshToken)
                         navigate("/")
+                        break
+                    case "customer/register":
+                        alert("성공")
+                        break
+                    default:
+                        alert("알 수 없는 오류")
+                        break
+                }
+            },
+            onError: () => {
+                console.log("실패")
+            },
+            onSettled: () => {
+                console.log("결과에 상관없이 실행")
+            }
+        })
+}
+
+export const useAuthPost = (url) => {
+    return useMutation(
+        () => postAuthUserApi(url), {
+            onSuccess: (key) => {
+                switch (url) {
+                    case "customer/address":
+                        alert("성공")
                         break
                     default:
                         alert("알 수 없는 오류")
