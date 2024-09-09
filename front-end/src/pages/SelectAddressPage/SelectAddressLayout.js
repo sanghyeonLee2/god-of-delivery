92918dacb9d88e475fab067cbaa13add978d247d
sanghyeonLee <hyeon012366@gmail.com>
2024-09-09 18:09:41 +0900

    Design: 직관성을 위한 배경색 제거

--- front-end/src/pages/SelectAddressPage/SelectAddressLayout.js
+++ front-end/src/pages/SelectAddressPage/SelectAddressLayout.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const SelectLocationOuter = styled.div`
   min-height: 1190px;
-  background-color: brown;
 `
 export const SelectLocationInner = styled.div`
   margin: 0 auto;
