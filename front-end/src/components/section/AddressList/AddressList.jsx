418679d67614f693bd495deb4fc68b73ac99b9a2
sanghyeonLee <hyeon012366@gmail.com>
2024-09-10 21:06:43 +0900

    Feat: 사용자가 선택 가능한 주소 리스트 useQuery 로 불러와서 데이터 시각화

--- front-end/src/components/section/AddressList/AddressList.jsx
+++ front-end/src/components/section/AddressList/AddressList.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {AddressListOuter} from "./AddressListLayout";
 import AddressBox from "../../common/AddressBox/AddressBox";
+import useGet from "../../../hooks/useGet";
+import Loading from "../../common/Loading/Loading";
+import {useRecoilValue} from "recoil";
+import {userInfoState} from "../../../recoil/user/atoms";
 
 function AddressList(props) {
+    const userInfo = useRecoilValue(userInfoState)
+    const [data, isError, status, isLoading] = useGet(`customer/${userInfo.userId}/address`)
+    //const {mutate: onSignIn} = usePost("auth/sign-in")
+    if (isLoading)
+        return <Loading/>
     return (
         <AddressListOuter>
-            <AddressBox/>
+            {data.data?.map((e) =>
+                <AddressBox addressInfo={e} key={e.id}/>
+            )}
         </AddressListOuter>
     );
 }
