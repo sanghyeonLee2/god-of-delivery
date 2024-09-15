c98ce4fc47623536e9b738fce744a5510b218a94
sanghyeonLee <hyeon012366@gmail.com>
2024-09-15 19:42:13 +0900

    Feat: props 로 받은 현재 주소 화면에 띄우는 기능 추가

--- front-end/src/components/common/Header/Header.jsx
+++ front-end/src/components/common/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {HeaderInner, HeaderOuter} from "./HeaderLayout";
 import {MainButton} from "../Button/main/MainButton";
-import {Logo} from "../../../assets/styles/CommonStyle";
+
 import {Link} from "react-router-dom";
 import {useMove} from "../../../hooks/useMove";
 import {useRecoilState} from "recoil";
 import {isSignInState} from "../../../recoil/user/atoms";
 import SearchSection from "../../section/SearchSection/SearchSection";
 
-export function Header(props) {
+export function Header({currentAddress}) {
     const navigate = useMove()
     const [isSignIn, setIsSignIn] = useRecoilState(isSignInState)
     return (
@@ -16,8 +16,11 @@ export function Header(props) {
             <HeaderOuter>
                 <HeaderInner>
                     <Link to={"/"}>
-                        <Logo>배달의 신</Logo>
+                        <h1>배달의 신</h1>
                     </Link>
+                    <div>
+                        {currentAddress}asd
+                    </div>
                     {isSignIn ? <MainButton type={"button"} text={"로그아웃"} onClick={
                             () => {
                                 localStorage.removeItem("access-token")
@@ -25,10 +28,10 @@ export function Header(props) {
                                 setIsSignIn(false)
                             }
                         }/> :
-                        <>
+                        <div>
                             <MainButton type={"button"} text={"회원가입"} onClick={() => navigate("sign-up")}/>
                             <MainButton type={"button"} text={"로그인"} onClick={() => navigate("sign-in")}/>
-                        </>}
+                        </div>}
                 </HeaderInner>
             </HeaderOuter>
             <SearchSection/>
