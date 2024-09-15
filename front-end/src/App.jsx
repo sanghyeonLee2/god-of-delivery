e8dd16da5b219c593e096f6d35e9a2ba01d734e3
sanghyeonLee <hyeon012366@gmail.com>
2024-09-15 19:31:52 +0900

    Feat: 새로고침시 로근인 유지를 위한 useEffect 추가

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {GlobalStyle} from "./assets/styles/GlobalStyle";
 import {Header} from "./components/common/Header/Header";
 import {Outlet} from "react-router-dom";
 import Footer from "./components/common/Footer/Footer";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {isSignInState, userInfoState} from "./recoil/user/atoms";
+import {getAuthApi} from "./apis/api/user";
 
 function App() {
+    //const [data, isError, status, isLoading,] = useAuthGet("me")
+    const isSignIn = useRecoilValue(isSignInState)
+    const [userInfo, setUserInfo] = useRecoilState(userInfoState)
+    useEffect(() => {
+        const fetchData = async () => {
+            if (isSignIn) {
+                const result = await getAuthApi("me")
+                console.log(result)
+                setUserInfo(result)
+            }
+        }
+        fetchData()
+    }, []);
+    console.log(userInfo)
     return (
         <>
             <div className="App">
-                <Header/>
+                <Header currentAddress={userInfo?.currentAddress}/>
                 <GlobalStyle/>
                 <Outlet/>
             </div>
