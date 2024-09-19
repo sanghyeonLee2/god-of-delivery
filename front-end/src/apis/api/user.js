14a42d1444e4f5e15379ce5d16261ce7530582ae
sanghyeonLee <hyeon012366@gmail.com>
2024-09-19 21:52:05 +0900

    Refactor: 함수화 시킨 axios instance 선언

--- front-end/src/apis/api/user.js
+++ front-end/src/apis/api/user.js
@@ -1,8 +1,8 @@
-import {authInstance, instance} from "../utils/instance";
+import {axiosApi, axiosAuthApi} from "../utils/instance";
 
 export const getUserApi = async (url) => {
     try {
-        const res = await instance.get(url)
+        const res = await axiosApi.get(url)
         if (res.status === 200) {
             return res.data.available
         }
@@ -13,23 +13,29 @@ export const getUserApi = async (url) => {
 
 export const getApi = async (url) => {
     try {
-        return await instance.get(url)
+        return await axiosAuthApi.get(url)
     } catch (err) {
         console.log(err)
     }
 }
 export const getAuthApi = async (url) => {
     try {
-        return await authInstance.get(url)
+        /*   const fullUrl = `${axiosAuthApi.defaults.baseURL}${url}`; // 전체 URL 생성
+           console.log('Request URL:', fullUrl); // 요청 URL 로그 출력
+           console.log(axiosAuthApi); // 요청 URL 로그 출력*/
+
+        const res = await axiosApi().get(url);
+        console.log('Response:', res.data); // 응답 로그 출력
+        return res;
     } catch (err) {
-        console.log(err)
+        console.log('Error fetching data:', err);
     }
 }
 
 
 export const postUserApi = async (url) => {
     try {
-        return await instance.post(url)
+        return await axiosApi.post(url)
     } catch (err) {
         console.log(err)
     }
@@ -37,7 +43,7 @@ export const postUserApi = async (url) => {
 
 export const postAuthUserApi = async (url) => {
     try {
-        return await authInstance.post(url)
+        return await axiosAuthApi.post(url)
     } catch (err) {
         console.log(err)
     }
@@ -46,7 +52,7 @@ export const postAuthUserApi = async (url) => {
 
 export const postReissue = async (url) => {
     try {
-        return await authInstance.post(url, {}, {
+        return await axiosAuthApi.post(url, {}, {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem("refresh-token")}`
             }
