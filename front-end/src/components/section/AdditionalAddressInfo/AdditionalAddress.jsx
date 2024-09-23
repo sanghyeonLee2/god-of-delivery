4b617bba02f25b4519098f6ebbfbba8ff8f43e21
sanghyeonLee <hyeon012366@gmail.com>
2024-09-23 20:51:41 +0900

    Feat: 반응형 스타일을 위한 스타일 컴포넌트 wrap 및 section 파일 컴포넌트 추가

--- front-end/src/components/section/AdditionalAddressInfo/AdditionalAddress.jsx
+++ front-end/src/components/section/AdditionalAddressInfo/AdditionalAddress.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import {AdditionalAddressWrapper} from "./AdditionalAddressLayout";
+import {AddAddressSection, AddAddressWrap} from "./AdditionalAddressLayout";
 import MainInput from "../../common/Input/MainInput/MainInput";
 import Select from "../../common/Select/Select";
 import Option from "../../common/Select/options/Option";
 
 function AdditionalAddress() {
     return (
-        <AdditionalAddressWrapper>
-            <p>라이더님께</p>
-            <Select>
-                <Option text={"문 앞에 두고 벨 눌러주세요."}/>
-                <Option text={"문 앞에 두고 노크 해주세요."}/>
-                <Option text={"문 앞에 두면 가져갈께요 (벨X,노크X)"}/>
-                <Option text={"직접 받을게요"}/>
-                <Option text={"전화주시면 마중 나갈게요"}/>
-                <Option text={"직접 입력"}/>
-            </Select>
-            <p>길 안내</p>
-            <MainInput reigterName={"roadInfo"}/>
-        </AdditionalAddressWrapper>
+        <AddAddressWrap>
+            <AddAddressSection>
+                <p>라이더님께</p>
+                <Select>
+                    <Option text={"문 앞에 두고 벨 눌러주세요."}/>
+                    <Option text={"문 앞에 두고 노크 해주세요."}/>
+                    <Option text={"문 앞에 두면 가져갈께요 (벨X,노크X)"}/>
+                    <Option text={"직접 받을게요"}/>
+                    <Option text={"전화주시면 마중 나갈게요"}/>
+                    <Option text={"직접 입력"}/>
+                </Select>
+            </AddAddressSection>
+            <AddAddressSection>
+                <p>길 안내</p>
+                <MainInput reigterName={"roadInfo"}/>
+            </AddAddressSection>
+        </AddAddressWrap>
     );
 }
 
