f9401524076d9af8f4ae76c383c2cbaa7e7c0b7c
sanghyeonLee <hyeon012366@gmail.com>
2024-09-23 21:17:05 +0900

    Refactor: isAuth 인자를 통해 api 요청시 헤더에 토큰 포함 여부 설정 및 코드 중복 제거

--- front-end/src/hooks/usePost.js
+++ front-end/src/hooks/usePost.js
@@ -1,18 +1,17 @@
 import {useMutation} from "react-query"
 import {useNavigate} from "react-router-dom";
-import {postAuthUserApi, postUserApi} from "../apis/api/user";
+import {authPostApi, postApi} from "../apis/api/user";
 import {useSetRecoilState} from "recoil";
 import {isSignInState} from "../recoil/user/atoms";
 import {isModalOpenState} from "../recoil/flag/atoms";
 
-export const usePost = (url) => {
-
+export const usePost = (url, isAuth = false) => {
     const setSignIn = useSetRecoilState(isSignInState)
-
+    const setIsModalOpen = useSetRecoilState(isModalOpenState)
     const navigate = useNavigate()
     return useMutation(
-        () => postUserApi(url), {
-            onSuccess: (key) => {
+        () => isAuth ? authPostApi(url) : postApi(url), {
+            onSuccess: (res) => {
                 switch (url) {
                     case "signUp":
                         navigate("/sign-in")
@@ -20,40 +19,19 @@ export const usePost = (url) => {
                         break
                     case "signIn":
                         setSignIn(true)
-                        console.log(key)
-                        localStorage.setItem("access-token", key.data.accessToken)
-                        localStorage.setItem("refresh-token", key.data.refreshToken)
+                        localStorage.setItem("access-token", res.data.accessToken)
+                        localStorage.setItem("refresh-token", res.data.refreshToken)
                         navigate("/")
                         break
                     case "customer/register":
                         alert("성공")
                         break
-                    default:
-                        console.log(url)
-                        alert("알 수 없는 오류")
-                        break
-                }
-            },
-            onError: () => {
-                console.log("실패")
-            },
-            onSettled: () => {
-                console.log("결과에 상관없이 실행")
-            }
-        })
-}
-
-export const useAuthPost = (url) => {
-    const setIsModalOpen = useSetRecoilState(isModalOpenState)
-    return useMutation(
-        () => postAuthUserApi(url), {
-            onSuccess: (key) => {
-                switch (url) {
-                    case "customer/address":
-                        alert("성공")
+                    case "address":
+                        alert("주소가 추가 되었습니다.")
                         setIsModalOpen(false)
                         break
                     default:
+                        console.log(url)
                         alert("알 수 없는 오류")
                         break
                 }
