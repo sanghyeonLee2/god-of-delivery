d5f3d98f21fd45feb6053b2b7f681c75c5492ddd
sanghyeonLee <hyeon012366@gmail.com>
2024-09-23 21:01:11 +0900

    Refactor: 변경된 스타일 컴포넌트 이름 사용 및 이미지 크기 감소

--- front-end/src/components/common/Button/icon/IconButton.jsx
+++ front-end/src/components/common/Button/icon/IconButton.jsx
@@ -1,12 +1,12 @@
+import {IconBtnWrap} from "./IconButtonLayout";
 import React from 'react';
-import {IconButtonWrapper} from "./IconButtonLayout";
 import reset from "../../../../assets/img/reset.png";
 
 function IconButton({type, onClick}) {
     return (
-        <IconButtonWrapper type={type} onClick={onClick}>
-            <img src={reset} width={40} alt={"닫기 버튼"}/>
-        </IconButtonWrapper>
+        <IconBtnWrap type={type} onClick={onClick}>
+            <img src={reset} width={28} alt={"닫기 버튼"}/>
+        </IconBtnWrap>
     );
 }
 
