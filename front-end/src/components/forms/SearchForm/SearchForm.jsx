c2c0637453b385ff5b595d4b9f68b98570a4631b
sanghyeonLee <hyeon012366@gmail.com>
2024-09-23 21:08:30 +0900

    Feat: 위치 선택 모달 창 추가 함수 및 useClick 커스텀 훅 사용을 위한 훅 선언, ref 설정

--- front-end/src/components/forms/SearchForm/SearchForm.jsx
+++ front-end/src/components/forms/SearchForm/SearchForm.jsx
@@ -2,34 +2,29 @@ import React from 'react';
 import Input from "../../common/Input/Input";
 import resetIcon from "../../../assets/img/reset.png";
 import {MainButton} from "../../common/Button/main/MainButton";
-import {LocationBtn, SearchInputForm, SearchInputWrapper} from "./SearchFormLayout";
-import locationIcon from "../../../assets/img/my_location.png";
-import {useRecoilState, useSetRecoilState} from "recoil";
+import {SearchInputForm} from "./SearchFormLayout";
+import {useSetRecoilState} from "recoil";
 import {isModalOpenState} from "../../../recoil/flag/atoms";
-import {coordsState} from "../../../recoil/map/atoms";
+import {useLocation} from "react-router-dom";
+import useClick from "../../../hooks/useClick";
 
 function SearchForm() {
-    const [isModalOpen, setIsModalOpen] = useRecoilState(isModalOpenState)
-    const setCoords = useSetRecoilState(coordsState)
-    const onClickCoords = () => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            console.log(`lat: ${position.coords.latitude}, lng: ${position.coords.longitude}`)
-            setCoords({center: {lat: position.coords.latitude, lng: position.coords.longitude}, isPanto: true})
-        })
+    const location = useLocation()
+    const setIsModalOpen = useSetRecoilState(isModalOpenState)
+    const setClickEvent = (pathname) => {
+        pathname !== "/" && setIsModalOpen(true)
     }
+    const onClick = useClick(() => setClickEvent(location.pathname))
+//react-hook-form으로 변경....ㅜ
     return (
-        <SearchInputWrapper>
-            <LocationBtn type={"button"} onClick={
-                isModalOpen ? onClickCoords : () => setIsModalOpen(true)}>
-                <img src={locationIcon} alt="구글 위치 아이콘"/>
-            </LocationBtn>
-            <SearchInputForm>
-                <Input type={"text"}/>
-                <img src={resetIcon} alt="구글 리셋 아이콘"/>
-                <MainButton type={"submit"} text={"검색"}/>
-            </SearchInputForm>
-        </SearchInputWrapper>
-
+        <SearchInputForm>
+            <Input type={"text"}
+                   ref={onClick}
+                   placeholder={location.pathname === "/" ? "배고프니까 일단 검색" : "주소 검색"}
+            />
+            <img src={resetIcon} alt="구글 리셋 아이콘"/>
+            <MainButton type={"submit"} text={"검색"}/>
+        </SearchInputForm>
     );
 }
 
