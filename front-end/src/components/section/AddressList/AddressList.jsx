cb602dbc23e61f69fa2a8112e5a07e9608e658fd
sanghyeonLee <hyeon012366@gmail.com>
2024-09-23 20:56:13 +0900

    Refactor: 불필요한 코드 삭제 및 api 엔드포인트 변경

--- front-end/src/components/section/AddressList/AddressList.jsx
+++ front-end/src/components/section/AddressList/AddressList.jsx
@@ -3,18 +3,16 @@ import {AddressListOuter} from "./AddressListLayout";
 import AddressBox from "../../common/AddressBox/AddressBox";
 import useGet from "../../../hooks/useGet";
 import Loading from "../../common/Loading/Loading";
-import {useRecoilValue} from "recoil";
-import {userInfoState} from "../../../recoil/user/atoms";
 
 function AddressList(props) {
-    const userInfo = useRecoilValue(userInfoState)
-    const [data, isError, status, isLoading] = useGet(`customer/${userInfo.userId}/address`)
+    //const userInfo = useRecoilValue(userInfoState)
+    const [data, isError, status, isLoading] = useGet("address")
     //const {mutate: onSignIn} = usePost("auth/sign-in")
     if (isLoading)
         return <Loading/>
     return (
         <AddressListOuter>
-            {data.data?.map((e) =>
+            {data?.data?.map((e) =>
                 <AddressBox addressInfo={e} key={e.id}/>
             )}
         </AddressListOuter>
