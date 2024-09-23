7c460883b021480b976a6f664ce6bcc0eab96fe8
sanghyeonLee <hyeon012366@gmail.com>
2024-09-23 21:09:41 +0900

    Design: 폼 크기 조정, 테두리 설정, 코드 이동에 따른 불필요 코드 제거

--- front-end/src/components/forms/SearchForm/SearchFormLayout.js
+++ front-end/src/components/forms/SearchForm/SearchFormLayout.js
@@ -1,20 +1,24 @@
-import {elementSize, flexLayout} from "../../../assets/styles/Mixin";
+import {elementSize} from "../../../assets/styles/Mixin";
 import styled from "styled-components";
 
 export const SearchInputForm = styled.form`
   position: relative;
-  ${flexLayout("center")};
-  width: 500px;
+  margin: 0 auto;
+  display: flex;
+  ${elementSize("500px", "45px")};
 
   input {
     border-bottom-right-radius: 0;
     border-top-right-radius: 0;
-    border: 0;
+    border: black solid 1px;
+    border-right: 0;
+
     ${elementSize("86%", "100%")};
   }
 
   button {
     border-bottom-left-radius: 0;
+    border: black solid 1px;
     border-top-left-radius: 0;
     ${elementSize("14%", "100%")};
   }
@@ -22,22 +26,10 @@ export const SearchInputForm = styled.form`
   img {
     position: absolute;
     left: 390px;
-    top: 10px;
+    top: 7.5px;
     cursor: pointer;
     ${elementSize("31px", "31px")}
   }
 `
-export const SearchInputWrapper = styled.div`
-  height: 50px;
-  ${flexLayout("space-between")}
-`
 
-export const LocationBtn = styled.button`
-  background-color: white;
-  width: 11%;
-
-  img {
-    ${elementSize("33px", "33px")}
-  }
-`
 
