3b21913c45123ab557b754c83908e828aa5693de
sanghyeonLee <hyeon012366@gmail.com>
2024-09-23 21:06:25 +0900

    Feat: 현재위치 탐지 버튼 map 안으로 추가

--- front-end/src/components/section/KakaoMap/KakaoMap.jsx
+++ front-end/src/components/section/KakaoMap/KakaoMap.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import {Map, MapMarker} from "react-kakao-maps-sdk";
-import {useRecoilState, useRecoilValue} from "recoil";
-import {addressState, coordsState} from "../../../recoil/map/atoms";
+import {useRecoilState} from "recoil";
+import {coordsState} from "../../../recoil/map/atoms";
+import {CurrentLocationBtn, MapWrap} from "./KakaoMapLayout";
+import locationImg from "../../../assets/img/my_location.png"
 
 function KakaoMap() {
-    const address = useRecoilValue(addressState)
     const [coords, setCoords] = useRecoilState(coordsState)
+    const onClickCoords = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            //console.log(`lat: ${position.coords.latitude}, lng: ${position.coords.longitude}`)
+            setCoords({center: {lat: position.coords.latitude, lng: position.coords.longitude}, isPanto: true})
+        })
+    }
     return (
-        <Map
-            center={coords.center}
-            style={{width: '80%', height: '40%', margin: "0 auto"}}
-            level={3}
-            onClick={(_, e) => {
-                const latLng = e.latLng
-                setCoords(
-                    {
-                        center: {
-                            lat: latLng.getLat(),
-                            lng: latLng.getLng()
-                        }, isPanto: false
-                    }
-                )
-            }
-            }>
-            <MapMarker position={coords.center}/>
-        </Map>
+        <MapWrap>
+            <Map
+                center={coords.center}
+                style={{width: '80%', height: '100%', margin: "0 auto"}}
+                level={3}
+                onClick={(_, e) => {
+                    const latLng = e.latLng
+                    setCoords(
+                        {
+                            center: {
+                                lat: latLng.getLat(),
+                                lng: latLng.getLng()
+                            }, isPanto: true
+                        }
+                    )
+                }
+                }>
+                <MapMarker position={coords.center}/>
+            </Map>
+            <CurrentLocationBtn type={"button"} onClick={onClickCoords}>
+                <img src={locationImg} width={35} alt={"위치 버튼"}/></CurrentLocationBtn>
+        </MapWrap>
     );
 }
 
