577b1d375422bf68a67021f8c9d33803d70b3606
sanghyeonLee <hyeon012366@gmail.com>
2024-09-30 19:32:11 +0900

    Style: 가게 페이지 내 헤더 컴포넌트 스타일 추가

---
+++ front-end/src/components/section/StoreHeader/StoreHeaderLayout.js
@@ -0,0 +1,13 @@
+import {flexLayout} from "../../../assets/styles/Mixin";
+import styled from "styled-components";
+
+export const StoreHeaderWrap = styled.div`
+  text-align: center;
+
+  div {
+    margin-bottom: 25px;
+  }
+`
+export const ReviewWrap = styled.div`
+  ${flexLayout("center")}
+`
