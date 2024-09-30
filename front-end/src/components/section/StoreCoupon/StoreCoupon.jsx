fe9303efba5c41d486693c10aa6f8fd6fff79fde
sanghyeonLee <hyeon012366@gmail.com>
2024-09-30 19:30:54 +0900

    Feat: 가게 내 쿠폰 섹션 컴포넌트 추가

---
+++ front-end/src/components/section/StoreCoupon/StoreCoupon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {CouponWrap} from "./StoreCouponLayout";
+
+function StoreCoupon(props) {
+    return (
+        <CouponWrap>
+            <div>쿠폰1</div>
+        </CouponWrap>
+    );
+}
+
+export default StoreCoupon;
