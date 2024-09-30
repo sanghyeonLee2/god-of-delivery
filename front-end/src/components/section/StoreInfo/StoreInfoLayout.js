1fdf0898bd7a2eb58d95a7d1425a602378c9cbf6
sanghyeonLee <hyeon012366@gmail.com>
2024-09-30 19:34:35 +0900

    Style: 가게 페이지 내 가게 정보 컴포넌트 스타일 추가

---
+++ front-end/src/components/section/StoreInfo/StoreInfoLayout.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const StoreInfoOuter = styled.div`
+  margin-top: 20px;
+  padding: 20px 10px;
+`
+export const StoreInfoInner = styled.div`
+  ul {
+    padding-bottom: 30px;
+  }
+
+  li {
+    margin-bottom: 10px;
+  }
+`
+export const StoreInfoTitleWrap = styled.div`
+  margin: 30px 0;
+`
