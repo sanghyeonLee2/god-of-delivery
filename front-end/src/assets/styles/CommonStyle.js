d76658b06d8144f6796956a471a3eba1155cb10e
sanghyeonLee <hyeon012366@gmail.com>
2024-09-30 19:26:43 +0900

    Style: 세로 회색 빈 공간 공통 스타일 추가

--- front-end/src/assets/styles/CommonStyle.js
+++ front-end/src/assets/styles/CommonStyle.js
@@ -4,3 +4,8 @@ import styled from "styled-components";
 export const P = styled.p`
   font-size: ${(props) => props.size}
 `
+export const VerticalSpace = styled.div`
+  height: 10px;
+  background-color: lightgray;
+  margin: 0 ${(props) => props.$margin || "0px"}
+`
