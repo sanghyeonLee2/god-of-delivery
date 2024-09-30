e1e53ab2c56f812e1ea791a67364a897d3bcf4c7
sanghyeonLee <hyeon012366@gmail.com>
2024-09-30 19:36:38 +0900

    Feat: 가게 메뉴 컴포넌트 스타일 추가

---
+++ front-end/src/components/section/StoreMenu/StoreMenuLayout.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const MenuWrap = styled.div`
+`
+
+export const InfoBox = styled.div`
+  padding: 20px 10px;
+
+  div {
+    border-radius: 5px;
+    padding: 10px 5px;
+    min-height: 35px;
+    background-color: lightgray;
+  }
+`
+export const RecommendWrap = styled.ul`
+`
