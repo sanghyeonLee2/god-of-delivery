9822021a0b3350acca6c16c0fbd4cb8c5179867a
sanghyeonLee <hyeon012366@gmail.com>
2024-09-30 19:38:37 +0900

    Design: 가게 페이지 내 리뷰컴포넌트 스타일 추가

---
+++ front-end/src/components/section/StoreReview/StoreReview.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StoreReviewWrap} from "./StoreReviewLayout";
+
+function StoreReview(props) {
+    return (
+        <StoreReviewWrap>
+            <ul>
+                <li>사진</li>
+                <li>내용</li>
+            </ul>
+        </StoreReviewWrap>
+    );
+}
+
+export default StoreReview;
