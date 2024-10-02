4c1496efe165688e5e0b2866a9c9ea070e83e04f
sanghyeonLee <hyeon012366@gmail.com>
2024-10-02 23:14:58 +0900

    Refactor: useTab 사용 및 content 데이터 사용으로 코드 단순화

--- front-end/src/components/section/StoreOrderInfo/StoreOrderInfo.jsx
+++ front-end/src/components/section/StoreOrderInfo/StoreOrderInfo.jsx
@@ -1,42 +1,49 @@
-import React, {useState} from 'react';
-import {OrderInfoWrap, OrderMethodWrap, OrderToggleWrap, OtherToggleSpan} from "./StoreOrderInfoLayout";
-import useClick from "../../../hooks/useClick";
+import React from 'react';
+import {OrderInfoWrap, OrderMethodWrap, OrderToggleWrap, OtherToggleDiv} from "./StoreOrderInfoLayout";
+import useTab from "../../../hooks/useTab";
 
 function StoreOrderInfo() {
-    const [isDelivery, setIsDelivery] = useState(true)
-    const onClickDelivery = useClick(() => {
-        setIsDelivery(true);
-    });
-    const onClickPickup = useClick(() => {
-        setIsDelivery(false);
-    });
-
+    const content = [
+        {
+            key: 0,
+            tab: "배달주문",
+            content: ["최소 주문금액", "결제방법", "배달시간", "배달팁"]
+        },
+        {
+            key: 1,
+            tab: "포장/방문주문",
+            content: ["최소 주문금액", "결제방법", "배달시간", "배달팁", "결제방법"]
+        }
+    ];
+    const {currentItem, setCurrentItem} = useTab(0, content)
     return (
         <OrderMethodWrap>
             <OrderToggleWrap>
-                <li ref={onClickDelivery}>
-                    <OtherToggleSpan value={isDelivery}>배달주문</OtherToggleSpan>
-                </li>
-                <li ref={onClickPickup}>
-                    <OtherToggleSpan value={!isDelivery}>포장/방문주문</OtherToggleSpan>
-                </li>
+                {content.map((ele, idx) => {
+                    return <li key={ele.key}>
+                        <OtherToggleDiv value={currentItem.key === ele.key}
+                                        onClick={() => setCurrentItem(idx)}
+                        >
+                            <span>{ele.tab}</span>
+                        </OtherToggleDiv>
+                    </li>
+                })}
             </OrderToggleWrap>
             <OrderInfoWrap>
                 <ul>
                     <li>
-                        최소주문금액:{isDelivery ? "10,000원" : "이용방법"}
-                    </li>
-                    {/*포장 정보 조건부 구현 예정*/}
-                    <li>
-                        {isDelivery ? "결제방법" : "이용방법"}
+                        {currentItem.content[0]}
                     </li>
                     <li>
-                        {isDelivery ? "배달시간" : "픽업시간"}
+                        {currentItem.content[1]}
                     </li>
                     <li>
-                        {isDelivery ? "배달팁" : "위치안내"}
+                        {currentItem.content[2]}
                     </li>
-                    {!isDelivery && <li>
+                    <li>
+                        {currentItem.content[3]}
+                    </li>
+                    {currentItem?.content[4] && <li>
                         결제방법
                     </li>}
                 </ul>
