891373cfad50f64da5b836d833f265eb1837aacb
yuseong <dbtjd7282@gmail.com>
2024-10-03 17:18:21 +0900

    Feat: getDibsWithStore 메서드(Dib한 StoreList 보기) include를 이용해 조회 / userId, storeId를 이용해 DibList 조회 메서드 추가 / Dib 메서드 Store.dibCount +1 추가 함수 / 생성일 수정일 추가 / Dib 취소 메서드 Store.dibCount 수정 추가 및 send data 추가

--- back-end/controllers/dibController.js
+++ back-end/controllers/dibController.js
@@ -1,61 +1,123 @@
-const Dib = require('../models/Dib');
-const User = require('../models/User');
-const Store = require('../models/Store');
+const Dib = require('../models/dib');
+const User = require('../models/user');
+const Store = require('../models/store');
+const {Sequelize} = require("sequelize");
 
-exports.getDibStoreList = async(req, res) => {
+exports.getDibsWithStore = async (req, res) => {
     try{
-        const findDibs = await Dib.findAll({
+        const StoreListWithDib = await Dib.findAll({
             where:{
                 userId: req.params.userId
             },
-            attributes:['storeId']
+            include: [{model:Store}],
         })
         res.status(200).send({
             status: 200,
-            data: findDibs
-        })
-    }catch(err){
-        res.status(400).send({
-            status: 400,
+            data: StoreListWithDib
+        });
+    }
+    catch (err){
+        res.status(500).send({
+            status: 500,
             message: err.message
+        })
+    }
+
+}
+
+exports.getDibsListByUser = async (req, res) => {
+    try{
+        const dibsList = await Dib.findAll({
+            where:{userId:req.params.userId}
+        });
+        res.status(200).send({
+            status:200,
+            data: dibsList
+        })
+    }
+    catch (err){
+        res.status(400).send({
+            status:400,
+            message:err.message
+        })
+    }
+}
+
+exports.getDibsListByStore = async (req, res) => {
+    try{
+        const dibsList = await Dib.findAll({
+            where:{storeId:req.params.storeId}
+        });
+        res.status(200).send({
+            status:200,
+            data: dibsList
+        })
+    }
+    catch (err){
+        res.status(400).send({
+            status:400,
+            message:err.message
         })
     }
 }
 
 exports.postDibFilled = async (req, res) => {
-    try{
+    const date = new Date();
+    try {
         const dibData = await Dib.create({
             userId: req.body.userId,
             storeId: req.body.storeId,
+            createdDate: date,
+            modifiedDate: date
         })
-        res.status(200).send({
-            status: 200,
+        await Store.update(
+            {
+                dibsCount: Sequelize.literal('dibs_count + 1'),
+            },
+            {
+                where: {storeId: req.body.storeId,},
+            }
+        );
+        const storeData = await Store.findOne({
+            where:{storeId:req.body.storeId}
+        })
+        res.status(201).send({
+            status: 201,
             filled: true,
-            data: dibData
+            storeData: storeData,
+            dibData: dibData,
         })
-    }
-    catch(err){
+    } catch (err) {
         res.status(400).send({
             status: 400,
-            filled:false,
+            filled: false,
             message: err.message,
         })
     }
 }
-exports.postDibCancel = async (req,res) => {
-    try{
+exports.postDibCancel = async (req, res) => {
+    try {
         await Dib.destroy({
-            where:{
+            where: {
                 userId: req.body.userId,
                 storeId: req.body.storeId,
             }
         })
+        await Store.update({
+            dibsCount: Sequelize.literal('dibs_count - 1'),},{
+            where: {
+                storeId: req.body.storeId,
+            }
+        })
+        const storeData = await Store.findOne({
+            where:{storeId:req.body.storeId}
+        })
         res.status(200).send({
             status: 200,
-            filled:false,
+            filled: false,
+            storeData: storeData,
         })
-    }
-    catch (err){
+    } catch (err) {
         res.status(400).send({
             status: 400,
             message: err.message,
