ea011cc593763ef649ba3df349acdf44e38d013f
yuseong <dbtjd7282@gmail.com>
2024-10-03 17:09:28 +0900

    Feat: createStore router추가

--- back-end/routes/storeRoutes.js
+++ back-end/routes/storeRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const {getStores} = require('../controllers/storeController')
+const {getStores, postCreateStore} = require('../controllers/storeController')
 
 router.get('/:city/:district/:category', getStores);
+router.post('/create', postCreateStore);
 
 module.exports = router;
