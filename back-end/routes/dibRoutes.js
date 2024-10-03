579d1e490c3ba3ca36347cd26d00b4618923bcd1
yuseong <dbtjd7282@gmail.com>
2024-10-03 17:10:37 +0900

    Feat: DibList(userId 또는 storeId로 조회) router 추가, Dib취소 라우터 추가

--- back-end/routes/dibRoutes.js
+++ back-end/routes/dibRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const {getDibStoreList, postDibFilled, postDibCancel} = require("../controllers/dibController");
+const {getDibsWithStore,getDibsListByUser, getDibsListByStore,postDibFilled, postDibCancel} = require("../controllers/dibController");
 const {verifyToken} = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 //찜 목록 가져오기
-router.get('/',verifyToken, getDibStoreList)
+router.get('/storeList/:userId',getDibsWithStore)
+router.get('/:userId', getDibsListByUser)
+router.get('/:storeId', getDibsListByStore)
 //찜 하기
 router.post('/', postDibFilled)
-router.post('/cancel',verifyToken, postDibCancel)
+router.post('/cancel', postDibCancel)
 
 module.exports = router
