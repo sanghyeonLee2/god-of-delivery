39bd541107ab3e1a0cbbaa95b4e3dbca3e6f6ac5
sanghyeonLee <hyeon012366@gmail.com>
2024-10-04 18:13:59 +0900

    Refactor: useTab 적용 및 불필요 스타일 컴포넌트 제거

--- front-end/src/pages/StorePage/StorePage.jsx
+++ front-end/src/pages/StorePage/StorePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MenuInfoTapWrap, MenuToggleWrap, StoreOuter} from "./StorePageLayout";
+import {MenuInfoTapInner, MenuToggleDiv, MenuToggleWrap, StoreOuter} from "./StorePageLayout";
 import {VerticalSpace} from "../../assets/styles/CommonStyle";
 import StoreInfo from "../../components/section/StoreInfo/StoreInfo";
 import StoreReview from "../../components/section/StoreReview/StoreReview";
@@ -8,8 +8,25 @@ import UserActions from "../../components/section/UserActions/UserActions";
 import StoreCoupon from "../../components/section/StoreCoupon/StoreCoupon";
 import StoreOrderInfo from "../../components/section/StoreOrderInfo/StoreOrderInfo";
 import StoreMenu from "../../components/section/StoreMenu/StoreMenu";
+import useTab from "../../hooks/useTab";
 
 function StorePage(props) {
+    const content = [
+        {
+            key: 0,
+            tab: "메뉴",
+        },
+        {
+            key: 1,
+            tab: "정보원산지",
+        },
+        {
+            key: 2,
+            tab: "리뷰",
+        }
+    ];
+    const {currentItem, setCurrentItem} = useTab(0, content)
+//·
     return (
         <StoreOuter>
             <StoreHeader/>
@@ -18,15 +35,21 @@ function StorePage(props) {
             <StoreOrderInfo/>
             <VerticalSpace/>
             <MenuToggleWrap>
-                <li>메뉴</li>
-                <li>정보·원산지</li>
-                <li>리뷰</li>
+                {content.map((ele, idx) => {
+                    return <li key={ele.key}>
+                        <MenuToggleDiv value={currentItem.key === ele.key}
+                                       onClick={() => setCurrentItem(idx)}
+                        >
+                            <span> {ele.tab}</span>
+                        </MenuToggleDiv>
+                    </li>
+                })}
             </MenuToggleWrap>
-            <MenuInfoTapWrap>
-                <StoreMenu/>
-                <StoreInfo/>
-                <StoreReview/>
-            </MenuInfoTapWrap>
+            <MenuInfoTapInner>
+                {currentItem.key === 0 && <StoreMenu/>}
+                {currentItem.key === 1 && <StoreInfo/>}
+                {currentItem.key === 2 && <StoreReview/>}
+            </MenuInfoTapInner>
         </StoreOuter>
     );
 }
