47ad90c6ce252a59c71202e326021017f8913087
sanghyeonLee <hyeon012366@gmail.com>
2024-10-04 18:14:48 +0900

    Design: li 높이 60px 고정 및 클릭시 테두리 동적 ui 적용

--- front-end/src/pages/StorePage/StorePageLayout.js
+++ front-end/src/pages/StorePage/StorePageLayout.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {elementSize} from "../../assets/styles/Mixin";
+import {elementSize, flexLayout} from "../../assets/styles/Mixin";
 
 export const StoreOuter = styled.div`
   max-width: 800px;
@@ -10,20 +10,21 @@ export const StoreOuter = styled.div`
 
 export const MenuToggleWrap = styled.ul`
   display: flex;
-  border-bottom: grey 1px solid;
-
 
   li {
-    padding-top: 20px;
-    ${elementSize("33.33%", "40px")}
-    text-align: center;
-  }
-
-  span {
+    ${elementSize("33.33%", "60px")}
   }
 `
+export const MenuToggleDiv = styled.div`
+  ${flexLayout("center", "center")};
+  border-top: ${(props) => props.value ? "black 3px solid" : "transparent 3px solid "};
+  border-bottom: ${(props) => props.value ? "transparent 1px solid" : "grey 1px solid"};
+  height: 100%;
+`
 
-
-export const MenuInfoTapWrap = styled.div`
+export const MenuInfoTapInner = styled.div`
+  > div {
+    padding: 15px 10px;
+  }
 `
 
