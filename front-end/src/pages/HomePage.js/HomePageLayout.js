35dee96c8378cc424d205064648649cced3ffa51
sanghyeonLee <hyeon012366@gmail.com>
2024-11-22 11:22:57 +0900

    Design: 카테고리 목록 미디어 쿼리 추가 및 스타일 코드 최적화

--- front-end/src/pages/HomePage.js/HomePageLayout.js
+++ front-end/src/pages/HomePage.js/HomePageLayout.js
@@ -1,30 +1,41 @@
 import styled from "styled-components";
 import {elementSize, flexLayout} from "../../assets/styles/Mixin";
 
-export const CategoryOuter = styled.main`
-  margin-top: 18px;
-  min-height: 1190px;
+
+export const CategoryWrap = styled.div`
+    min-height: 1075px;
+    max-width: 1180px;
+    margin: 0 auto;
+    height: 100%;
+    ${flexLayout("space-between")};
+    flex-wrap: wrap;
+    padding: 10px 0
 `
-export const CategoryInner = styled.div`
-  margin: 0 auto;
-  max-width: 1180px;
-  height: 100%;
-  ${flexLayout("space-between")};
-  flex-wrap: wrap;
+export const CategoryBoxOuter = styled.div`
+    ${elementSize("285px", "255px")};
+
+    @media (max-width: 1200px) {
+        width: 32.8%;
+    }
+    @media (max-width: 1100px) {
+        width: 49.1%;
+    }
+    position: relative;
+    padding: 4px 2px;
+    cursor: pointer;
 `
-export const CategoryBox = styled.div`
-  ${elementSize("280px", "280px")}
-  border: lightgray 1px solid;
-  position: relative;
-  cursor: pointer;
-  margin-bottom: 18px;
+export const CategoryBoxInner = styled.div`
+    border: lightgray 1px solid;
+    height: 100%;
+
+    cursor: pointer;
 `
 export const CategoryText = styled.p`
-  margin-top: 25px;
-  margin-left: 25px;
+    margin-top: 25px;
+    margin-left: 25px;
 `
 export const CategoryImg = styled.img`
-  position: absolute;
-  left: 106px;
-  top: 108px;
+    position: absolute;
+    left: 106px;
+    top: 108px;
 `
