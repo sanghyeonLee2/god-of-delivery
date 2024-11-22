45eddb678b7465422624c4f34c8522a43352fccf
sanghyeonLee <hyeon012366@gmail.com>
2024-11-22 11:23:42 +0900

    Design: 푸터 스타일 코드 최적화

--- front-end/src/components/common/Footer/Footer.jsx
+++ front-end/src/components/common/Footer/Footer.jsx
@@ -1,60 +1,44 @@
 import React from 'react';
-import {
-    FooterAsideInfo,
-    FooterInfoInner,
-    FooterInfoOuter,
-    FooterMenuInner,
-    FooterMenuLeftUl,
-    FooterMenuOuter,
-    FooterMenuRightUl,
-    FooterWrapper
-} from "./FooterLayout";
+import {FooterInfoInner, FooterMenuLeft, FooterMenuOuter, FooterMenuRight, FooterWrapper} from "./FooterLayout";
 import {Link} from "react-router-dom";
+import {SmallSizeFont} from "../../../assets/styles/CommonStyle";
 
 function Footer(props) {
     return (
         <FooterWrapper>
             <FooterMenuOuter>
-                <FooterMenuInner>
-                    <FooterMenuLeftUl>
-                        <li>
-                            공지사항
-                        </li>
-                        <li>
-                            FAQ
-                        </li>
-                        <li>
-                            입점문의
-                        </li>
-                        <li>
-                            개인정보처리방침
-                        </li>
-                    </FooterMenuLeftUl>
-                    <FooterMenuRightUl>
-                        <li>
-                            깃허브
-                        </li>
-                        <li>
-                            블로그
-                        </li>
-                    </FooterMenuRightUl>
-                </FooterMenuInner>
+                <FooterMenuLeft>
+                    <li>
+                        <SmallSizeFont>공지사항</SmallSizeFont>
+                    </li>
+                    <li>
+                        <SmallSizeFont>FAQ</SmallSizeFont>
+                    </li>
+                    <li>
+                        <SmallSizeFont>개인정보처리방침</SmallSizeFont>
+                    </li>
+                </FooterMenuLeft>
+                <FooterMenuRight>
+                    <li>
+                        <SmallSizeFont>깃허브</SmallSizeFont>
+                    </li>
+                    <li>
+                        <SmallSizeFont>블로그</SmallSizeFont>
+                    </li>
+                </FooterMenuRight>
             </FooterMenuOuter>
-            <FooterInfoOuter>
-                <FooterInfoInner>
-                    <Link to={"/"}>
-                        <h1>
-                            배달의 신
-                        </h1>
-                    </Link>
-                    <FooterAsideInfo>
-                        <strong>주식회사 DN</strong>
-                        <p>부산시 사상구 주례로 47 대표이사 이상현, 신유성</p>
-                        <p>고객센터: hyeon012366@gmail.com</p>
-                        <p>호스팅 제공자: 이상현</p>
-                    </FooterAsideInfo>
-                </FooterInfoInner>
-            </FooterInfoOuter>
+            <FooterInfoInner>
+                <Link to={"/"}>
+                    <h1>
+                        배달의 신
+                    </h1>
+                </Link>
+                <div>
+                    <SmallSizeFont>주식회사 DN</SmallSizeFont>
+                    <br/>
+                    <SmallSizeFont>부산시 사상구 주례로 47 대표이사 이상현, 신유성</SmallSizeFont>
+                </div>
+            </FooterInfoInner>
         </FooterWrapper>
     );
 }
