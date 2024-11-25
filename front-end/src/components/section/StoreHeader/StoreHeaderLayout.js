45aafa4992fd865c782567cc274bb6bbaec5ecad
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 02:03:11 +0900

    Feat: 가게 컴포넌트의 헤더 컴포넌트 레이아웃 추가

--- front-end/src/components/section/StoreHeader/StoreHeaderLayout.js
+++ front-end/src/components/section/StoreHeader/StoreHeaderLayout.js
@@ -1,13 +1,18 @@
-import {flexLayout} from "../../../assets/styles/Mixin";
+import {elementSize, flexLayout} from "../../../assets/styles/Mixin";
 import styled from "styled-components";
+import {exampleColor} from "../../../assets/styles/CommonStyle";
 
 export const StoreHeaderWrap = styled.div`
-  text-align: center;
+    text-align: center;
 
-  div {
-    margin-bottom: 25px;
-  }
+    > div {
+        margin-bottom: 18px;
+    }
+`
+export const StoreImgWrap = styled.div`
+    ${elementSize("100%", "250px")}
+    ${exampleColor()}
 `
 export const ReviewWrap = styled.div`
-  ${flexLayout("center")}
+    ${flexLayout("center")}
 `
