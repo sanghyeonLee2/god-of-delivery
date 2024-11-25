821c1a968ed2215a0ae2fcf5df92558d73a91494
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 02:04:11 +0900

    Design: 가게 정보 UI 컴포넌트 레이아웃 추가

--- front-end/src/components/section/StoreInfo/StoreInfoLayout.js
+++ front-end/src/components/section/StoreInfo/StoreInfoLayout.js
@@ -2,15 +2,7 @@ import styled from "styled-components";
 
 
 export const StoreInfoWrap = styled.div`
-
-  ul {
-    padding-bottom: 30px;
-  }
-
-  li {
-    margin-bottom: 10px;
-  }
-`
-export const StoreInfoTitleWrap = styled.div`
-  margin: 30px 0;
+    ul {
+        padding-bottom: 15px;
+    }
 `
