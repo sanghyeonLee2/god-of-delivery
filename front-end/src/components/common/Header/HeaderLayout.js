b843c88fb784ad727b9ca0aed2d001d294406567
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 01:50:08 +0900

    Feat: 헤더 레이아웃 최적화

--- front-end/src/components/common/Header/HeaderLayout.js
+++ front-end/src/components/common/Header/HeaderLayout.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
-import {flexLayout} from "../../../assets/styles/Mixin";
+import {elementSize, flexLayout} from "../../../assets/styles/Mixin";
+import {COLORS} from "../../../assets/styles/colors";
 
 
-export const HeaderOuter = styled.header`
-    height: 92px;
-    padding: 0 30px;
-`
-
-export const HeaderInner = styled.div`
+export const HeaderWrap = styled.div`
     ${flexLayout("space-between", "center")};
     max-width: 1100px;
     margin: 0 auto;
-    height: 100%;
+    height: 92px;
+    padding: 0 10px;
+`
+
+export const CartWrap = styled.div`
+    position: absolute;
+    background-color: ${COLORS.BTN.SUB};
+    border-radius: 35px;
+    ${elementSize("80px", "80px")};
+    ${flexLayout("center", "center")};
 `
