9cad71d1a01002db94b675bda6a0d85ff045e694
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 02:02:00 +0900

    Feat: 쿠폰 컴포넌트 ui 레이아웃 추가

--- front-end/src/components/section/StoreCoupon/StoreCouponLayout.js
+++ front-end/src/components/section/StoreCoupon/StoreCouponLayout.js
@@ -1,14 +1,24 @@
 import styled from "styled-components";
-import {elementSize} from "../../../assets/styles/Mixin";
+import {elementSize, flexLayout} from "../../../assets/styles/Mixin";
 
 export const CouponWrap = styled.div`
-  height: 40px;
-  padding-top: 20px;
-
-  div {
-    ${elementSize("70%", "80%")}
-    border: black 1px solid;
+    display: flex;
+    border: #E7E6FA 1px solid;
     margin: 0 auto;
-    border-radius: 5px;
-  }
+    border-radius: 8px;
+    background-color: #F8F7FE;
+    cursor: pointer;
+    ${elementSize("65%", "40px")}
+`
+
+export const CouponContent = styled.div`
+    align-content: center;
+    width: 85%;
+    padding-left: 20px;
+    height: 100%;
+    border-right: 3px dotted #E7E6FA;
+`
+export const DownloadImgWrap = styled.div`
+    width: 15%;
+    ${flexLayout("center", "center")}
 `
