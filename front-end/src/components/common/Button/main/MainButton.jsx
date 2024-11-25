93b8b667cd1804d2a338d7a5b546b2b6fd01aeca
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 01:53:26 +0900

    Feat: 재사용성 고려해 버튼 컴포넌트 세분화

--- front-end/src/components/common/Button/main/MainButton.jsx
+++ front-end/src/components/common/Button/main/MainButton.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import {MainButtonLayout} from "./MainButtonLayout";
+import {MainButtonLayout, SubButtonLayout, TransButtonLayout} from "./MainButtonLayout";
+import {MiddleSizeFont} from "../../../../assets/styles/CommonStyle";
 
-export function MainButton({type, text, onClick}) {
+export function MainBtn({type, text, onClick}) {
     return (
         <MainButtonLayout type={type} onClick={onClick}>
-            <span className={"common-text"}> {text}</span>
+            <MiddleSizeFont> {text}</MiddleSizeFont>
         </MainButtonLayout>
+    );
+}
+
+export function SubBtn({type, text, onClick}) {
+    return (
+        <SubButtonLayout type={type} onClick={onClick}>
+            <MiddleSizeFont color={"white"}> {text}</MiddleSizeFont>
+        </SubButtonLayout>
+    );
+}
+
+export function TransBtn({type, text, onClick}) {
+    return (
+        <TransButtonLayout type={type} onClick={onClick}>
+            <MiddleSizeFont> {text}</MiddleSizeFont>
+        </TransButtonLayout>
     );
 }
