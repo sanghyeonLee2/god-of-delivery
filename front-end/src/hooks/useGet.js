f39e1dae74a75a6f28fb7417c88089bd5757a06b
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 02:08:21 +0900

    Feat: 데이터 캐싱 시간 추가

--- front-end/src/hooks/useGet.js
+++ front-end/src/hooks/useGet.js
@@ -5,6 +5,10 @@ export const useGet = (url) => {
     const {data, isError, status, isLoading} = useQuery(
         ["getData", url],  // 쿼리 키를 고유하게 만들기 위해 url 포함
         () => getApi(url), // queryFn
+        {
+            staleTime: 1000 * 60 * 5, // 5분 동안 데이터가 신선한 상태로 유지됨
+            cacheTime: 1000 * 60 * 10, // 10분 동안 캐시에 유지
+        }
     );
     return [data, isError, status, isLoading];
 }
