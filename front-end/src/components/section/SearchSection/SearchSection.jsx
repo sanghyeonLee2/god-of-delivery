7f7c8acc9f570bd21cede14aced3f3846336c9dd
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 01:59:38 +0900

    Design: 중간 크기의 텍스트 컴포넌트 추가

--- front-end/src/components/section/SearchSection/SearchSection.jsx
+++ front-end/src/components/section/SearchSection/SearchSection.jsx
@@ -3,11 +3,11 @@ import {
     HeaderMainText,
     HeaderTextWrap,
     SearchBoxInner,
-    SearchBoxMainText,
     SearchBoxOuter,
     SearchBoxTextOuter
 } from "./SearchSectionLayout";
 import SearchForm from "../../forms/SearchForm/SearchForm";
+import {MiddleSizeFont} from "../../../assets/styles/CommonStyle";
 
 function SearchSection(props) {
     return (
@@ -18,9 +18,9 @@ function SearchSection(props) {
                         <HeaderMainText>
                             헤더 메인 텍스트 입니다
                         </HeaderMainText>
-                        <SearchBoxMainText>
+                        <MiddleSizeFont color={"white"}>
                             배달 받으실 위치를 입력해 주세요
-                        </SearchBoxMainText>
+                        </MiddleSizeFont>
                     </HeaderTextWrap>
                 </SearchBoxTextOuter>
                 <SearchForm/>
