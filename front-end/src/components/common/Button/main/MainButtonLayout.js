398dd2f95b25cee87ead5759e1a8a6a3414bdfd2
sanghyeonLee <hyeon012366@gmail.com>
2024-11-26 01:53:48 +0900

    Design: 세분화된 버튼 레이아웃 수정

--- front-end/src/components/common/Button/main/MainButtonLayout.js
+++ front-end/src/components/common/Button/main/MainButtonLayout.js
@@ -1,9 +1,28 @@
 import styled from "styled-components";
 import {elementSize} from "../../../../assets/styles/Mixin";
+import {COLORS} from "../../../../assets/styles/colors";
 
 export const MainButtonLayout = styled.button`
-  ${elementSize("130px", "45px")}
-  background-color: #FF4F6D;
-  border-style: none;
-  border-radius: 4px;
+    ${elementSize("130px", "45px")}
+    background-color: white;
+    border: 1px solid ${COLORS.BTN.MAIN};
+    border-radius: 4px;
 `
+
+export const SubButtonLayout = styled.button`
+    ${elementSize("100%", "100%")}
+    background-color: ${COLORS.BTN.SUB};
+    border-radius: 4px;
+`
+
+export const TransButtonLayout = styled.button`
+    ${elementSize("100%", "100%")}
+    border-radius: 4px;
+    border: 0;
+    font-size: inherit;
+    background-color: white;
+    box-sizing: content-box;
+`
+export const OrderBtnWrap = styled.div`
+    margin-top: 20px;
+    height: 50px`
