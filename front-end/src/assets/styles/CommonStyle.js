5052a3909e3ab2d5ceb6da7f3ebce7d1149253b5
sanghyeonLee <hyeon012366@gmail.com>
2024-11-29 08:48:58 +0900

    Feat: 폰트 스타일 매개변수 조건무 등록해 가변 폰트 생성

--- front-end/src/assets/styles/CommonStyle.js
+++ front-end/src/assets/styles/CommonStyle.js
@@ -1,43 +1,20 @@
 import styled, {css} from "styled-components";
 
-
-export const P = styled.p`
-    font-size: ${(props) => props.size}
-`
-
-export const MiddleSizeFont = styled.span`
+export const Font = styled.p`
     color: ${(props) => props.color};
+    font-size: ${(props) => {
+        switch (props.size) {
+            case "small":
+                return "smaller"
+            case "large":
+                return "20px"
+            case "x-large":
+                return "28px"
+            default:
+                return "normal"
+        }
 
-`
-export const SmallSizeFont = styled.span`
-    font-size: smaller;
-    color: ${(props) => props.color};
-
-`
-export const MiddleSizeTitleFont = styled.div`
-    font-size: 25px;
-
-    p {
-        align-content: center;
-    }
-
-`
-export const LargeSizeTitleFont = styled.div`
-    font-size: 35px;
-
-    p {
-        height: 70px;
-        align-content: center;
-    }
-`
-export const SmallSizeTitleFont = styled.div`
-    font-size: 20px;
-
-    p {
-        height: 50px;
-        align-content: center;
-    }
-`
+    }};`
 export const VerticalSpace = styled.div`
     height: 10px;
     background-color: #F6F6F6;
@@ -65,9 +42,9 @@ export const FlexOnly = styled(({element: Element = 'div', ...props}) => (
     <Element {...props} />
 ))`
     display: flex;
+    align-items: center;
 `;
 //기본적으로 전달되는 props가 있음
-
 export const exampleColor = () => css`background-color: lightgray`
 export const outerPadding = () => css`padding: 15px 25px;`
 export const innerPadding = () => css`padding: 10px 15px;`
