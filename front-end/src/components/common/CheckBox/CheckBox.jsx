66290c565d829a529cd99e2b218c7813f7d84a7f
sanghyeonLee <hyeon012366@gmail.com>
2024-12-04 22:04:29 +0900

    Feat: 체크박스 컴포넌트 추가 및 조건부 체크된 요소 관련 로직 추가

---
+++ front-end/src/components/common/CheckBox/CheckBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function CheckBox({children, value, name, getValues, setValue, maxQuantity}) {
+    const currentValues = getValues(name) || [];
+    return (
+        <label>
+            <input type={'checkbox'}
+                   name={name}
+                   onChange={(e) => {
+                       const isChecked = e.target.checked;
+                       if (isChecked && getValues(name).length >= maxQuantity) {
+                           e.target.checked = false
+                           return
+                       }
+                       if (isChecked) {
+                           console.log("getValues", getValues(name))
+                           return setValue(name, [...currentValues, value])
+                       }
+                       console.log("Asd")
+                       return setValue(name, currentValues.filter((item) => item.content !== value.content))
+                   }}
+            />
+            {children}
+        </label>
+    );
+}
+
+export default CheckBox;
