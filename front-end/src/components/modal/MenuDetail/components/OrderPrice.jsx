680da64a5320d41c70879a4980d6474b52c9badf
sanghyeonLee <hyeon012366@gmail.com>
2024-12-04 22:11:18 +0900

    Feat: 메뉴 상세의 총 주문금액 관련 로직 추가

--- front-end/src/components/modal/MenuDetail/components/OrderPrice.jsx
+++ front-end/src/components/modal/MenuDetail/components/OrderPrice.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import {Font} from "../../../../assets/styles/CommonStyle";
 import {OrderPriceWrap} from "./ModalComponentsLayout";
 
-function OrderPrice({price}) {
+function OrderPrice({watch, defaultPrice = 0}) {
+    const orderPrice = watch("quantity") * (defaultPrice +
+        Object.values(watch()).reduce((acc, option) => {
+            if (option?.price) {
+                acc += option.price
+            }
+            if (Array.isArray(option)) {
+                acc += option.reduce((detailAcc, detailOption) => {
+                    return detailAcc + detailOption.price
+                }, 0)
+            }
+            return acc
+        }, 0))
     return (
         <OrderPriceWrap>
             <Font>총 주문금액</Font>
             <Font color={"#116EDB"}>
-                {price.toLocaleString()}원
+                {orderPrice.toLocaleString()}원
             </Font>
         </OrderPriceWrap>
     );
