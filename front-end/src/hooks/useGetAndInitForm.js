6f4c9560d8becb9318116f61fc5f13c650b37cef
sanghyeonLee <hyeon012366@gmail.com>
2024-12-14 23:57:47 +0900

    Feat: 쿼리 비동기 요청 성공시 폼 데이터 저장 기능 동기적 실행 위한 react-query, react-hook-form 을 결합 커스텀 훅 구현

---
+++ front-end/src/hooks/useGetAndInitForm.js
@@ -0,0 +1,26 @@
+import {useQuery} from "react-query";
+import {getApi} from "../apis/api/user";
+import {useForm} from "react-hook-form";
+
+export const useGetAndInitForm = (url) => {
+    const {register, reset, watch, getValues, setValue} = useForm()
+    const {data, isError, status, isLoading} = useQuery(
+        ["getData", url],  // 쿼리 키를 고유하게 만들기 위해 url 포함
+        () => getApi(url),
+        {
+            onSuccess: response => {
+                reset({
+                        receiptMethod: "배달",
+                        tip: 0,
+                        menus: response.data?.menus
+                    }
+                )
+            },
+            staleTime: 1000 * 60 * 5, // 5분 동안 데이터가 신선한 상태로 유지됨
+            cacheTime: 1000 * 60 * 10, // 10분 동안 캐시에 유지
+        }
+    );
+
+    return {query: {data: data?.data, isError, status, isLoading}, form: {data: register, getValues, setValue, watch}};
+}
+export default useGetAndInitForm
