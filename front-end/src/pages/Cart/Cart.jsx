b7ace01e152c237f28d78711fa7f2ee57c053ab2
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 22:01:15 +0900

    Feat: 수정된 커스텀 훅 사용한 장바구니 로직 수정 및 Controller 추가

--- front-end/src/pages/Cart/Cart.jsx
+++ front-end/src/pages/Cart/Cart.jsx
@@ -6,30 +6,36 @@ import {OrderBtnWrap} from "../../components/common/Button/main/MainButtonLayout
 import CartPayment from "./components/CartPayment";
 import CartReceiptMethod from "./components/CartReceiptMethod";
 import CartMenus from "./components/CartMenus";
-import useGet from "../../hooks/useGet";
+import {useGetAndInitForm} from "../../hooks/useGetAndInitForm";
+import Loading from "../../components/common/Loading/Loading";
 
 function Cart(props) {
-    const [data, isError, status, isLoading] = useGet("cart")
-    return (
-        <CartWrap>
+    const {query, form} = useGetAndInitForm("cart")
+    if (query.isLoading) {
+        return <Loading/>;
+    }
+    console.log(form.watch())
+    return (<CartWrap>
             <CartHeader>
                 <MinStoreInfoWrap>
-                    <Font size={"x-large"}>{data.data?.storeTitle}</Font>
-                    <Font size={"small"} color={"gray"}>{data.data?.deliveryTime} 후 도착예정</Font>
+                    <Font size={"x-large"}>{query.data?.storeTitle}</Font>
+                    <Font size={"small"} color={"gray"}>{query.data?.deliveryTime} 후 도착예정</Font>
                     {/*쿠폰 컴포넌트 추가*/}
                 </MinStoreInfoWrap>
             </CartHeader>
-            <CartMenus menus={data.data?.menus}/>
-            <MenuAddBtnWrap>
-                <TransBtn text={"메뉴 추가"}/>
-            </MenuAddBtnWrap>
-            <CartReceiptMethod/>
-            <CartPayment/>
-            <OrderBtnWrap>
-                <SubBtn text={"12000원 결제하기"}/>
-            </OrderBtnWrap>
-        </CartWrap>
-    );
+            <form onSubmit={form.handleSubmit((data) => console.log(data))}>
+                <CartMenus menus={query.data?.menus} form={form}/>
+                <MenuAddBtnWrap>
+                    <TransBtn text={"메뉴 추가"}/>
+                </MenuAddBtnWrap>
+                <CartReceiptMethod getValues={form.getValues} control={form.control}
+                                   receiptMethods={query.data?.receiptMethods}/>
+                <CartPayment form={form}/>
+                <OrderBtnWrap>
+                    <SubBtn text={"결제하기"}/>
+                </OrderBtnWrap>
+            </form>
+        </CartWrap>);
 }
 
 export default Cart;
