f0e8fd3dd32e008b031b1091778e10c1653e8f32
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 21:58:16 +0900

    Feat: Controller 를 사용한 폼 데이터 가공을 위한 control, getValues, setValues 추가

--- front-end/src/pages/Cart/components/CartReceiptMethod.jsx
+++ front-end/src/pages/Cart/components/CartReceiptMethod.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import {FlexOnly, Font} from "../../../assets/styles/CommonStyle";
 import {MethodReceiptBox} from "./CartReceiptMethodLayout";
+import RadioGroup from "../../../components/common/RadioGroup/RadioGroup";
+import Radio from "../../../components/common/RadioGroup/Radio/Radio";
+import {Controller} from "react-hook-form";
 
-function CartReceiptMethod(props) {
+function CartReceiptMethod({receiptMethods, getValues, control}) {
     return (
         <>
             <Font size={"large"}>수령방법을 선택해주세요</Font>
-            <ul>
-                <MethodReceiptBox>
-                    <FlexOnly>
-                        <Font>
-                            배달
-                        </Font>
-                        &nbsp;
-                        <Font size={"small"} color={"gray"}>10분~20분</Font>
-                    </FlexOnly>
-                    <Font>
-                        2000원
-                    </Font>
-                </MethodReceiptBox>
-                <MethodReceiptBox>
-                    <FlexOnly>
-                        <Font>
-                            포장
-                        </Font>
-                        &nbsp;
-                        <Font size={"small"} color={"gray"}>10분~20분</Font>
-                    </FlexOnly>
-                    <Font>
-                        2000원
-                    </Font>
-                </MethodReceiptBox>
-            </ul>
+            <Controller control={control} name={"receiptMethods"} defaultValue={receiptMethods[0].receiptMethodType}
+                        render={({field: {onChange, value}}) => (
+                            <RadioGroup isOtherCheckStyle={true}>
+                                {receiptMethods.map((receiptMethod) =>
+                                    <Radio value={receiptMethod} key={receiptMethod.receiptMethodType}
+                                           onChange={() => onChange(receiptMethod)}
+                                           checked={value === receiptMethod.receiptMethodType}>
+                                        <MethodReceiptBox
+                                            isChecked={receiptMethod.receiptMethodType === getValues(`receiptMethods.receiptMethodType`)}>
+                                            <FlexOnly>
+                                                <Font>
+                                                    {receiptMethod.receiptMethodType}
+                                                </Font>
+                                                &nbsp;
+                                                <Font size={"small"} color={"gray"}>{receiptMethod.waiting}</Font>
+                                            </FlexOnly>
+                                            <Font>
+                                                {receiptMethod.tip.toLocaleString()}원
+                                            </Font>
+                                        </MethodReceiptBox>
+                                    </Radio>
+                                )}
+                            </RadioGroup>
+                        )
+                        }/>
         </>
     );
 }
