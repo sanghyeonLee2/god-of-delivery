d2f96f15c75856323c5386ae5c7a96c5f162e0a1
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 22:01:37 +0900

    Refactor: 모달 전역 상태 객체로 수정

--- front-end/src/recoil/flag/atoms.js
+++ front-end/src/recoil/flag/atoms.js
@@ -2,7 +2,7 @@ import {atom} from "recoil";
 
 export const isModalOpenState = atom({
     key: "isModalOpen",
-    default: false
+    default: {modalType: null, modalFlag: false},
 })
 export const modalDataState = atom({
     key: "modalData",
