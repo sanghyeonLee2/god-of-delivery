275e8bffbdf822e6c268d6ac5d543316891da44a
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 21:48:48 +0900

    Design: 체크 공간 없는 체크박스 스타일을 위한 가변 스타일 추가

--- front-end/src/components/common/RadioGroup/RadioGroup.jsx
+++ front-end/src/components/common/RadioGroup/RadioGroup.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from "styled-components";
 
-function RadioGroup({children}) {
+function RadioGroup({children, isOtherCheckStyle = false}) {
     return (
-        <div>
+        <RadioGroupWrap isOtherCheckStyle={isOtherCheckStyle}>
             {children}
-        </div>
+        </RadioGroupWrap>
     );
 }
 
 export default RadioGroup;
+
+const RadioGroupWrap = styled.div.withConfig(
+    {shouldForwardProp: (prop) => prop !== 'isOtherCheckStyle',})`
+    input {
+        display: ${(props) => props.isOtherCheckStyle && "none"}
+    }`
