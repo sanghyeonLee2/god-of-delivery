06364c76ab2620840b40b5cb709445a3d8029d1f
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 22:00:04 +0900

    Design: 장바구니 메뉴에 대한 추가 로직 작성

--- front-end/src/pages/Cart/components/CartMenus.jsx
+++ front-end/src/pages/Cart/components/CartMenus.jsx
@@ -1,32 +1,60 @@
 import React from 'react';
 import {Font} from "../../../assets/styles/CommonStyle";
-import {MainBtn} from "../../../components/common/Button/main/MainButton";
-import {CartMenuBox, MenuOptionImg, MenuOptionWrap, OptionBtnInner, OptionBtnOuter} from "./CartMenusLayout";
+import {MainBtn, TransBtn} from "../../../components/common/Button/main/MainButton";
+import {
+    CartMenuBox,
+    ChgQuantityWrap,
+    MenuOptionImg,
+    MenuOptionLeft,
+    MenuOptionWrap,
+    OptionBtnInner,
+    OptionBtnOuter
+} from "./CartMenusLayout";
+import {useSetRecoilState} from "recoil";
+import {isModalOpenState} from "../../../recoil/flag/atoms";
 
-function CartMenus(props) {
+function CartMenus({menus, form, register,}) {
+    const setModalData = () => {
+    }
+    const setIsModalOpen = useSetRecoilState(isModalOpenState);
     return (
         <CartMenuBox>
-            <MenuOptionWrap>
-                <ul>
+            {menus?.map((menu, idx) =>
+                <MenuOptionWrap key={menu.menuName}>
                     <li>
-                        <Font>후라이드 치킨</Font>
+                        <MenuOptionLeft>
+                            <div>
+                                <Font>{menu.menuName}</Font>
+                                <Font size={"small"} color={"gray"}>치킨무 주세요 치킨 양념 주세요</Font>
+                                <Font>{(menu.price * form.getValues(`menus[${idx}].quantity`)).toLocaleString()}원</Font>
+                            </div>
+                            <MenuOptionImg/>
+                        </MenuOptionLeft>
                     </li>
+                    <li>
+                        <OptionBtnOuter>
+                            <OptionBtnInner>
+                                <ChgQuantityWrap text={"삭제/수량 변경"}>
+                                    <TransBtn text={"-"} onClick={() => {
+                                        form.setValue(`menus[${idx}].quantity`, form.getValues(`menus[${idx}].quantity`) - 1)
+                                    }}/>
+                                    <div {...register}>{form.watch(`menus[${idx}].quantity`)}</div>
+                                    <TransBtn text={"+"}
+                                              onClick={() => {
+                                                  form.setValue(`menus[${idx}].quantity`, form.getValues(`menus[${idx}].quantity`) + 1)
+                                              }}/>
+                                </ChgQuantityWrap>
+                                <MainBtn text={"옵션 변경"}
+                                         onClick={() => setIsModalOpen({
+                                             modalType: "updateCartMenu",
+                                             modalFlag: true
+                                         })}/>
+                            </OptionBtnInner>
+                        </OptionBtnOuter>
+                    </li>
+                </MenuOptionWrap>
+            )}
 
-                    <li>
-                        <Font size={"small"} color={"gray"}>치킨무 주세요 치킨 양념 주세요</Font>
-                    </li>
-                    <li>
-                        <Font>3000원</Font>
-                    </li>
-                </ul>
-                <MenuOptionImg/>
-            </MenuOptionWrap>
-            <OptionBtnOuter>
-                <OptionBtnInner>
-                    <MainBtn text={"삭제/수량 변경"}/>
-                    <MainBtn text={"옵션 변경"}/>
-                </OptionBtnInner>
-            </OptionBtnOuter>
         </CartMenuBox>
     );
 }
