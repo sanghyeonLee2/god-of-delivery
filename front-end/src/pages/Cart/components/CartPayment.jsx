15f009de4f07ec6cc6ceb317fb565f233f965d46
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 21:58:54 +0900

    Feat: 총합 금액 및 각각의 메뉴 금액 보여주는 로직 추가

--- front-end/src/pages/Cart/components/CartPayment.jsx
+++ front-end/src/pages/Cart/components/CartPayment.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import {Font} from "../../../assets/styles/CommonStyle";
 import {PaymentBox, PriceWrap} from "./CartPaymentLayout";
 
-function CartPayment(props) {
+function CartPayment({form}) {
     return (
         <>
             <Font size={"large"}>결제금액을 확인해주세요</Font>
             <PaymentBox>
                 <PriceWrap>
                     <Font>주문금액</Font>
-                    <Font>10000원</Font>
+                    <Font>{form?.getValues("menus").reduce((acc, menu) => {
+                        return acc + menu?.price * menu?.quantity
+                    }, 0).toLocaleString()}원</Font>
                 </PriceWrap>
                 <PriceWrap>
                     <Font>배달팁</Font>
-                    <Font>10000원</Font>
+                    <Font>{form?.getValues("tip").toLocaleString()}원</Font>
                 </PriceWrap>
                 <PriceWrap>
                     <Font size={"x-large"}>결제 예정 금액</Font>
-                    <Font size={"x-large"}>10000원</Font>
+                    <Font size={"x-large"}>{(form?.getValues("menus").reduce((acc, menu) => {
+                        return acc + menu?.price * menu?.quantity
+                    }, 0) + form?.getValues("tip")).toLocaleString()}원</Font>
                 </PriceWrap>
             </PaymentBox>
         </>
