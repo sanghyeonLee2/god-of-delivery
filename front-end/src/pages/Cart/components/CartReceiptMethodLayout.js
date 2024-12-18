00e6b5794f60f6b533e1ae8920dbbd368e1ab736
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 21:57:01 +0900

    Fix: 스타일드 컴포넌트에서 돔에 데이터 추가되어 발생하는 오류 수정 위해 shouldForwardProp 속성으로 "checked" 속성 돔 추가 배제

--- front-end/src/pages/Cart/components/CartReceiptMethodLayout.js
+++ front-end/src/pages/Cart/components/CartReceiptMethodLayout.js
@@ -1,11 +1,15 @@
 import {flexLayout, setBorder} from "../../../assets/styles/Mixin";
 import {outerPadding} from "../../../assets/styles/CommonStyle";
 import styled from "styled-components";
+import {COLORS} from "../../../assets/styles/colors";
 
-export const MethodReceiptBox = styled.li`
+export const MethodReceiptBox = styled.li.withConfig({
+    shouldForwardProp: (prop) => prop !== 'isChecked',
+})`
+    background-color: ${(props) => props.isChecked && COLORS.BORDER};
     ${flexLayout("space-between", "center")};
     ${setBorder()};
     ${outerPadding()};
     height: 60px;
     margin-bottom: 20px;
-`
+`;
