d1bffb9ad2d6ab186c5d8a06268ea3b0f1e90705
sanghyeonLee <hyeon012366@gmail.com>
2024-12-18 21:59:20 +0900

    Design: 수량 변경 공간 스타일 추가

--- front-end/src/pages/Cart/components/CartMenusLayout.js
+++ front-end/src/pages/Cart/components/CartMenusLayout.js
@@ -3,13 +3,18 @@ import {elementSize, flexLayout, setBorder} from "../../../assets/styles/Mixin";
 import {exampleColor, outerPadding} from "../../../assets/styles/CommonStyle";
 
 export const CartMenuBox = styled.div`
-    ${setBorder()}
-    ${outerPadding()};
-    border-bottom-left-radius: 0;
+    ${setBorder()};
+    min-height: 100px;
     border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
     border-bottom: none;
-    min-height: 100px`
-export const MenuOptionWrap = styled.div`
+`
+
+export const MenuOptionWrap = styled.ul`
+    ${outerPadding()};
+    border-bottom: 1px solid gray;
+`
+export const MenuOptionLeft = styled.div`
     ${flexLayout("space-between")};
 `
 export const MenuOptionImg = styled.div`
@@ -26,4 +31,18 @@ export const OptionBtnInner = styled.div`
     width: 275px;
     ${flexLayout("space-between")};
     flex-direction: row-reverse;
+`
+export const ChgQuantityWrap = styled.div`
+    display: flex;
+    ${elementSize("130px", "45px")};
+
+    button {
+        width: 35%;
+    }
+
+    div {
+        align-content: center;
+        text-align: center;
+        width: 30%;
+    }
 `
