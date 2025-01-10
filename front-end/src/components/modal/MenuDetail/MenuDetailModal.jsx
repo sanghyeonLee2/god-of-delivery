b6baf0297b7de93479a3574fa4d8650171a39f50
SanghyeonLee <hyeon012366@gmail.com>
2025-01-10 23:36:56 +0900

    Refactor: Controller 사용을 위한 코드 수정 및 useGetMenuDetailAndInitForm 사용

--- front-end/src/components/modal/MenuDetail/MenuDetailModal.jsx
+++ front-end/src/components/modal/MenuDetail/MenuDetailModal.jsx
@@ -1,34 +1,22 @@
 import React from 'react';
 import {ModalContentWrap, ModalForm, ModalInner, ModalOuter,} from "../ModalLayout";
 import {Font} from "../../../assets/styles/CommonStyle";
-import {useRecoilValue} from "recoil";
-import {modalDataState} from "../../../recoil/flag/atoms";
 import {SubBtn, TransBtn} from "../../common/Button/main/MainButton";
 import MenuDetailOptions from "./components/MenuDetailOptions";
 import ModalHeader from "./components/ModalHeader";
 import OrderPrice from "./components/OrderPrice";
 import {MenuDetailBtnWrap, MenuDetailTextWrap, SelectQuantityWrap} from "./MenuDetailModalLayout";
 import MenuDetailProlog from "./components/MenuDetailProlog";
-import {useForm} from "react-hook-form";
-import {setMenuDetail} from "../../../utils/defaultValues";
 import useClick from "../../../hooks/useClick";
 import {usePost} from "../../../hooks/usePost";
+import useGetMenuDetailAndInitForm from "../../../hooks/useGetMenuDetailAndInitForm";
+import Loading from "../../common/Loading/Loading";
 
 function MenuDetailModal({modalType}) {
-    const modalData = useRecoilValue(modalDataState)
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        getValues,
-        watch
-    } = useForm({
-        defaultValues: setMenuDetail(modalData.details),
-    },)
-
+    const {form, query} = useGetMenuDetailAndInitForm("menuDetails")
     const quantityOnChg = (operand) => {
-        const newQuantity = getValues("quantity") + Number(operand)
-        return newQuantity >= 1 && setValue("quantity", newQuantity);
+        const newQuantity = form.getValues("quantity") + Number(operand)
+        return newQuantity >= 1 && form.setValue("quantity", newQuantity);
     };
 
     const minusBtnRef = useClick(() => {
@@ -39,36 +27,41 @@ function MenuDetailModal({modalType}) {
         quantityOnChg(1);
     });
     const {mutate: handlePutInCart} = usePost("cart-post")
-
-    //리액트쿼리 사용해서 post
+    if (query.isLoading) {
+        return <Loading/>
+    }
     return (
         <ModalOuter>
             <ModalInner>
                 <ModalHeader title={"메뉴 상세"}/>
                 <ModalContentWrap>
-                    <MenuDetailProlog name={modalData.name} description={modalData.description}/>
+                    <MenuDetailProlog name={query.data?.name} description={query.data?.description}/>
                     <MenuDetailTextWrap>
                         <Font>가격</Font>
-                        <Font>{modalData.price.toLocaleString()}원</Font>
+                        <Font>{query.data?.price.toLocaleString()}원</Font>
                     </MenuDetailTextWrap>
                     <ModalForm>
-                        <MenuDetailOptions details={modalData.details} register={register} setValue={setValue}
-                                           getValues={getValues}/>
+                        <MenuDetailOptions details={query.data?.details} register={form.register}
+                                           setValue={form.setValue}
+                                           getValues={form.getValues}
+                                           control={form.control}/>
                         <MenuDetailTextWrap>
                             <Font>수량</Font>
                             <SelectQuantityWrap>
                                 <TransBtn dataAction={-1} type={"button"} text={"-"} ref={minusBtnRef}/>
-                                <div>{getValues("quantity")}</div>
+                                <div>{form.getValues("quantity")}</div>
                                 <TransBtn dataAction={1} type={"button"} text={"+"} ref={plusBtnRef}/>
                             </SelectQuantityWrap>
                         </MenuDetailTextWrap>
                     </ModalForm>
-                    <OrderPrice defaultPrice={modalData.price} watch={watch}/>
+                    <OrderPrice defaultPrice={query.data?.price} watch={form.watch}/>
                 </ModalContentWrap>
                 <MenuDetailBtnWrap>
                     {modalType === "menuDetail" &&
                         <>
-                            <SubBtn text={"장바구니에 담기"} onClick={handleSubmit((data) => handlePutInCart(data))}/>
+                            <SubBtn text={"장바구니에 담기"}
+                                    onClick={form.handleSubmit((data) =>
+                                        handlePutInCart(data))}/>
                             <SubBtn text={"주문하기"}/>
                         </>}
                     {modalType === "updateCartMenu" &&
