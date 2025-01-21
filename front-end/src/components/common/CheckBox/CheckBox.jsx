6b8babf034677961a852ebf6263b8a1d4b343fbd
SanghyeonLee <hyeon012366@gmail.com>
2025-01-21 15:06:19 +0900

    Refactor: Controller 추가로 인한 기존 체크 로직 삭제 후 onchange만 추가

--- front-end/src/components/common/CheckBox/CheckBox.jsx
+++ front-end/src/components/common/CheckBox/CheckBox.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 
-function CheckBox({children, value, name, getValues, setValue, maxQuantity}) {
-    const currentValues = getValues(name) || [];
+function CheckBox({children, name, onChange}) {
+
     return (
         <label>
             <input type={'checkbox'}
                    name={name}
-                   onChange={(e) => {
-                       const isChecked = e.target.checked;
-                       if (isChecked && getValues(name).length >= maxQuantity) {
-                           e.target.checked = false
-                           return
-                       }
-                       if (isChecked) {
-                           return setValue(name, [...currentValues, value])
-                       }
-                       return setValue(name, currentValues.filter((item) => item.content !== value.content))
-                   }}
+                   onChange={onChange}
             />
             {children}
         </label>
