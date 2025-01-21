e365cc72374bb0584cbb3e5c172b3063fb352eff
SanghyeonLee <hyeon012366@gmail.com>
2025-01-21 15:08:41 +0900

    Refactor: 불필요한 checkbox 요소 삭제

--- front-end/src/components/modal/MenuDetail/components/MenuDetailOptions.jsx
+++ front-end/src/components/modal/MenuDetail/components/MenuDetailOptions.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {FlexOnly, Font} from "../../../../assets/styles/CommonStyle";
-import RadioGroup from "../../../common/RadioGroup/RadioGroup";
-import Radio from "../../../common/RadioGroup/Radio/Radio";
 import {MenuDetailOptionsWrap, OptionWrap} from "./ModalComponentsLayout";
 import {MenuDetailTextWrap} from "../MenuDetailModalLayout";
 import CheckBox from "../../../common/CheckBox/CheckBox";
+import {Controller} from "react-hook-form";
 
-function MenuDetailOptions({details, register, setValue, getValues}) {
+function MenuDetailOptions({details, control}) {
+    console.log(details)
     return (
         <>
             {details.map((detail) =>
@@ -15,44 +15,41 @@ function MenuDetailOptions({details, register, setValue, getValues}) {
                         <FlexOnly>
                             <Font>{detail.title}</Font>
                             &nbsp;&nbsp;
-                            {detail?.isEssential ? <Font size={"small"} color={"red"}>필수 선택</Font> :
+                            {detail?.isEssential ?
+                                <Font size={"small"} color={"red"}>{detail?.maxQuantity}개 필수 선택</Font> :
                                 <Font size={"small"}>최대{detail?.maxQuantity}개 선택가능</Font>}
                         </FlexOnly>
                     </MenuDetailTextWrap>
-                    {detail.isEssential ?
-                        <RadioGroup>
-                            {/*라디오가 아닌 체크박스를 만들어야한다 -> 커스텀 훅으로?*/}
-                            {detail.options.map((option, optionIdx) =>
-                                <OptionWrap key={option.content}>
-                                    <Radio value={option}
-                                           name={detail.title}
-                                           setValue={setValue}
-                                           defaultChecked={optionIdx === 0}>
-                                        &nbsp;&nbsp;{option.content}
-                                    </Radio>
-                                    <Font>
-                                        +{option.price.toLocaleString()}원
-                                    </Font>
-                                </OptionWrap>
-                            )}
-                        </RadioGroup> :
+                    {
                         <>
-                            {detail.options.map((option) =>
-                                <OptionWrap key={option.content}>
-                                    <CheckBox setValue={setValue}
-                                              value={option}
-                                              name={detail.title}
-                                              register={register}
-                                              getValues={getValues}
-                                              maxQuantity={detail.maxQuantity}>
-                                        &nbsp;&nbsp;{option.content}
-                                    </CheckBox>
-                                    <Font>
-                                        +{option.price.toLocaleString()}원
-                                    </Font>
-                                </OptionWrap>
-                            )
-                            }
+                            <Controller control={control} name={detail.title}
+                                        defaultValue={detail.options.filter(option => option.isChecked)}
+                                        render={(({field: {onChange, value}}) => (
+                                            detail.options.map((option) =>
+                                                <OptionWrap key={option.content}>
+
+                                                    <CheckBox
+                                                        value={option}
+                                                        onChange={(e) => {
+                                                            const isChecked = e.target.checked;
+                                                            /*if (isChecked && value.length >= detail.maxQuantity) {
+                                                                e.target.checked = false
+                                                                return
+                                                            }*/
+                                                            if (isChecked) {
+                                                                return onChange([...value, option])
+                                                            }
+                                                            return onChange(value.filter((item) => item.content !== option.content))
+                                                        }}>
+                                                        &nbsp;&nbsp;{option.content}
+                                                    </CheckBox>
+                                                    <Font>
+                                                        +{option.price.toLocaleString()}원
+                                                    </Font>
+                                                </OptionWrap>
+                                            )
+                                        ))}/>
+
                         </>
                     }
                 </MenuDetailOptionsWrap>)
