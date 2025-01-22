bc7a713be5fb29c9c007baaf4e43ad87e50cb2e7
sanghyeonLee <hyeon012366@gmail.com>
2025-01-23 01:36:55 +0900

    Feat: 폼 데이터 수정으로 인한 총합금액 계산 로직 수정

--- front-end/src/components/modal/MenuDetail/components/OrderPrice.jsx
+++ front-end/src/components/modal/MenuDetail/components/OrderPrice.jsx
@@ -3,18 +3,13 @@ import {Font} from "../../../../assets/styles/CommonStyle";
 import {OrderPriceWrap} from "./ModalComponentsLayout";
 
 function OrderPrice({watch, defaultPrice = 0}) {
-    const orderPrice = watch("quantity") * (defaultPrice +
-        Object.values(watch()).reduce((acc, option) => {
-            if (option?.price) {
-                acc += option.price
-            }
-            if (Array.isArray(option)) {
-                acc += option.reduce((detailAcc, detailOption) => {
-                    return detailAcc + detailOption.price
-                }, 0)
-            }
-            return acc
-        }, 0))
+    const orderPrice = watch("quantity") * (
+        defaultPrice +
+        Object.values(watch())
+            .filter((option) => Array.isArray(option) && option.length >= 1)
+            .flat()
+            .reduce((acc, option) => acc + (option.price || 0), 0)
+    );
     return (
         <OrderPriceWrap>
             <Font>총 주문금액</Font>
