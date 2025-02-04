8bf1d73d60b565bb655ea27bc04fd61909c3fb8d
sanghyeonLee <hyeon012366@gmail.com>
2025-02-04 21:00:34 +0900

    Refactor: 카트 정보 및 초기 폼 값 설정 커스텀훅 로직 데이터 구조에 맞게 수정

--- front-end/src/hooks/useGetCartAndInitForm.js
+++ front-end/src/hooks/useGetCartAndInitForm.js
@@ -2,28 +2,28 @@ import {useQuery} from "react-query";
 import {getApi} from "../apis/api/user";
 import {useForm} from "react-hook-form";
 
-export const useGetAndInitForm = (url) => {
+export const useGetCartAndInitForm = (url) => {
     const {handleSubmit, control, register, reset, watch, getValues, setValue} = useForm()
     const {data, isError, status, isLoading} = useQuery(
-        ["getData", url],  // 쿼리 키를 고유하게 만들기 위해 url 포함
+        ["getCart", url],  // 쿼리 키를 고유하게 만들기 위해 url 포함
         () => getApi(url),
         {
             onSuccess: response => {
+                console.log(response.data)
                 reset({
-                        receiptMethodType: response.data?.receiptMethods[0]?.receiptMethodType,
-                        tip: 0,
-                        menus: response.data?.menus
+                        cartId: response.data.cartId,
+                        receiptMethod: response.data.receiptMethods[0],
+                        menus: response.data?.menus,
+                        storeId: response.data.storeId,
                     }
                 )
             },
-            staleTime: 1000 * 60 * 5, // 5분 동안 데이터가 신선한 상태로 유지됨
             cacheTime: 1000 * 60 * 10, // 10분 동안 캐시에 유지
         }
     );
-
+    //쿼리로 받아오고 reset 메소드로 폼 초기값 설정 해야함
     return {
         query: {data: data?.data, isError, status, isLoading},
         form: {control, register, getValues, setValue, watch, handleSubmit}
     };
 }
-export default useGetAndInitForm
