615d045c998cc7f7d6acddf6d72ed610f44fbc2b
sanghyeonLee <hyeon012366@gmail.com>
2025-02-04 20:43:32 +0900

    Remove: 전역 상태 구조 수정

--- front-end/src/recoil/flag/atoms.js
+++ front-end/src/recoil/flag/atoms.js
@@ -2,9 +2,5 @@ import {atom} from "recoil";
 
 export const isModalOpenState = atom({
     key: "isModalOpen",
-    default: {modalType: null, modalFlag: false},
-})
-export const modalDataState = atom({
-    key: "modalData",
-    default: null
+    default: {modalType: null, modalFlag: false, modalIdData: null},
 })
