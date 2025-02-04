5e3477ae0f608ea20286da30c7e4f1216fe0233e
sanghyeonLee <hyeon012366@gmail.com>
2025-02-04 20:40:02 +0900

    Refactor: 주소 상세 컴포넌트 구조 수정

--- front-end/src/pages/SelectAddressPage/components/AddressInfo.jsx
+++ front-end/src/pages/SelectAddressPage/components/AddressInfo.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
 import MainInput from "../../../components/common/Input/MainInput/MainInput";
 import {ErrorMsg} from "../../AuthPage/AuthPageFormLayout";
-import Radio from "../../../components/common/RadioGroup/Radio/Radio";
-import RadioGroup from "../../../components/common/RadioGroup/RadioGroup";
 import {useFormContext} from "react-hook-form";
 import {useRecoilValue} from "recoil";
 import {addressState} from "../../../recoil/map/atoms";
-import {AddressInfoWrap, AddressP, RoadAddressStrong} from "./AddressInfoLayout";
+import {Font} from "../../../assets/styles/CommonStyle";
 
 function AddressInfo(props) {
     const {formState: {errors}} = useFormContext()
     const addressValue = useRecoilValue(addressState);
     return (
-        <AddressInfoWrap>
-            <RoadAddressStrong>
+        <div>
+            <Font>
                 {addressValue?.roadInfo}
-            </RoadAddressStrong>
-            <AddressP>
+            </Font>
+            <Font>
                 {addressValue?.detailAddress}
-            </AddressP>
+            </Font>
             <MainInput type={"text"} placeholder={"상세주소를 입력해 주세요."} reigterName={"detailAddress"}/>
             <ErrorMsg>{errors.detailAddress?.message}</ErrorMsg>
-            <RadioGroup>
-                <Radio name={"address-type"} value={"home"} defaultChecked>
-                    우리집
-                </Radio>
-                <Radio name={"address-type"} value={"company"}>
-                    회사
-                </Radio>
-                <Radio name={"address-type"} value={"direct"}>
-                    직접 입력
-                </Radio>
-            </RadioGroup>
-        </AddressInfoWrap>
+        </div>
     );
 }
 
