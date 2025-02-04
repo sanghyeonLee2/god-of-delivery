350660da0cc225f3c446adb78ffb52a1ff0229b8
sanghyeonLee <hyeon012366@gmail.com>
2025-02-04 20:50:19 +0900

    Refactor: 카트 금액 정보 컴포넌트 구조 수정

--- front-end/src/pages/Cart/components/CartPayment.jsx
+++ front-end/src/pages/Cart/components/CartPayment.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import {Font} from "../../../assets/styles/CommonStyle";
-import {PaymentBox, PriceWrap} from "./CartPaymentLayout";
+import {CommonBorder, FlexOnly, Font} from "../../../assets/styles/CommonStyle";
 
-function CartPayment({form}) {
+function CartPayment({getValues, watch}) {
     return (
         <>
             <Font size={"large"}>결제금액을 확인해주세요</Font>
-            <PaymentBox>
-                <PriceWrap>
+            <CommonBorder>
+                <FlexOnly justify={"space-between"}>
                     <Font>주문금액</Font>
-                    <Font>{form?.getValues("menus").reduce((acc, menu) => {
+                    <Font>{getValues("menus").reduce((acc, menu) => {
                         return acc + menu?.price * menu?.quantity
                     }, 0).toLocaleString()}원</Font>
-                </PriceWrap>
-                <PriceWrap>
+                </FlexOnly>
+                <FlexOnly justify={"space-between"}>
                     <Font>배달팁</Font>
-                    <Font>{form?.getValues("tip").toLocaleString()}원</Font>
-                </PriceWrap>
-                <PriceWrap>
+                    <Font>{watch("receiptMethod.tip").toLocaleString()}원</Font>
+                </FlexOnly>
+                <FlexOnly justify={"space-between"}>
                     <Font size={"x-large"}>결제 예정 금액</Font>
-                    <Font size={"x-large"}>{(form?.getValues("menus").reduce((acc, menu) => {
+                    <Font size={"x-large"}>{(getValues("menus").reduce((acc, menu) => {
                         return acc + menu?.price * menu?.quantity
-                    }, 0) + form?.getValues("tip")).toLocaleString()}원</Font>
-                </PriceWrap>
-            </PaymentBox>
+                    }, 0) + getValues("receiptMethod.tip")).toLocaleString()}원</Font>
+                </FlexOnly>
+            </CommonBorder>
         </>
     );
 }
