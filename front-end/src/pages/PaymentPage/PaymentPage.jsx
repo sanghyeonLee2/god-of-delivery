fe9e3bac8d6024a8dfc9e3cd2a3e78ad73d473b8
SanghyeonLee <hyeon012366@gmail.com>
2025-02-05 23:50:10 +0900

    Refactor: 레이아웃 구조 수정

--- front-end/src/pages/PaymentPage/PaymentPage.jsx
+++ front-end/src/pages/PaymentPage/PaymentPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {PaymentContent, PaymentHeader} from "./PaymentPageLayout";
-import {CommonPageWrap, Font} from "../../assets/styles/CommonStyle";
+import {CommonPageHeader, CommonPageWrap, CommonSectionWrap, Font} from "../../assets/styles/CommonStyle";
 import {SubBtn} from "../../components/common/Button/main/MainButton";
 import PaymentAmount from "./components/PaymentAmount";
 import {useLocation} from "react-router-dom";
@@ -19,31 +18,26 @@ function PaymentPage(props) {
     }
     return (
         <CommonPageWrap>
+            <CommonPageHeader>
+                <Font size={"large"}>{form.getValues("receiptMethod.receiptMethodType")}로 받을게요</Font>
+                <Font color={"gray"}>{form.getValues("receiptMethod.waiting")} 후 도착예정</Font>
+            </CommonPageHeader>
             <form onSubmit={form.handleSubmit((data) => handlePayment(data))}>
-                <PaymentContent>
-                    <PaymentHeader>
-                        <Font size={"large"}>{form.getValues("receiptMethod.receiptMethodType")}로 받을게요</Font>
-                        <Font color={"gray"}>{form.getValues("receiptMethod.waiting")} 후 도착예정</Font>
-                    </PaymentHeader>
-                    {form.getValues("receiptMethod.receiptMethodType") === "배달" && <>
-                        <PaymentInput title={"배달 주소"} value={form.getValues("currentAddress")}
-                                      register={form.register("currentAddress")} disabled={true}
-                        />
-                        <PaymentInput title={"상세 주소"} value={form.getValues("detailAddress")}
-                                      register={form.register("detailAddress")}
-                        />
-                    </>}
-                    <PaymentInput title={"주문시 요청사항"} value={form.getValues("requestedTerm")}
-                                  register={form.register("requestedTerm")}
-                    />
-                    <PaymentInput title={"내 연락처"} value={form.getValues("contact")}
-                                  register={form.register("contact")}
-                    />
-                    {/*<SelectCard control={form.control} cards={query.data.cards}/>*/}
-                    <PaymentMethods control={form.control} cards={query.data.cards} getValues={form.getValues}/>
-                    <PaymentAmount/>
-                </PaymentContent>
-                <SubBtn text={"결제하기"} height={"50px"}/>
+                {form.getValues("receiptMethod.receiptMethodType") === "배달" && <>
+                    <PaymentInput title={"배달 주소"} value={form.getValues("currentAddress")}
+                                  register={form.register("currentAddress")} disabled={true}/>
+                    <PaymentInput title={"상세 주소"} value={form.getValues("detailAddress")}
+                                  register={form.register("detailAddress")}/>
+                </>}
+                <PaymentInput title={"주문시 요청사항"} value={form.getValues("requestedTerm")}
+                              register={form.register("requestedTerm")}/>
+                <PaymentInput title={"내 연락처"} value={form.getValues("contact")}
+                              register={form.register("contact")}/>
+                <PaymentMethods control={form.control} cards={query.data.cards} getValues={form.getValues}/>
+                <PaymentAmount/>
+                <CommonSectionWrap>
+                    <SubBtn text={"결제하기"} height={"50px"}/>
+                </CommonSectionWrap>
             </form>
         </CommonPageWrap>
     )
