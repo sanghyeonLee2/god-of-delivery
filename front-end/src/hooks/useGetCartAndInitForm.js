4e0d1c1c1eecb1f627ce839b23fd239df001ec05
SanghyeonLee <hyeon012366@gmail.com>
2025-02-05 23:52:08 +0900

    Fix: watch 로 인한 데이터 불일치 문제 해결 위해 onSuccess 상태에서 콜백함수 실행 -> useEffect 사용 으로 수정

--- front-end/src/hooks/useGetCartAndInitForm.js
+++ front-end/src/hooks/useGetCartAndInitForm.js
@@ -1,29 +1,33 @@
 import {useQuery} from "react-query";
 import {getApi} from "../apis/api/user";
 import {useForm} from "react-hook-form";
+import {useEffect} from "react";
 
 export const useGetCartAndInitForm = (url) => {
-    const {handleSubmit, control, register, reset, watch, getValues, setValue} = useForm()
-    const {data, isError, status, isLoading} = useQuery(
-        ["getCart", url],  // 쿼리 키를 고유하게 만들기 위해 url 포함
+    const {handleSubmit, control, register, reset, watch, getValues, setValue} = useForm();
+    const {data, isError, status, isLoading, refetch} = useQuery(
+        ["getCart", url],
         () => getApi(url),
         {
-            onSuccess: response => {
-                console.log(response.data)
-                reset({
-                        cartId: response.data.cartId,
-                        receiptMethod: response.data.receiptMethods[0],
-                        menus: response.data?.menus,
-                        storeId: response.data.storeId,
-                    }
-                )
-            },
-            cacheTime: 1000 * 60 * 10, // 10분 동안 캐시에 유지
+            staleTime: 1000 * 60 * 5, // 5분 동안 신선한 데이터 유지
+            cacheTime: 1000 * 60 * 10, // 10분 동안 캐시 유지
         }
     );
-    //쿼리로 받아오고 reset 메소드로 폼 초기값 설정 해야함
+
+    useEffect(() => {
+        if (data?.data) {
+            reset({
+                cartId: data.data.cartId,
+                receiptMethod: data.data.receiptMethods[0],
+                menus: data.data?.menus,
+                storeId: data.data.storeId,
+            });
+        }
+    }, [data, reset]);
+
     return {
-        query: {data: data?.data, isError, status, isLoading},
-        form: {control, register, getValues, setValue, watch, handleSubmit}
+        query: {data: data?.data, isError, status, isLoading, refetch},
+        form: {control, register, getValues, setValue, watch, handleSubmit},
+        refetchCart: refetch,  // 외부에서 강제로 다시 데이터를 불러올 수 있도록 반환
     };
-}
+};
