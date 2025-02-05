ea64b60d7e65f53f0b0f92cb68c987c6e74c4efc
SanghyeonLee <hyeon012366@gmail.com>
2025-02-05 23:40:39 +0900

    Refactor: 제출 함수 인자 handleSubmit 의 매개변수로 변경

--- front-end/src/pages/CartPage/CartPage.jsx
+++ front-end/src/pages/CartPage/CartPage.jsx
@@ -3,12 +3,12 @@ import {CartHeader, MenuAddBtnWrap, MinStoreInfoWrap} from "./CartLayout";
 import {CommonPageWrap, Font} from "../../assets/styles/CommonStyle";
 import {SubBtn, TransBtn} from "../../components/common/Button/main/MainButton";
 import {OrderBtnWrap} from "../../components/common/Button/main/MainButtonLayout";
-import CartPayment from "./components/CartPayment";
 import CartReceiptMethod from "./components/CartReceiptMethod";
 import CartMenus from "./components/CartMenus";
 import {useGetCartAndInitForm} from "../../hooks/useGetCartAndInitForm";
 import Loading from "../../components/common/Loading/Loading";
 import {useNavigate} from "react-router-dom";
+import CartPayment from "./components/CartPayment";
 
 function CartPage(props) {
     const {query, form} = useGetCartAndInitForm("cart")
@@ -26,14 +26,14 @@ function CartPage(props) {
                 </MinStoreInfoWrap>
             </CartHeader>
             <form
-                onSubmit={form.handleSubmit((data) => navigate("/payment", {state: {orderInfo: form.getValues()}}))}>
-                <CartMenus menus={query.data?.menus} form={form}/>
+                onSubmit={form.handleSubmit((data) => navigate("/payment", {state: {orderInfo: data}}))}>
+                <CartMenus menus={query.data?.menus} getValues={form.getValues}/>
                 <MenuAddBtnWrap>
                     <TransBtn text={"메뉴 추가"} onClick={() => navigate(`/store/${query.data?.storeId}`)}/>
                 </MenuAddBtnWrap>
                 <CartReceiptMethod getValues={form.getValues} control={form.control}
                                    receiptMethods={query.data?.receiptMethods}/>
-                <CartPayment getValues={form.getValues} watch={form.watch}/>
+                <CartPayment getValues={form.getValues} control={form.control}/>
                 <OrderBtnWrap>
                     <SubBtn text={"결제하기"}/>
                 </OrderBtnWrap>
