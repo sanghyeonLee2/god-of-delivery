156c246d2a6f4f773e6cbb32f115169e69c83e35
SanghyeonLee <hyeon012366@gmail.com>
2025-02-05 23:44:42 +0900

    Remove: 불필요한 코드 삭제

--- front-end/src/components/common/Header/HeaderLayout.js
+++ front-end/src/components/common/Header/HeaderLayout.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import {elementSize, flexLayout} from "../../../assets/styles/Mixin";
-import {COLORS} from "../../../assets/styles/colors";
 import searchBoxBackground from "../../../assets/img/search-box-background.jpg";
 
 
@@ -9,16 +8,6 @@ export const HeaderWrap = styled.div`
     max-width: 1100px;
     margin: 0 auto;
     height: 92px;
-    padding: 0 10px;
-`
-
-export const CartWrap = styled.div`
-    cursor: pointer;
-    position: absolute;
-    background-color: ${COLORS.BTN.SUB};
-    border-radius: 35px;
-    ${elementSize("80px", "80px")};
-    ${flexLayout("center", "center")};
 `
 
 export const SearchBoxOuter = styled.div`
--- front-end/src/pages/PaymentPage/PaymentPageLayout.js
+++ front-end/src/pages/PaymentPage/PaymentPageLayout.js
@@ -1,15 +1,6 @@
 import styled from "styled-components";
-import {elementSize, flexLayout} from "../../assets/styles/Mixin";
+import {elementSize} from "../../assets/styles/Mixin";
 
-export const PaymentHeader = styled.div`
-    height: 60px;
-    ${flexLayout("space-between")}
-`
-export const PaymentContent = styled.ul`
-    li {
-        margin-bottom: 15px;
-    }
-`
 export const PaymentInputTextWrap = styled.div`
     input {
         ${elementSize("100%", "45px")};
