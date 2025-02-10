505e30897a0010c95cc1bbd0531a5a5969028a21
SanghyeonLee <hyeon012366@gmail.com>
2025-02-11 03:31:45 +0900

    Feat: 페이지네이션 컴포넌트 기능 구현

--- front-end/src/components/common/Pagination/Pagination.jsx
+++ front-end/src/components/common/Pagination/Pagination.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import {PageElement, PaginationInner, PaginationWrap} from "./PaginationLayout";
+import {PaginationInner, PaginationWrap} from "./PaginationLayout";
+import PaginationBtn from "./components/PaginationBtn";
+import IconBtn from "../Button/icon/IconBtn";
+import arrowLeft from "../../../assets/img/arrow_left.png"
+import arrowRight from "../../../assets/img/arrow_right.png"
+import arrowDoubleLeft from "../../../assets/img/double_arrow_left.png"
+import arrowDoubleRight from "../../../assets/img/double_arrow_right.png"
 
-function Pagination(props) {
+const PAGE_BTN_LIMIT = 5;
+
+function Pagination({currentPage, totalPages, setCurrentPage}) {
+    const startPage = Math.floor((currentPage - 1) / PAGE_BTN_LIMIT) * PAGE_BTN_LIMIT + 1;
+    const endPage = Math.min(startPage + PAGE_BTN_LIMIT - 1, totalPages);
+    const pages = Array.from({length: endPage - startPage + 1}, (_, i) => startPage + i);
     return (
         <PaginationWrap>
             <PaginationInner>
-                <PageElement>
-                    asd
-                </PageElement>
-                <PageElement>
-                    asd
-                </PageElement>
-                <PageElement>
-                    asd
-                </PageElement>
-                <PageElement>
-                    asd
-                </PageElement>
+                <IconBtn src={arrowDoubleLeft} isDisable={currentPage === 1} alt={arrowDoubleLeft} width={22}
+                         onClick={() => setCurrentPage(1)}/>
+                <IconBtn src={arrowLeft} isDisable={currentPage === 1} alt={arrowLeft} width={22}
+                         onClick={() => setCurrentPage(currentPage - 1)}/>
+                {pages.map((page) => <PaginationBtn key={page} page={page} clicked={page === currentPage}
+                                                    onClick={() => setCurrentPage(page)}/>)}
+                <IconBtn src={arrowRight} isDisable={currentPage === totalPages} alt={arrowRight} width={22}
+                         onClick={() => setCurrentPage(currentPage + 1)}/>
+                <IconBtn src={arrowDoubleRight} isDisable={endPage === totalPages} alt={arrowDoubleRight}
+                         onClick={() => setCurrentPage(totalPages)} width={22}/>
             </PaginationInner>
         </PaginationWrap>
     );
