c5c18307dca875a62bb22d027f4408182ad1b84c
SanghyeonLee <hyeon012366@gmail.com>
2025-02-11 03:28:06 +0900

    Refactor: 불필요 코드 삭제

--- front-end/src/pages/StoresPage/components/CategoryListLayout.js
+++ front-end/src/pages/StoresPage/components/CategoryListLayout.js
@@ -1,42 +1,32 @@
 import styled from "styled-components";
 import {elementSize, flexLayout} from "../../../assets/styles/Mixin";
+import {COLORS} from "../../../assets/styles/colors";
 
-export const CategoryListOuter = styled.div`
-    height: 75px;
-    border-bottom: 1px solid #d9d9d9;`
 
-export const CategoryListInner = styled.div`
+export const CategoryListWrap = styled.ul`
+    @media (max-width: 880px ) {
+        display: none;
+    }
     max-width: 1100px;
-    height: 100%;
-    margin: 0 auto;`
+    height: 75px;
+    margin: 0 auto;
+    ${flexLayout("center", "center")}`
 
-export const CategoryListUl = styled.ul`
-    ${flexLayout("center", "center")}
-    height: 100%;`
-export const CategoryListLi = styled.li`
-    ${elementSize("7.3%", "100%")};
+export const CategoryElement = styled.li`
+    ${elementSize("7.5%", "100%")};
     ${flexLayout("center", "center")};
-    background-color: ${(props) => props.id === props.clicked && "#333333"};
+    background-color: ${(props) => props.$clicked === props.id && COLORS.BTN.SUB};
 
-    span {
-        color: ${(props) => props.id === props.clicked && "white"}
+    p {
+        color: ${(props) => props.$clicked === props.id && "white"}
     }
 
     &:hover {
-        background-color: #333333;
+        background-color: ${COLORS.BTN.SUB};
 
-        span {
+        p {
             color: white;
         }
     }
-
-`
-export const SearchImg = styled.img`
-    border: lightgray 1px solid;
-    border-radius: 3px;
-`
-export const CategoryListSearchLi = styled.li`
-    ${flexLayout("center")}
-    width: 5%;
-`
+`;
 
