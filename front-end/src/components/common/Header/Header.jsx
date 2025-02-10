b4068809dbbbfe1c694c99a58a27ee64110d77ed
SanghyeonLee <hyeon012366@gmail.com>
2025-02-11 03:26:25 +0900

    Refactor: 컴포넌트 구조 수정

--- front-end/src/components/common/Header/Header.jsx
+++ front-end/src/components/common/Header/Header.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-    HeaderMainText,
-    HeaderTextWrap,
-    HeaderWrap,
-    SearchBoxInner,
-    SearchBoxOuter,
-    SearchBoxTextOuter
-} from "./HeaderLayout";
+import {HeaderWrap, SearchBoxOuter} from "./HeaderLayout";
 import {Link} from "react-router-dom";
 import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 import {isSignInState, userInfoState} from "../../../recoil/user/atoms";
@@ -17,6 +10,7 @@ import myPage from "../../../assets/img/my-page.png";
 import cart from "../../../assets/img/cart.png"
 import logout from "../../../assets/img/logout.png"
 import location_on from "../../../assets/img/location_on.png"
+import logo from "../../../assets/img/logo.png"
 import {isModalOpenState} from "../../../recoil/flag/atoms";
 
 export function Header({currentAddress}) {
@@ -27,15 +21,12 @@ export function Header({currentAddress}) {
         <>
             <HeaderWrap>
                 <Link to={"/"}>
-                    <h1>배달의 신</h1>
+                    <IconBtn src={logo} alt={logo} width={110}/>
                 </Link>
                 <FlexOnly>
-
                     <IconBtn src={location_on} width={26} onClick={
                         () => {
-                            setIsModalOpen(
-                                {modalType: "주소설정", modalFlag: true, modalIdData: null}
-                            )
+                            setIsModalOpen({modalType: "주소설정", modalFlag: true, modalIdData: null})
                         }}>
                         {userInfo.currentAddress ? <Font> {userInfo.currentAddress}</Font> :
                             <Font size={"small"}>주소를 설정해 주세요</Font>}
@@ -60,19 +51,15 @@ export function Header({currentAddress}) {
                 </FlexOnly>
             </HeaderWrap>
             <SearchBoxOuter>
-                <SearchBoxInner>
-                    <SearchBoxTextOuter>
-                        <HeaderTextWrap>
-                            <HeaderMainText>
-                                헤더 메인 텍스트 입니다
-                            </HeaderMainText>
-                            <Font color={"white"}>
-                                배달 받으실 위치를 입력해 주세요
-                            </Font>
-                        </HeaderTextWrap>
-                    </SearchBoxTextOuter>
-                    <SearchForm/>
-                </SearchBoxInner>
+                <section>
+                    <h2>
+                        헤더 메인 텍스트 입니다
+                    </h2>
+                    <Font color={"white"}>
+                        배달 받으실 위치를 입력해 주세요
+                    </Font>
+                </section>
+                <SearchForm/>
             </SearchBoxOuter>
         </>
     );
