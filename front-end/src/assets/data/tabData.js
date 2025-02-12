ffdec08e5ee45f0ec883fb31ffd992bfa7a6282e
SanghyeonLee <hyeon012366@gmail.com>
2025-02-12 21:44:08 +0900

    Feat: 탭 데이터 전역 객체로 분리

---
+++ front-end/src/assets/data/tabData.js
@@ -0,0 +1,28 @@
+export const TAB_CONTENTS = {
+    STORE: [
+        {
+            key: 0,
+            tab: "메뉴",
+        },
+        {
+            key: 1,
+            tab: "정보",
+        },
+        {
+            key: 2,
+            tab: "리뷰",
+        }
+    ],
+    ORDER_INFO: [
+        {
+            key: 0,
+            tab: "배달주문",
+            content: ["최소 주문금액", "배달시간", "배달팁"]
+        },
+        {
+            key: 1,
+            tab: "포장/방문주문",
+            content: ["할인", "최소 주문금액", "픽업시간", "위치안내"]
+        }
+    ]
+}
