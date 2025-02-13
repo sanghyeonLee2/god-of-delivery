cc5db775b7c8a4476c19e0f57bc904d6de74a46f
SanghyeonLee <hyeon012366@gmail.com>
2025-02-13 21:12:34 +0900

    Feat: stores 불러오는 커스텀 훅 추가 쿼리 파라미터로 데이터 가공 및 커스텀 네이게이트 함수 추가

--- front-end/src/hooks/useGetStores.js
+++ front-end/src/hooks/useGetStores.js
@@ -1,31 +1,58 @@
 import {useQuery} from "react-query";
 import {getApi} from "../apis/api/user";
-import {useState} from "react";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
+import {useEffect} from "react";
 
 export const useGetStores = (url) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const {data, isError, status, isLoading} = useQuery(
-        ["getStoresData", currentPage],  // 쿼리 키를 고유하게 만들기 위해 url 포함
-        () => getApi(`${url}/${currentPage}`),
+    const navigate = useNavigate();
+    const location = useLocation();
+    const {categoryId = "all"} = useParams();
+    const queryParams = new URLSearchParams(location.search);
+    const currentPage = queryParams.get('currentPage') || 1;
+    const sorting = queryParams.get('sorting') || 'basicAsc';
+    const apiUrl = `${url}/${categoryId}/${currentPage}/${sorting}`;
+
+    const {data, isError, status, isLoading, refetch} = useQuery(
+        ["getStoresData", categoryId, currentPage, sorting],
+        () => getApi(apiUrl),
         {
-            select: (res) => {
-                return {
-                    storesData: res.data.storeList,
-                    totalPages: Math.ceil(res.data.totalItems / res.data.pageSize),
-                }
-            },
-            staleTime: 1000 * 60 * 5, // 5분 동안 데이터가 신선한 상태로 유지됨
-            cacheTime: 1000 * 60 * 10, // 10분 동안 캐시에 유지
+            select: (res) => ({
+                storesData: res.data.storeList,
+                totalPages: Math.ceil(res.data.totalItems / res.data.pageSize),
+            }),
+            staleTime: 1000 * 60 * 5,
+            cacheTime: 1000 * 60 * 10
         }
     );
+
+    const setCategory = (newCategoryId) => {
+        navigate(`/stores/${newCategoryId}?currentPage=1&sorting=${sorting}`);
+    };
+
+    const setCurrentPage = (newPage) => {
+        navigate(`/stores/${categoryId}?currentPage=${newPage}&sorting=${sorting}`);
+    };
+
+    const setSorting = (newSorting) => {
+        navigate(`/stores/${categoryId}?currentPage=${currentPage}&sorting=${newSorting}`);
+    };
+
+    useEffect(() => {
+        refetch();
+    }, [categoryId, currentPage, sorting]);
+
     return {
         storesData: data?.storesData,
         totalPages: data?.totalPages,
+        setCategory,
+        setCurrentPage,
+        setSorting,
         isError,
-        status,
         isLoading,
-        currentPage,
-        setCurrentPage
+        currentPage: parseInt(currentPage, 10),
+        categoryId,
+        sorting,
     };
-}
-export default useGetStores
+};
+
+export default useGetStores;
--- front-end/src/pages/StoresPage/components/CategoryList.jsx
+++ front-end/src/pages/StoresPage/components/CategoryList.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import {CategoryElement, CategoryListWrap} from "./CategoryListLayout";
 import categoryDummy from "../../../assets/data/categoryDummy.json";
 import {Font} from "../../../assets/styles/CommonStyle";
-import {useNavigate} from "react-router-dom";
 
 
-function CategoryList({categoryId}) {
-    const navigate = useNavigate();
+function CategoryList({categoryId, setCategory}) {
     return (
         <CategoryListWrap>
-            {categoryDummy.map((e, idx) =>
-                <CategoryElement key={e.id} id={e.id} $clicked={categoryId}
-                                 onClick={() => navigate(`/category-info/${e.id}/${idx + 1}`, {state: {categoryId: e.id}})}>
-                    <Font size={"small"}>{e.id}</Font>
+            {categoryDummy.map((category) =>
+                <CategoryElement key={category.id} id={category.id} $clicked={categoryId}
+                                 onClick={() => setCategory(category.id)}>
+                    <Font size={"small"}>{category.name}</Font>
                 </CategoryElement>
             )}
         </CategoryListWrap>
