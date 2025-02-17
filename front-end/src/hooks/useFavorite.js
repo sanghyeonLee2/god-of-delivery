0d8c7d4faeced42b597a88127bbe94d3c9bf8bdb
SanghyeonLee <hyeon012366@gmail.com>
2025-02-17 23:51:25 +0900

    Feat: 찜 추가 / 삭제하는 커스텀 훅 추가

---
+++ front-end/src/hooks/useFavorite.js
@@ -0,0 +1,29 @@
+import {useMutation} from "react-query"
+import {authDeleteApi, authPostApi} from "../apis/api/user";
+import {useState} from "react";
+
+export const useFavorite = (url, dipsInit) => {
+    const [dipState, setDipState] = useState(dipsInit);
+    const handleToggleDip = useMutation(
+        async (storeId) => {
+            if (dipState.isDip) {
+                await authDeleteApi(`dip-delete/${storeId}`); // 기존 상태가 찜이면 삭제 요청
+            } else {
+                await authPostApi("dip-post", {storeId}); // 기존 상태가 찜이 아니면 추가 요청
+            }
+        }, {
+            onSuccess: () => {
+                setDipState(({isDip, dips}) => ({
+                    isDip: !isDip,
+                    dips: isDip ? dips - 1 : dips + 1,
+                }));
+                console.log("성공");
+            },
+            onError: () => {
+                console.log("실패")
+            },
+        })
+    return [handleToggleDip, dipState]
+};
+
+
