2529864da8318f7527e90edccd27a0f0ccd6b232
Justbeanpole <dbtjd7282@gmail.com>
2025-02-20 21:39:56 +0900

    feat: token서비스 파일 세분화

---
+++ back-end/services/token.service.js
@@ -0,0 +1,18 @@
+const Token = require("../models/token");
+
+exports.createToken = async(userId, refreshToken) => await Token.create({
+    userId: userId,
+    token: refreshToken,
+})
+
+exports.updateTokenForRefresh = async (userId, refreshToken) => await Token.update(
+    {
+        token: refreshToken
+    },
+    {
+        where:{userId:userId}
+    })
+
+exports.findById = async(userId) => await Token.findOne({
+    where:{userId: userId}
+})
