158799147837af2f2d42322f60399f92d6170887
Justbeanpole <dbtjd7282@gmail.com>
2025-02-20 21:38:34 +0900

    feat: 미들웨어 추가, 경로 변경, 가게 리스트/정보 받는 메서드 추가

--- back-end/routes/store.routes.js
+++ back-end/routes/store.routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
-const {getStores, postCreateStore} = require('../controllers/store.controller')
+const {getStoresList, postCreateStore, getStore} = require('../controllers/store.controller')
+const {verifyToken} = require("../middlewares/auth.middleware");
 
-router.get('/:city/:district/:category', getStores);
-router.post('/create', postCreateStore);
+
+
+router.get('/:category',[verifyToken], getStoresList);
+router.post('/create',[verifyToken], postCreateStore);
+router.post('/:storeId', getStore);
 
 module.exports = router;
