fd152399d4e33839ecddab66d87bc0d084a7b12c
Justbeanpole <dbtjd7282@gmail.com>
2025-02-21 12:09:13 +0900

    feat: 리뷰 업데이트 라우터

--- back-end/routes/review.routes.js
+++ back-end/routes/review.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const {verifyToken} = require("../middlewares/auth.middleware");
 
 const router = express.Router();
-const {postCreateReview} = require('../controllers/review.controller')
+const {postCreateReview, patchReview} = require('../controllers/review.controller')
 
 router.post('/create',[verifyToken], postCreateReview)
+router.patch('/:reviewId', [verifyToken], patchReview)
 
 module.exports = router;
