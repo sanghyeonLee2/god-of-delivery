90986ec6d40465c035566e4bdfa8e0628f755afb
Justbeanpole <dbtjd7282@gmail.com>
2025-02-21 12:08:57 +0900

    feat: 유저의 리뷰 리스트 응답 라우터

--- back-end/routes/user.routes.js
+++ back-end/routes/user.routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const {getCheckId, postSignUp, postSignIn , getRefreshReissued, getLatLng, postAddress} = require('../controllers/user.controller');
 const {verifyToken} = require("../middlewares/auth.middleware");
+const {getReviewList} = require("../controllers/review.controller");
 
 
 
@@ -12,5 +13,6 @@ router.post('/sign-in', postSignIn);
 router.post('/sign-in/reissue', getRefreshReissued);
 router.get('/me/location',[verifyToken], getLatLng)
 router.post('/me/address',[verifyToken], postAddress)
+router.get('/reviews',[verifyToken], getReviewList)
 
 module.exports = router;
