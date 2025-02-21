c655f910cfa75e2e284a2b4c6d9f4e2bb54aed25
Justbeanpole <dbtjd7282@gmail.com>
2025-02-21 11:47:07 +0900

    feat: 리뷰 생성 컨트롤러 구현

--- back-end/controllers/review.controller.js
+++ back-end/controllers/review.controller.js
@@ -11,5 +11,20 @@ exports.getReviewList = async(req, res) => {
             message: error.message
         })
     }
+}
 
+exports.postCreateReview = async (req, res) => {
+    try{
+        const data = await ReviewService.createReview(req);
+        res.status(201).send({
+            status: 201,
+            data: data
+        })
+    }
+    catch (error) {
+        res.status(500).send({
+            status: 500,
+            message: error.message
+        })
+    }
 }
