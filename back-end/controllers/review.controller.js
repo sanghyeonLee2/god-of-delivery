15e15be5784a11ca72adb950fbcb9b983fa5c0bd
Justbeanpole <dbtjd7282@gmail.com>
2025-02-21 12:09:46 +0900

    feat: 리뷰 업데이트 및 유저 리뷰 리스트 응답 구현

--- back-end/controllers/review.controller.js
+++ back-end/controllers/review.controller.js
@@ -2,7 +2,7 @@ const ReviewService = require('../services/review.service');
 
 exports.getReviewList = async(req, res) => {
     try{
-        const reviewData = await ReviewService.getReviewsByStoreId(req.params, req.query)
+        const reviewData = await ReviewService.findReviewsByStoreId(req.params, req.query)
         res.status(200).send(reviewData);
     }
     catch (error) {
@@ -19,6 +19,35 @@ exports.postCreateReview = async (req, res) => {
         res.status(201).send({
             status: 201,
             data: data
+        })
+    }
+    catch (error) {
+        res.status(500).send({
+            status: 500,
+            message: error.message
+        })
+    }
+}
+
+exports.getUserReview = async (req, res) => {
+    try {
+        const userReviews = await ReviewService.findReviewsByUserId(req)
+        res.status(200).send(userReviews);
+    }
+    catch (error) {
+        res.status(500).send({
+            status: 500,
+            message: error.message
+        })
+    }
+}
+
+exports.patchReview = async (req, res) => {
+    try{
+        const updated = await ReviewService.updateReview(req)
+        res.status(200).send({
+            status: 200,
+            data: updated
         })
     }
     catch (error) {
