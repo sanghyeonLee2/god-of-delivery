a7c1402c35b4903d02b314b3881eac10a00cceac
Justbeanpole <dbtjd7282@gmail.com>
2025-02-21 11:33:53 +0900

    feat: 리뷰리스트 응답 라우트

--- back-end/routes/store.routes.js
+++ back-end/routes/store.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const {getStoresList, postCreateStore, getStore} = require('../controllers/store.controller')
+const {getReviewList} = require('../controllers/review.controller')
 const {verifyToken} = require("../middlewares/auth.middleware");
 
 
@@ -9,5 +10,6 @@ const {verifyToken} = require("../middlewares/auth.middleware");
 router.get('/:category',[verifyToken], getStoresList);
 router.post('/create',[verifyToken], postCreateStore);
 router.post('/:storeId', getStore);
+router.get('/:storeId/reviews', getReviewList);
 
 module.exports = router;
