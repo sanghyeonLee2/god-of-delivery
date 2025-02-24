02bac2a060cf5e0210b7e00f5940731d4a08d3e9
SanghyeonLee <hyeon012366@gmail.com>
2025-02-24 23:57:20 +0900

    Design: 푸터 디자인 수정

--- front-end/src/components/common/Footer/FooterLayout.js
+++ front-end/src/components/common/Footer/FooterLayout.js
@@ -1,32 +1,17 @@
 import styled from "styled-components";
 import {flexLayout} from "../../../assets/styles/Mixin";
+import {innerPadding} from "../../../assets/styles/CommonStyle";
 
 
-export const FooterWrapper = styled.footer`
+export const FooterWrap = styled.footer`
+    ${innerPadding()};
     position: relative;
-    transform: translateY(-100%);
     border-top: 1px solid #d9d9d9;
+    height: 92px;
 `
-export const FooterMenuOuter = styled.div`
+export const FooterInner = styled.div`
     max-width: 1100px;
     margin: 0 auto;
-    ${flexLayout("space-between")};
-`
-export const FooterMenuLeft = styled.ul`
-    padding: 13px 10px;
-    width: 40%;
-    ${flexLayout("space-between")}
-`
-
-export const FooterMenuRight = styled.ul`
-    padding: 15px 10px;
-    width: 18%;
-    ${flexLayout("space-between")}
-`
-
-export const FooterInfoInner = styled.div`
-    max-width: 1100px;
-    ${flexLayout("space-between")}
-    margin: 0 auto;
-    padding: 13px 10px;
+    height: 100%;
+    ${flexLayout("space-between", "center")};
 `
