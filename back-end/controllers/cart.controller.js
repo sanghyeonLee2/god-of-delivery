6c7ba256c4606a8a8836be5cbd5186f3ecb07c40
Justbeanpole <dbtjd7282@gmail.com>
2025-02-26 15:05:18 +0900

    feat: 메뉴 상세 확인, 메뉴 변경사항 추가, 장바구니 삭제, 장바구니 내 메뉴 삭제 기능 구현

--- back-end/controllers/cart.controller.js
+++ back-end/controllers/cart.controller.js
@@ -1,4 +1,5 @@
 const CartService = require('../services/cart.service');
+const MenuService = require('../services/menu.service');
 
 exports.postAddCart = async (req, res) => {
     try {
@@ -21,6 +22,64 @@ exports.getCartData = async (req, res) => {
     try{
         const cartData = await CartService.findCartDataByUserId(req.userId)
         res.status(200).send(cartData);
+    }
+    catch (err){
+        res.status(500).send({
+            status: 500,
+            message: err.message
+        })
+    }
+}
+
+exports.getCartMenuDetail = async (req, res) => {
+    try{
+        const menuData = await MenuService.findById(req.params)
+        res.status(200).send(menuData);
+    }
+    catch (err){
+        res.status(500).send({
+            status: 500,
+            message: err.message
+        })
+    }
+}
+exports.postUpdateCartItem = async (req, res) => {
+    try{
+        const updateData = await CartService.updateCartItemOption(req,req.params,req.body);
+        res.status(200).send({
+            status: 200,
+            data: updateData
+        });
+    }
+    catch (err){
+        res.status(500).send({
+            status: 500,
+            message: err.message
+        })
+    }
+}
+
+exports.deleteCart= async (req, res) => {
+    try{
+        const delData = CartService.destroyCart(req.params)
+        res.status(200).send({
+            message: 'Success',
+        })
+    }
+    catch (err){
+        res.status(500).send({
+            status: 500,
+            message: err.message
+        })
+    }
+}
+
+exports.deleteCartItem = async (req, res) => {
+    try{
+        const delData = CartService.destroyCartItem(req.params)
+        res.status(200).send({
+            message: 'Success',
+        })
     }
     catch (err){
         res.status(500).send({
