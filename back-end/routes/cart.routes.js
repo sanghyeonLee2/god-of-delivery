0b0303128b0c782e2e78ae221e02bba874798a70
Justbeanpole <dbtjd7282@gmail.com>
2025-02-26 15:05:32 +0900

    feat: 메뉴 상세 확인, 메뉴 변경사항 추가, 장바구니 삭제, 장바구니 내 메뉴 삭제 기능 구현

--- back-end/routes/cart.routes.js
+++ back-end/routes/cart.routes.js
@@ -3,9 +3,13 @@ const {middleVerifyToken} = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
-const {postAddCart, getCartData} = require('../controllers/cart.controller');
+const {postAddCart, getCartData, getCartMenuDetail, deleteCart, postUpdateCartItem, deleteCartItem} = require('../controllers/cart.controller');
 
 router.post('/', [middleVerifyToken], postAddCart)
 router.get('/', [middleVerifyToken], getCartData);
+router.get('/:menuId',[middleVerifyToken], getCartMenuDetail);
+router.post('/:cartItemId', [middleVerifyToken], postUpdateCartItem)
+router.delete('/:cartId', [middleVerifyToken], deleteCart);
+router.delete('/:cartItemId',[middleVerifyToken], deleteCartItem);
 
 module.exports = router;
