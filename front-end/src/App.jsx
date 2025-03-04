e6fda1b5fd2606813a141d818955799a0696e857
SanghyeonLee <hyeon012366@gmail.com>
2025-03-04 23:53:34 +0900

    Feat: 리액트 쿼리 전역 에러 처리 로직 추가

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,39 @@
 import React, {useEffect} from "react";
 import {GlobalStyle} from "./assets/styles/GlobalStyle";
-import {Header} from "./components/common/Header/Header";
+import {Header} from "components/common/Header/Header";
 import {Outlet} from "react-router-dom";
 import Footer from "./components/common/Footer/Footer";
-import {useRecoilValue, useSetRecoilState} from "recoil";
+import {useRecoilState, useRecoilValue} from "recoil";
 import {isSignInState, userInfoState} from "./recoil/user/atoms";
 import {authGetApi} from "./apis/api/user";
 import {isModalOpenState} from "./recoil/flag/atoms";
 import DefaultModal from "./components/modal/DefaultModal";
 
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false,
+            onError: (error) => {
+                console.error("쿼리에서 에러 발생!", error);
+                window.location.href = "/error";  // ✅ 에러 발생 시 /error 페이지로 이동
+            },
+        },
+        mutations: {
+            onError: (error, variables) => {
+                console.error("변이에서 에러 발생!", error);
+                window.location.href = "/error";  // ✅ 에러 발생 시 /error 페이지로 이동
+            },
+        },
+    },
+});
+
+
 function App() {
-    //const [data, isError, status, isLoading,] = useAuthGet("me")
     const isSignIn = useRecoilValue(isSignInState)
     const isModalOpen = useRecoilValue(isModalOpenState)
-    const setUserInfo = useSetRecoilState(userInfoState)
+    const [userInfo, setUserInfo] = useRecoilState(userInfoState)
     useEffect(() => {
         const fetchData = async () => {
             const {data} = await authGetApi("me")
@@ -22,8 +42,9 @@ function App() {
         fetchData()
     }, [isSignIn, setUserInfo]);
 
+
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <div className="App">
                 <Header/>
                 <GlobalStyle/>
@@ -32,7 +53,7 @@ function App() {
             <Footer/>
             {isModalOpen.modalFlag &&
                 <DefaultModal modalType={isModalOpen.modalType} modalIdData={isModalOpen.modalIdData}/>}
-        </>
+        </QueryClientProvider>
     );
 }
 
