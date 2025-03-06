a07284047d83156472a0a8e0b7c4b9601e96380b
Justbeanpole <dbtjd7282@gmail.com>
2025-03-06 20:24:29 +0900

    feat: 유저 주문 조회

--- back-end/routes/user.routes.js
+++ back-end/routes/user.routes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {getLatLng, getAddress, getUserInfo} = require('../controllers/user.controller');
 const {middleVerifyToken} = require("../middlewares/auth.middleware");
 const {getUserReview} = require("../controllers/review.controller");
+const {getUserOrders} = require("../controllers/user.controller");
 
 router.get("/me",[middleVerifyToken], getUserInfo);
 router.get('/me/location',[middleVerifyToken], getLatLng)
 router.get('/me/address',[middleVerifyToken], getAddress)
 router.get('/me/reviews',[middleVerifyToken], getUserReview)
+router.get('/me/orders',[middleVerifyToken], getUserOrders)
 
 module.exports = router;
