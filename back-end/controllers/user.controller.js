599cbb52da93bde53aa87f65535637e23e701139
Justbeanpole <dbtjd7282@gmail.com>
2025-03-06 20:22:13 +0900

    feat: 유저 주문 조회 기능 구현

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const UserService = require("../services/user.service");
+const OrderService = require("../services/order.service");
 
 exports.getLatLng = async (req, res) => {
     try{
@@ -37,6 +38,22 @@ exports.getAddress = async (req, res) => {
             lng : userAddress.longitude,
             address : userAddress.address,
         })
+    }
+    catch (err) {
+        res.status(500).send({
+            status : 500,
+            message: err.message
+        })
+    }
+}
+
+exports.getUserOrders = async (req, res) => {
+    try{
+        const userOrderList = await OrderService.findUserOrder(req, req.query)
+        res.status(200).send({
+            page: Number(req.query.page),
+            totalItems: userOrderList.length,
+            userOrderList})
     }
     catch (err) {
         res.status(500).send({
