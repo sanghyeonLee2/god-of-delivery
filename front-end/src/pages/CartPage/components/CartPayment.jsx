bc54bed540d3474ba63b3b1c259e0543290117a8
SanghyeonLee <hyeon012366@gmail.com>
2025-03-07 18:54:12 +0900

    Refactor: 장바구니 결제 금액 ui 단순화

--- front-end/src/pages/CartPage/components/CartPayment.jsx
+++ front-end/src/pages/CartPage/components/CartPayment.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import {useWatch} from "react-hook-form";
 import {CommonBorder, FlexOnly, Font} from "../../../assets/styles/CommonStyle";
 
-function CartPayment({getValues, control}) {
-    const tip = useWatch({
-        control,
-        name: "receiptMethod.tip",
-        defaultValue: 0,
-    });
-
-    if (!getValues("receiptMethod")) {
-        return; // 데이터가 설정되기 전에는 렌더링하지 않음
-    }
+function CartPayment({totalCartMenuPrice, tips}) {
 
     return (
         <>
@@ -19,19 +9,16 @@ function CartPayment({getValues, control}) {
             <CommonBorder>
                 <FlexOnly justify={"space-between"}>
                     <Font>주문금액</Font>
-                    <Font>{getValues("menus")?.reduce((acc, menu) => {
-                        return acc + (menu?.price ?? 0) * (menu?.quantity ?? 0);
-                    }, 0).toLocaleString()}원</Font>
+                    <Font>{totalCartMenuPrice.toLocaleString()}원</Font>
                 </FlexOnly>
                 <FlexOnly justify={"space-between"}>
                     <Font>배달팁</Font>
-                    <Font>{tip.toLocaleString()}원</Font>
+                    <Font>{tips.toLocaleString()}원</Font>
                 </FlexOnly>
                 <FlexOnly justify={"space-between"}>
-                    <Font size={"x-large"}>결제 예정 금액</Font>
-                    <Font size={"x-large"}>{(getValues("menus")?.reduce((acc, menu) => {
-                        return acc + (menu?.price ?? 0) * (menu?.quantity ?? 0);
-                    }, 0) + tip).toLocaleString()}원</Font>
+                    <Font size={"large"}>결제 예정 금액</Font>
+                    <Font size={"large"}>배달 {(totalCartMenuPrice + tips).toLocaleString()}원 |
+                        포장 {totalCartMenuPrice.toLocaleString()}원</Font>
                 </FlexOnly>
             </CommonBorder>
         </>
