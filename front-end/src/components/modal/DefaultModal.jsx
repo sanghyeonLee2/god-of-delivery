9a8e79025820974d294026227eea64a80ca34b36
SanghyeonLee <hyeon012366@gmail.com>
2025-03-10 23:47:11 +0900

    Refactor: 프롭스 이름 수정

--- front-end/src/components/modal/DefaultModal.jsx
+++ front-end/src/components/modal/DefaultModal.jsx
@@ -4,13 +4,13 @@ import {ModalInner, ModalOuter} from "./ModalLayout";
 import MenuDetailModal from "./MenuDetail/MenuDetailModal";
 import SelectMapModal from "./SelectMap/SelectMapModal";
 
-function DefaultModal({modalType, modalIdData}) {
+function DefaultModal({modalType, apiUrl}) {
     return (
         <ModalOuter>
             <ModalInner>
                 <ModalHeader title={modalType}/>
                 {(modalType === "메뉴상세" || modalType === "메뉴수정") &&
-                    <MenuDetailModal menuId={modalIdData} modalType={modalType}/>}
+                    <MenuDetailModal apiUrl={apiUrl} modalType={modalType}/>}
                 {modalType === "주소설정" && <SelectMapModal/>}
             </ModalInner>
         </ModalOuter>
