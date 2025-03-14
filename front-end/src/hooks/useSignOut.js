cdbb61d4733432cd96bcab3fa6edc0bbe3dc499f
SanghyeonLee <hyeon012366@gmail.com>
2025-03-14 23:52:08 +0900

    Refactor: 로그아웃 커스텀 훅 생성

--- front-end/src/hooks/useSignOut.js
+++ front-end/src/hooks/useSignOut.js
@@ -1,13 +1,18 @@
 import {useSetRecoilState} from "recoil";
 import {isSignInState} from "../recoil/user/atoms";
 import {useNavigate} from "react-router-dom";
+import {useQueryClient} from "react-query";
+import {QUERY_KEYS} from "../apis/constants/queryKeys";
+import {API_URLS} from "../apis/constants/urls";
 
 const useSignOut = () => {
+    const queryClient = useQueryClient();
     const navigate = useNavigate();
     const setIsSignIn = useSetRecoilState(isSignInState)
-    return () => {
+    return async () => {
         localStorage.removeItem("access-token")
         localStorage.removeItem("refresh-token")
+        await queryClient.removeQueries([QUERY_KEYS.ME, API_URLS.GET_ME])
         setIsSignIn(false)
         navigate("/")
     };
