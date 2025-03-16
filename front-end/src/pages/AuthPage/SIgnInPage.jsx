1cd23969f6024e0dccc2d7a64e25b0acb438abe8
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:35:20 +0900

    Refactor: ui 수정

--- front-end/src/pages/AuthPage/SIgnInPage.jsx
+++ front-end/src/pages/AuthPage/SIgnInPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {AuthPageInner, AuthPageOuter} from "./components/AuthPageLayout";
+import {AuthPageInner, AuthPageOuter} from "./AuthPageLayout";
 import SignInForm from "./components/SignInForm";
+import {Font} from "../../assets/styles/CommonStyle";
+import {Link} from "react-router-dom";
 
 function SignInPage(props) {
     return (
@@ -8,6 +10,16 @@ function SignInPage(props) {
             <AuthPageInner>
                 <h1>로그인</h1>
                 <SignInForm/>
+                <div>
+                    <Font color={"gray"}>
+                        계정이 없으신가요?
+                    </Font>
+                    <Link to="/sign-up">
+                        <Font>
+                            회원가입
+                        </Font>
+                    </Link>
+                </div>
             </AuthPageInner>
         </AuthPageOuter>
     );
