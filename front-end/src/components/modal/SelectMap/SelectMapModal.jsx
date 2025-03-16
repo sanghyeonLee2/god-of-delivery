eccff3907fa28e011dbe71a2931144fe7f0f6431
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:34:48 +0900

    Refactor: 불필요 코드 삭제 및 커스텀 훅 수정으로 인한 코드 리펙토링

--- front-end/src/components/modal/SelectMap/SelectMapModal.jsx
+++ front-end/src/components/modal/SelectMap/SelectMapModal.jsx
@@ -1,45 +1,34 @@
 import React from 'react';
 import {MapAddressForm} from "./SelectMapModalLayout";
 import LocationSearchForm from "../../forms/SearchForm/LocationSearchForm"
-import {ModalBtn} from "../../common/Button/main/MainButton";
-import {usePost} from "../../../hooks/usePost";
 import AddressInfo from "./AddressInfo";
 import {ModalContentWrap} from "../ModalLayout";
-import {useKakaoLoader} from "react-kakao-maps-sdk";
-import Loading from "../../common/Loading/Loading";
 import KakaoMap from "../../kakaoMap/KakaoMap";
-import {useGetCoordsAndInitForm} from "../../../hooks/useGetCoordsAndInitForm";
+import SelectMapSubmitBtn from "components/modal/SelectMap/SelectMapSubmitBtn";
+import {useKakaoLoader} from "react-kakao-maps-sdk";
+import Loading from "components/common/Loading/Loading";
 
 function SelectMapModal(props) {
-    const {query, form} = useGetCoordsAndInitForm("coords")
-    const {mutate: onAddressRegister} = usePost("address", true)
     const [loading, error] = useKakaoLoader({
         appkey: process.env.REACT_APP_KAKAO_API, // 발급 받은 APPKEY
         libraries: ["services"],
     })
-
-    if (query.isLoading) {
-        return <Loading/>
-    }
     if (loading) {
         return <Loading/>
     }
     if (error) {
         return <div>카카오 api를 불러오는 중 에러 발생</div>
     }
-
     return (
         <>
             <ModalContentWrap>
                 <LocationSearchForm/>
                 <KakaoMap/>
                 <MapAddressForm>
-                    <AddressInfo setValue={form.setValue}/>
+                    <AddressInfo/>
                 </MapAddressForm>
             </ModalContentWrap>
-            <ModalBtn onClick={form.handleSubmit((data) =>
-                onAddressRegister(data)
-            )} text={"등록"}/>
+            <SelectMapSubmitBtn/>
         </>
     );
 }
