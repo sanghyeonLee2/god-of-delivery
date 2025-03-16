15e827fe65fa876ef83737db64824a2ffe7214ac
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 21:03:53 +0900

    Feat: 사장님 리뷰 생성 컴포넌트 추가

---
+++ front-end/src/components/modal/CreateOwnerReview/CreateOwnerReview.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {ModalContentWrap} from "components/modal/ModalLayout";
+import {ModalReviewForm} from "components/modal/CreateReview/CreateReviewLayout";
+import {ModalBtn} from "components/common/Button/main/MainButton";
+import ReviewMain from "components/common/Review/components/ReviewMain";
+import Textarea from "components/common/TextArea/TextArea";
+import {useCreateOwnerReview} from "../../../hooks/useCreateOwnerReview";
+
+function CreateOwnerReview({modalData}) {
+    const {
+        register,
+        handleSubmit,
+        handleCreateOwnerReview,
+        isCreatingOwnerReview
+    } = useCreateOwnerReview(modalData.reviewId);
+    return (
+        <>
+            <ModalContentWrap>
+                <div style={{width: '80%', margin: '0 auto'}}>
+                    <ReviewMain review={modalData}/>
+                    <ModalReviewForm>
+                        <Textarea {...register("content")} placeholder={"리뷰를 작성 해주세요"}/>
+                    </ModalReviewForm>
+                </div>
+            </ModalContentWrap>
+            <ModalBtn text={"등록"} onClick={
+                handleSubmit(
+                    (data) => handleCreateOwnerReview(data)
+                )}/>
+        </>
+    );
+}
+
+export default CreateOwnerReview;
