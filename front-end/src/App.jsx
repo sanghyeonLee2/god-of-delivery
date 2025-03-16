2929571fcc0dd033103bb81c67d71a908cbe1124
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:17:55 +0900

    Refactor: useAuth 사용

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,47 +1,35 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {GlobalStyle} from "./assets/styles/GlobalStyle";
 import {Header} from "components/common/Header/Header";
 import {Outlet} from "react-router-dom";
 import Footer from "./components/common/Footer/Footer";
-import {useRecoilState, useRecoilValue} from "recoil";
-import {isSignInState, userInfoState} from "./recoil/user/atoms";
-import {authGetApi} from "./apis/api/user";
+import {useRecoilValue} from "recoil";
 import {isModalOpenState} from "./recoil/flag/atoms";
 import DefaultModal from "./components/modal/DefaultModal";
 
 import {QueryClient, QueryClientProvider} from 'react-query';
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: false,
-            onError: (error) => {
-                console.error("쿼리에서 에러 발생!", error);
-                window.location.href = "/error";  // ✅ 에러 발생 시 /error 페이지로 이동
-            },
-        },
-        mutations: {
-            onError: (error, variables) => {
-                console.error("변이에서 에러 발생!", error);
-                window.location.href = "/error";  // ✅ 에러 발생 시 /error 페이지로 이동
-            },
-        },
-    },
+    /*  defaultOptions: {
+          queries: {
+              retry: false,
+              onError: (error) => {
+                  console.error("쿼리에서 에러 발생!", error);
+                  window.location.href = "/error";  // ✅ 에러 발생 시 /error 페이지로 이동
+              },
+          },
+          mutations: {
+              onError: (error, variables) => {
+                  console.error("변이에서 에러 발생!", error);
+                  window.location.href = "/error";  // ✅ 에러 발생 시 /error 페이지로 이동
+              },
+          },
+      },*/
 });
 
 
 function App() {
-    const isSignIn = useRecoilValue(isSignInState)
     const isModalOpen = useRecoilValue(isModalOpenState)
-    const [userInfo, setUserInfo] = useRecoilState(userInfoState)
-    useEffect(() => {
-        const fetchData = async () => {
-            const {data} = await authGetApi("me")
-            setUserInfo(data)
-        }
-        fetchData()
-    }, [isSignIn, setUserInfo]);
-
 
     return (
         <QueryClientProvider client={queryClient}>
@@ -52,7 +40,7 @@ function App() {
             </div>
             <Footer/>
             {isModalOpen.flag &&
-                <DefaultModal modalType={isModalOpen.modalType} apiUrl={isModalOpen.apiUrl}/>}
+                <DefaultModal modalType={isModalOpen.modalType} modalData={isModalOpen.modalData}/>}
         </QueryClientProvider>
     );
 }
