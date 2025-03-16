dcaf868e8421f1be675d33f38ab4f05d2bf81c17
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:54:23 +0900

    Refactor: 응답 객체 데이터 수정에 따른 코드 수정

--- front-end/src/pages/OrderPage/components/UserPaymentInfo.jsx
+++ front-end/src/pages/OrderPage/components/UserPaymentInfo.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {CommonSectionWrap, DividingLine, Font} from "../../../assets/styles/CommonStyle";
 
-function UserPaymentInfo({userPaymentInfo, orderType}) {
+function UserPaymentInfo({userPaymentInfo}) {
     return (
         <div>
             <CommonSectionWrap>
-                {orderType === "배달" ? <><Font>배달 주소</Font>
+                {userPaymentInfo.orderType === "Delivery" ? <><Font>배달 주소</Font>
                     <Font size={"small"} color={"gray"}>
-                        {userPaymentInfo?.userAddress}
+                        {userPaymentInfo?.addressSnapshot}
                     </Font>
                     <Font size={"small"} color={"gray"}>
-                        {userPaymentInfo?.userDetailAddress}
+                        {userPaymentInfo?.detailAddressSnapshot}
                     </Font>
                 </> : <Font>
                     포장주문 입니다
@@ -20,14 +20,14 @@ function UserPaymentInfo({userPaymentInfo, orderType}) {
             <CommonSectionWrap>
                 <Font>전화번호</Font>
                 <Font size={"small"} color={"gray"}>
-                    {userPaymentInfo?.userContact}
+                    {userPaymentInfo?.contact}
                 </Font>
             </CommonSectionWrap>
             <DividingLine/>
             <CommonSectionWrap>
                 <Font>주문시 요청사항</Font>
                 <Font size={"small"} color={"gray"}>
-                    {userPaymentInfo?.userContact}
+                    {userPaymentInfo?.requests}
                 </Font>
             </CommonSectionWrap>
             <DividingLine/>
