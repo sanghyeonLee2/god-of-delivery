ef2771d796f45e142b7b1d56720e1c358dbf1e6e
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:36:49 +0900

    Refactor: useDibs 로 좋아요 관련 커스텀 훅 수정 및 사용

--- front-end/src/pages/StorePage/components/StoreHeader.jsx
+++ front-end/src/pages/StorePage/components/StoreHeader.jsx
@@ -2,33 +2,29 @@ import React from 'react';
 import {ReviewWrap, StoreHeaderWrap} from "./StoreHeaderLayout";
 import StarRatings from "react-star-ratings/build/star-ratings";
 import {Font} from "../../../assets/styles/CommonStyle";
+import {useDibs} from "../../../hooks/useDibs";
 import IconBtn from "../../../components/common/Button/icon/IconBtn";
-import favorite from "../../../assets/img/favorite.png";
 import favoriteFill from "../../../assets/img/favorite_fill.png";
-import {useFavorite} from "../../../hooks/useFavorite";
+import favorite from "../../../assets/img/favorite.png";
 
 function StoreHeader({storeHeader, storeId}) {
-    const [handleToggleDip, dipState] = useFavorite("dip", {isDip: storeHeader?.isDip, dips: storeHeader?.dips})
+    const {handleToggleDib, isLoading} = useDibs()
     return (
         <StoreHeaderWrap>
             <Font size={"x-large"}>{storeHeader?.storeName}</Font>
             <div>
-                <StarRatings
-                    rating={storeHeader?.rating}
-                    starRatedColor={"gold"}
-                    starDimension={"30px"}
-                />
+                <StarRatings rating={storeHeader?.rating} starRatedColor={"gold"} starDimension={"30px"}/>
                 &nbsp;&nbsp;
-                <span>{storeHeader?.rating}</span>
+                <span>{storeHeader?.rating.toFixed(1)}</span>
             </div>
             <ReviewWrap>
                 <IconBtn
-                    src={dipState?.isDip ? favoriteFill : favorite}
-                    alt={dipState?.isDip ? favoriteFill : favorite}
+                    src={storeHeader?.isDib ? favoriteFill : favorite}
+                    alt={storeHeader?.isDib ? "찜한 상태" : "찜 안한 상태"}
                     width={19}
-                    onClick={() => handleToggleDip.mutate(storeId)} // 클릭 시 찜 상태 변경
+                    onClick={() => handleToggleDib({storeId, isDib: storeHeader?.isDib})}
                 />
-                &nbsp;<Font>{dipState?.dips}</Font>&nbsp;|&nbsp;
+                &nbsp;<Font>{storeHeader?.dibs}</Font>&nbsp;|&nbsp;
                 <Font>리뷰 수 {storeHeader?.reviewCnt}</Font>&nbsp;|&nbsp;
                 <Font>사장님 댓글 수 {storeHeader?.ownerReviewCnt}</Font>
             </ReviewWrap>
