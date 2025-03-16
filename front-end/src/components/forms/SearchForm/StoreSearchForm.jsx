971ec8ef4be0a9c44378061bcfd8603ca1b31b37
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:58:45 +0900

    Refactor: 검색 시 useGetStores 훅 사용

--- front-end/src/components/forms/SearchForm/StoreSearchForm.jsx
+++ front-end/src/components/forms/SearchForm/StoreSearchForm.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Input from "../../common/Input/Input";
 import {SubBtn} from "../../common/Button/main/MainButton";
 import {SearchInputForm} from "./SearchFormLayout";
-import {useForm} from "react-hook-form";
 import useGetStores from "../../../hooks/useGetStores";
 
 function StoreSearchForm() {
-    const {setKeyword} = useGetStores("stores")
-    const {handleSubmit, register} = useForm()
+    const {setKeyword, searchForm} = useGetStores(false)
     return (
-        <SearchInputForm onSubmit={handleSubmit((data) => setKeyword(data.keyword))}>
-            <Input type={"text"} register={register("keyword")} placeholder={"음식점을 검색해 보세요"}/>
+        <SearchInputForm onSubmit={searchForm.handleSubmit((data) => setKeyword(data.keyword))}>
+            <Input type={"text"} register={searchForm.register("keyword")} placeholder={"음식점을 검색해 보세요"}/>
             <SubBtn type={"submit"} text={"검색"}/>
         </SearchInputForm>
     );
