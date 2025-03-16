0599b2a6bba32e9387286b967a5112748a6ea9f3
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 21:04:58 +0900

    Feat: 모달 타입에 따른 새로운 모달 추가

--- front-end/src/components/modal/DefaultModal.jsx
+++ front-end/src/components/modal/DefaultModal.jsx
@@ -3,15 +3,19 @@ import ModalHeader from "./MenuDetail/components/ModalHeader";
 import {ModalInner, ModalOuter} from "./ModalLayout";
 import MenuDetailModal from "./MenuDetail/MenuDetailModal";
 import SelectMapModal from "./SelectMap/SelectMapModal";
+import CreateReview from "components/modal/CreateReview/CreateReview";
+import CreateOwnerReview from "components/modal/CreateOwnerReview/CreateOwnerReview";
 
-function DefaultModal({modalType, apiUrl}) {
+function DefaultModal({modalType, modalData}) {
     return (
         <ModalOuter>
             <ModalInner>
                 <ModalHeader title={modalType}/>
                 {(modalType === "메뉴상세" || modalType === "메뉴수정") &&
-                    <MenuDetailModal apiUrl={apiUrl} modalType={modalType}/>}
+                    <MenuDetailModal api={modalData.api} modalType={modalType}/>}
                 {modalType === "주소설정" && <SelectMapModal/>}
+                {modalType === "리뷰작성" && <CreateReview modalData={modalData}/>}
+                {modalType === "사장님 리뷰작성" && <CreateOwnerReview modalData={modalData}/>}
             </ModalInner>
         </ModalOuter>
     );
