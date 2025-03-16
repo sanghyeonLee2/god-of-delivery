5409862d879db7175c9f6a25e091565728ecdbfd
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:20:49 +0900

    Design: 헤더 wrap -> outer, inner 사용

--- front-end/src/components/common/Header/HeaderLayout.js
+++ front-end/src/components/common/Header/HeaderLayout.js
@@ -1,16 +1,27 @@
 import styled from "styled-components";
-import {flexLayout} from "../../../assets/styles/Mixin";
+import {flexLayout, setBorder} from "../../../assets/styles/Mixin";
 import searchBoxBackground from "../../../assets/img/search-box-background.jpg";
 import {innerPadding} from "../../../assets/styles/CommonStyle";
 
 
-export const HeaderWrap = styled.div`
+export const HeaderInner = styled.div`
     ${flexLayout("space-between", "center")};
     max-width: 1100px;
     margin: 0 auto;
     height: 92px;
-    ${innerPadding()}
+    ${innerPadding()};
+`;
+
+export const HeaderOuter = styled.div`
+    border-bottom: 1px solid #d9d9d9;
+    background-color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
 `
+
 export const SearchBoxOuter = styled.div`
     height: 260px;
     background-image: url(${searchBoxBackground});
@@ -27,5 +38,39 @@ export const SearchBoxOuter = styled.div`
     section {
         text-align: center;
         padding-bottom: 10px;
+    }
+`
+export const UserMenuWrap = styled.div`
+    position: relative;
+
+    ul {
+        display: none;
+        border: 1px solid black;
+        right: 0; /* 우측 정렬 */
+        width: 110px;
+        background-color: white;
+        position: absolute;
+        top: 100%;
+        box-shadow: 0 4px 5px rgba(0, 0, 0, 0.3);
+        ${setBorder()};
+        overflow: hidden;
+    }
+
+    li {
+        border-bottom: 1px solid gray;
+        text-align: center;
+        height: 60px;
+    }
+
+    p {
+        font-size: 11px;
+    }
+
+    li:last-child {
+        border-bottom: none;
+    }
+
+    li:hover {
+        background-color: #f0f0f0;
     }
 `
