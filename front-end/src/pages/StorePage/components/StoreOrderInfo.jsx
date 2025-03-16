b510eedbfdee4fd64e10bc499257e37cc64ed6f8
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:38:07 +0900

    Refactor: api 응답 객체 수정으로 인한 코드 리펙토링

--- front-end/src/pages/StorePage/components/StoreOrderInfo.jsx
+++ front-end/src/pages/StorePage/components/StoreOrderInfo.jsx
@@ -4,14 +4,14 @@ import useTab from "../../../hooks/useTab";
 import {TAB_CONTENTS} from "../../../assets/data/tabData";
 import {FixedTextInterval, Font} from "../../../assets/styles/CommonStyle";
 
-function StoreOrderInfo({deliveryInfo, takeoutInfo}) {
+function StoreOrderInfo({deliveryInfo, takeoutInfo, address}) {
     const {currentItem, setCurrentItem} = useTab(0, TAB_CONTENTS.ORDER_INFO)
     return (
         <div>
             <OrderTabWrap>
-                {TAB_CONTENTS.ORDER_INFO.map((content, idx) =>
-                    <li key={content.key} onClick={() => setCurrentItem(idx)}>
-                        <OrderTypeText $isOn={currentItem.key === content.key}>{content.tab}</OrderTypeText>
+                {TAB_CONTENTS.ORDER_INFO.map((elem, idx) =>
+                    <li key={elem.key} onClick={() => setCurrentItem(idx)}>
+                        <OrderTypeText $isOn={currentItem?.key === elem.key}>{elem.tab}</OrderTypeText>
                     </li>
                 )}
             </OrderTabWrap>
@@ -25,12 +25,12 @@ function StoreOrderInfo({deliveryInfo, takeoutInfo}) {
                         </li>
                         <li>
                             <Font>{currentItem.content[1]}</Font>
-                            <Font>{deliveryInfo?.deliveryTime}</Font>
+                            <Font>{deliveryInfo?.deliveryTime}분</Font>
 
                         </li>
                         <li>
                             <Font>{currentItem.content[2]}</Font>
-                            <Font>{deliveryInfo?.tip.toLocaleString()}원</Font>
+                            <Font>{deliveryInfo?.tips.toLocaleString()}원</Font>
                         </li>
                     </>
                 }
@@ -38,19 +38,15 @@ function StoreOrderInfo({deliveryInfo, takeoutInfo}) {
                     <>
                         <li>
                             <Font>{currentItem.content[0]}</Font>
-                            <Font>{takeoutInfo?.discount.toLocaleString()}원</Font>
+                            <Font>{takeoutInfo?.minPrice.toLocaleString()}원</Font>
                         </li>
                         <li>
                             <Font>{currentItem.content[1]}</Font>
-                            <Font>{deliveryInfo?.minPrice.toLocaleString()}원</Font>
-                        </li>
-                        <li>
-                            <Font>{currentItem.content[2]}</Font>
                             <Font>{takeoutInfo?.pickUpTime}분</Font>
                         </li>
                         <li>
-                            <Font>{currentItem.content[3]}</Font>
-                            <Font>{takeoutInfo?.location}</Font>
+                            <Font>{currentItem.content[2]}</Font>
+                            <Font>{address}</Font>
                         </li>
                     </>
                 }
