bb29b7f5048d06a85198780ba232cfbe1369a514
SanghyeonLee <hyeon012366@gmail.com>
2025-03-16 20:31:14 +0900

    Refactor: 프롭스 및 변수 이름 수정

--- front-end/src/components/common/Pagination/Pagination.jsx
+++ front-end/src/components/common/Pagination/Pagination.jsx
@@ -9,23 +9,24 @@ import arrowDoubleRight from "../../../assets/img/double_arrow_right.png"
 
 const PAGE_BTN_LIMIT = 5;
 
-function Pagination({currentPage, totalPages, setCurrentPage}) {
-    const startPage = Math.floor((currentPage - 1) / PAGE_BTN_LIMIT) * PAGE_BTN_LIMIT + 1;
+function Pagination({page, totalPages, setPage}) {
+    const startPage = Math.floor((page - 1) / PAGE_BTN_LIMIT) * PAGE_BTN_LIMIT + 1;
     const endPage = Math.min(startPage + PAGE_BTN_LIMIT - 1, totalPages);
-    const pages = Array.from({length: endPage - startPage + 1}, (_, i) => startPage + i);
+    const allPages = Array.from({length: endPage - startPage + 1}, (_, i) => startPage + i);
     return (
         <PaginationWrap>
             <PaginationInner>
-                <IconBtn src={arrowDoubleLeft} isDisable={currentPage === 1} alt={arrowDoubleLeft} width={22}
-                         onClick={() => setCurrentPage(1)}/>
-                <IconBtn src={arrowLeft} isDisable={currentPage === 1} alt={arrowLeft} width={22}
-                         onClick={() => setCurrentPage(currentPage - 1)}/>
-                {pages.map((page) => <PaginationBtn key={page} page={page} clicked={page === currentPage}
-                                                    onClick={() => setCurrentPage(page)}/>)}
-                <IconBtn src={arrowRight} isDisable={currentPage === totalPages} alt={arrowRight} width={22}
-                         onClick={() => setCurrentPage(currentPage + 1)}/>
+                <IconBtn src={arrowDoubleLeft} isDisable={page === 1} alt={arrowDoubleLeft} width={22}
+                         onClick={() => setPage(1)}/>
+                <IconBtn src={arrowLeft} isDisable={page === 1} alt={arrowLeft} width={22}
+                         onClick={() => setPage(page - 1)}/>
+                {allPages.map((seletedpage) => <PaginationBtn key={seletedpage} page={seletedpage}
+                                                              clicked={seletedpage === page}
+                                                              onClick={() => setPage(seletedpage)}/>)}
+                <IconBtn src={arrowRight} isDisable={page === totalPages} alt={arrowRight} width={22}
+                         onClick={() => setPage(page + 1)}/>
                 <IconBtn src={arrowDoubleRight} isDisable={endPage === totalPages} alt={arrowDoubleRight}
-                         onClick={() => setCurrentPage(totalPages)} width={22}/>
+                         onClick={() => setPage(totalPages)} width={22}/>
             </PaginationInner>
         </PaginationWrap>
     );
