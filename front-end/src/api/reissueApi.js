7c8dce260f3158bb1e84a54849955886ba4cf1b0
SanghyeonLee <hyeon012366@gmail.com>
2025-03-19 17:06:07 +0900

    Feat: 리이슈 함수 추가

---
+++ front-end/src/api/reissueApi.js
@@ -0,0 +1,30 @@
+import {authInstance} from "./instance";
+import {postApi} from "./user";
+import {API_URLS} from "../constants/urls";
+
+const reissue = async (originalRequest) => {
+    try {
+        originalRequest._retry = true;
+
+        const reissueResult = await postApi(API_URLS.REISSUE, {
+            refreshToken: localStorage.getItem("refresh-token"),
+        });
+
+        if (reissueResult.status >= 300) {
+            return Promise.reject(reissueResult);
+        }
+
+        const {accessToken, refreshToken} = reissueResult.data;
+        localStorage.setItem("access-token", accessToken);
+        localStorage.setItem("refresh-token", refreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+        return authInstance(originalRequest);
+    } catch (error) {
+        console.error("🚨 리이슈 요청 중 오류 발생:", error);
+        return Promise.reject(error);
+    }
+};
+
+export default reissue;
