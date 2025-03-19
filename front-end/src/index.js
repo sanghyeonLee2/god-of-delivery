1409ab2cd0b37d5dcd00d5acde9fbf443f28ac91
SanghyeonLee <hyeon012366@gmail.com>
2025-03-20 05:10:54 +0900

    Refactor: 라우터 구조 수정

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,26 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import SignUpPage from "./pages/AuthPage/SignUpPage";
-import SignInPage from "./pages/AuthPage/SIgnInPage";
-import HomePage from "./pages/HomePage/HomePage";
-import StoresPage from "./pages/StoresPage/StoresPage";
+import SignUpPage from "pages/common/AuthPage/SignUpPage";
+import SignInPage from "pages/common/AuthPage/SIgnInPage";
+import HomePage from "pages/user/HomePage/HomePage";
+import StoresPage from "pages/user/StoresPage/StoresPage";
 import {QueryClientProvider} from "react-query"
 import {RecoilRoot} from "recoil";
 import Loading from "./components/common/Loading/Loading";
-import StorePage from "./pages/StorePage/StorePage";
-import CartPage from "./pages/CartPage/CartPage";
-import PaymentPage from "./pages/PaymentPage/PaymentPage";
-import OrderStatusPage from "pages/OrderStatusPage/OrderStatusPage";
-import OrdersPage from "pages/ManagementPage/OrdersPage";
-import OwnerPage from "pages/OwnerPage/OwnerPage";
-import StoreManagement from "pages/OwnerPage/components/StoreManagement";
-import MenuManagement from "pages/OwnerPage/components/MenuManagement";
-import ErrorPage from "pages/ErrorPage/ErrorPage";
-import OwnerReviewPage from "pages/OwnerPage/OwnerReviewPage";
-import ReviewManagementPage from "pages/ManagementPage/ReviewManagementPage";
+import StorePage from "pages/user/StorePage/StorePage";
+import CartPage from "pages/user/CartPage/CartPage";
+import PaymentPage from "pages/user/PaymentPage/PaymentPage";
+import OrderStatusPage from "pages/user/OrderStatusPage/OrderStatusPage";
+import OrdersPage from "pages/user/ManagementPage/OrdersPage";
+import OwnerHomePage from "pages/owner/OwnerHomePage/OwnerHomePage";
+import StoreManagementPage from "pages/owner/StoreManagementPage/StoreManagementPage";
+import ErrorPage from "pages/common/ErrorPage/ErrorPage";
+import ReviewManagementPage from "pages/user/ManagementPage/ReviewManagementPage";
 import customQueryClient from "./react-query/queryClient";
 import {ToastContainer} from "react-toastify";
+import DibsPage from "pages/user/DibsPage/DibsPage";
+import MenuCategoryManagementPage from "pages/owner/ManuCategoryManagementPage/MenuCategoryManagementPage";
+import OwnerReviewPage from "pages/owner/OwnerReviewPage/OwnerReviewPage";
+import MenuManagementPage from "pages/owner/MenuManagementPage/MenuManagementPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([{
@@ -43,7 +45,7 @@ const router = createBrowserRouter([{
             element: <StoresPage/>,
         },
         {
-            path: "stores/:categoryId/:storeId",
+            path: "stores/info/:storeId",
             element: <StorePage/>,
         },
         {
@@ -69,21 +71,27 @@ const router = createBrowserRouter([{
         {
             path: "users/me/reviews",
             element: <ReviewManagementPage/>
+        }, {
+            path: "users/me/dibs",
+            element: <DibsPage/>
         },
         {
             path: "owners/me",
-            element: <OwnerPage/>
+            element: <OwnerHomePage/>
         },
         {
             path: "owners/me/store",
-            element: <StoreManagement/>
+            element: <StoreManagementPage/>
         }, {
             path: "owners/me/reviews",
             element: <OwnerReviewPage/>
         },
         {
             path: "owners/me/menus",
-            element: <MenuManagement/>
+            element: <MenuManagementPage/>
+        }, {
+            path: "owners/me/menus/:menuId/options",
+            element: <MenuCategoryManagementPage/>
         },
     ]
 }, {
