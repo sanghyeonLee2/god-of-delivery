84dd27bb77e9ef881b6dd91fb19d20124d5534c4
SanghyeonLee <hyeon012366@gmail.com>
2025-03-19 17:03:16 +0900

    Refactor: 전연 에러 바운더리 추가

--- front-end/src/components/common/ErrorBoundary/GlobalErrorBoundary.js
+++ front-end/src/components/common/ErrorBoundary/GlobalErrorBoundary.js
@@ -1,16 +1,12 @@
 import React, {Suspense} from 'react';
 import {ErrorBoundary} from "react-error-boundary";
-import ErrorFallback from "pages/ErrorPage/ErrorFallback";
-import {useQueryErrorResetBoundary} from "react-query";
+import ErrorFallback from "components/common/ErrorBoundary/ErrorFallback";
 import Loading from "components/common/Loading/Loading";
 
 function GlobalErrorBoundary({children}) {
-    const {reset} = useQueryErrorResetBoundary();
     return (
-        <ErrorBoundary onReset={() => reset} FallbackComponent={ErrorFallback}>
-            <Suspense fallback={<Loading/>}>
-              
-            </Suspense>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <Suspense fallback={<Loading/>}>{children}</Suspense>
         </ErrorBoundary>
     );
 }
