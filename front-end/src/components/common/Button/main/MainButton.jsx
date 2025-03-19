a260708ec59811e0c93d2f25fb7e5f2de30d68eb
SanghyeonLee <hyeon012366@gmail.com>
2025-03-19 17:04:56 +0900

    Feat: 로딩 추가

--- front-end/src/components/common/Button/main/MainButton.jsx
+++ front-end/src/components/common/Button/main/MainButton.jsx
@@ -1,6 +1,7 @@
 import React, {forwardRef} from 'react';
 import {MainButtonWrap, ModalButtonWrap, SubButtonWrap, TransButtonWrap} from "./MainButtonLayout";
 import {Font} from "../../../../assets/styles/CommonStyle";
+import MutateLoading from "components/common/Loading/MutateLoading";
 
 
 export const MainBtn = forwardRef(({type, text, onClick, width}, ref) => {
@@ -11,19 +12,19 @@ export const MainBtn = forwardRef(({type, text, onClick, width}, ref) => {
     );
 });
 
-export const ModalBtn = forwardRef(({type, text, onClick, width, height}, ref) => {
+export const ModalBtn = forwardRef(({type, text, onClick, width, height, isLoading}, ref) => {
     return (
         <ModalButtonWrap width={width} height={height} type={type} onClick={onClick} ref={ref}>
-            <Font color={"white"}>{text}</Font>
+            {isLoading ? <MutateLoading/> : <Font color={"white"}>{text}</Font>}
         </ModalButtonWrap>
     );
 });
 
 
-export function SubBtn({type, text, onClick, height}) {
+export function SubBtn({type, text, onClick, height, isLoading}) {
     return (
-        <SubButtonWrap type={type} onClick={onClick} height={height}>
-            <Font color={"white"}>{text}</Font>
+        <SubButtonWrap type={type} onClick={onClick} $height={height} $isLoading={isLoading}>
+            {isLoading ? <MutateLoading/> : <Font color={"white"}>{text}</Font>}
         </SubButtonWrap>
     );
 }
