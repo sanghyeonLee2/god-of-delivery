1ca8a595630ab335acf74c795d1ca9f6cfa861ca
SanghyeonLee <hyeon012366@gmail.com>
2025-03-21 10:24:52 +0900

    Refactor: 코드 스타일 쉊ㅇ

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,28 +1,31 @@
 import React from "react";
-import {GlobalStyle} from "./assets/styles/GlobalStyle";
-import {Header} from "components/common/Header/Header";
-import {Outlet} from "react-router-dom";
+import { GlobalStyle } from "./assets/styles/GlobalStyle";
+import { Header } from "components/common/Header/Header";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/common/Footer/Footer";
-import {useRecoilValue} from "recoil";
-import {isModalOpenState} from "./recoil/flag/atoms";
+import { useRecoilValue } from "recoil";
+import { isModalOpenState } from "./recoil/flag/atoms";
 import DefaultModal from "./components/modal/DefaultModal";
 import GlobalErrorBoundary from "components/common/ErrorBoundary/GlobalErrorBoundary";
 
-
 function App() {
-    const isModalOpen = useRecoilValue(isModalOpenState)
-    return (
-        <GlobalErrorBoundary>
-            <div className="App">
-                <Header/>
-                <GlobalStyle/>
-                <Outlet/>
-            </div>
-            <Footer/>
-            {isModalOpen.flag &&
-                <DefaultModal modalType={isModalOpen.modalType} modalData={isModalOpen.modalData}/>}
-        </GlobalErrorBoundary>
-    );
+  const isModalOpen = useRecoilValue(isModalOpenState);
+  return (
+    <GlobalErrorBoundary>
+      <div className="App">
+        <Header />
+        <GlobalStyle />
+        <Outlet />
+      </div>
+      <Footer />
+      {isModalOpen.flag && (
+        <DefaultModal
+          modalType={isModalOpen.modalType}
+          modalData={isModalOpen.modalData}
+        />
+      )}
+    </GlobalErrorBoundary>
+  );
 }
 
 export default App;
---
+++ front-end/src/hooks/useReviewForm.js
@@ -0,0 +1,71 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { userInfoState } from "../recoil/user/atoms";
+import { useMutation, useQueryClient } from "react-query";
+import { authDeleteApi, authPatchApi } from "../api/user";
+import { API_URLS } from "../constants/urls";
+import { QUERY_KEYS } from "../constants/queryKeys";
+import { useLocation } from "react-router-dom";
+import { showSuccess } from "../utils/toasts";
+
+export const useReviewForm = (review) => {
+  const queryClient = useQueryClient();
+  const [updateMode, setUpdateMode] = useState(false);
+  const { userId, role } = useRecoilValue(userInfoState);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const page = queryParams.get("page") || 1;
+  const GET_MY_REVIEWS_URL = API_URLS.GET_MY_REVIEWS(page);
+
+  const { handleSubmit, register, watch, getValues, setValue } = useForm();
+
+  const { mutate: deleteReview } = useMutation(
+    () => authDeleteApi(API_URLS.DELETE_MY_REVIEW(review.reviewId)),
+    {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries([
+          GET_MY_REVIEWS_URL,
+          QUERY_KEYS.REVIEWS,
+        ]);
+        showSuccess("리뷰가 삭제되었습니다");
+      },
+    }
+  );
+
+  const { mutate: updateReview } = useMutation(
+    () => authPatchApi(API_URLS.PATCH_MY_REVIEW(review.reviewId), getValues()),
+    {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries([
+          GET_MY_REVIEWS_URL,
+          QUERY_KEYS.REVIEWS,
+        ]);
+        showSuccess("리뷰가 수정되었습니다");
+      },
+    }
+  );
+
+  return {
+    updateMode,
+    isMyReview: userId === review.userId && location.pathname.includes("users"),
+    deleteReview,
+    updateReview,
+    handleRatingChange: (newRating) => {
+      setValue("rating", newRating);
+    },
+    setUpdateMode: () => {
+      setValue("rating", review.rating);
+      setValue("content", review.content);
+      setValue("img", review.img);
+      setUpdateMode(true);
+    },
+    cancelUpdateMode: () => setUpdateMode(false),
+    register,
+    getValues,
+    setValue,
+    watch,
+    isOwner: role === "owner" && location.pathname.includes("owners"),
+    handleSubmit,
+  };
+};
