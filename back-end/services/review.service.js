b203207862b6b1138eb2d9e8e7d906587ebe17ea
Justbeanpole <dbtjd7282@gmail.com>
2025-03-21 10:31:29 +0900

    feat: 사장님 리뷰 조회, 생성, 삭제, 수정

--- back-end/services/review.service.js
+++ back-end/services/review.service.js
@@ -1,6 +1,8 @@
 const {Review, CeoReview} = require('../models');
 const {sequelize} = require('../models');
 const {Op} = require("sequelize");
+const StoreService = require('./store.service')
+
 
 exports.findReviewsByStoreId = async ({storeId}, {page}) => {
     const pageNum = Number(page);
@@ -89,4 +91,49 @@ exports.reviewCntInThreeMonth = async(storeId) => {
         where:{storeId,
         createdAt: {[Op.gte]:threeMonthsAgo}}
     })
+}
+
+exports.findListOwnerReview = async({userId, query}) => {
+    const store = await StoreService.findStoreByUserId(userId)
+    const pageNum = Number(query.page);
+    const data = await Review.findAll({
+        where: {storeId: store.storeId},
+        offset: (pageNum - 1) * 10,
+        limit: 10,
+        include:[{model:CeoReview}]
+    })
+    const countReviews = await Review.findAll({
+        attributes: [
+            [sequelize.fn('COUNT', sequelize.col('rating')), 'count'],
+        ],
+        where:{storeId: store.storeId, rating: [1, 2, 3, 4, 5]},
+        group:['rating'],
+        order:[['rating','ASC']],
+    });
+    const countList = countReviews.map(item => item.dataValues.count)
+    return ({
+        totalItems: data.length,
+        reviewStat: countList,
+        reviewList: data
+    })
+}
+
+exports.createCeoReview = async({userId},{reviewId, content}) => {
+    const newCeoReview = await CeoReview.create({
+        content, userId, reviewId
+    })
+    return (newCeoReview)
+}
+
+exports.updateCeoReview = async ({params, body}) => {
+    const updateCeoReview = await CeoReview.update(body, {where: {reviewId: params.reviewId}})
+    console.log(updateCeoReview)
+    return (updateCeoReview > 0)
+}
+
+exports.deleteCeoReview = async({reviewId}) => {
+    const delCeoReview = await CeoReview.destroy({
+        where: {reviewId},
+    })
+    return (delCeoReview)
 }
