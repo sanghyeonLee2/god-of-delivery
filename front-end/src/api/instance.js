f8979cb8d47eadc77b171ec61893435372ffff11
SanghyeonLee <hyeon012366@gmail.com>
2025-03-21 12:42:19 +0900

    Chore: api url 수정

--- front-end/src/api/instance.js
+++ front-end/src/api/instance.js
@@ -1,48 +1,40 @@
 import axios from "axios";
 import reissue from "./reissueApi";
 
-//const API_KEY = process.env.REACT_APP_API
-const API_KEY = process.env.REACT_APP_JSON_SERVER// 테스트 용
+const API_KEY = process.env.REACT_APP_API;
+//const API_KEY = process.env.REACT_APP_JSON_SERVER// 테스트 용
 
 export const instance = axios.create({
-    baseURL: API_KEY,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: API_KEY,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const authInstance = axios.create({
-    baseURL: API_KEY,
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("access-token")}`,
-    },
+  baseURL: API_KEY,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+  },
 });
 
 authInstance.interceptors.response.use(
-    (response) => response,
-    (err) => {
-        const originalRequest = err.config;
+  (response) => response,
+  (err) => {
+    const originalRequest = err.config;
 
-        if (!err.response) {
-            return Promise.reject(err);
-        }
-
-        if (originalRequest._retry) {
-            return Promise.reject(err);
-        }
-        if (err.response.status === 401) {
-            return reissue(originalRequest);
-        }
-
-        return Promise.reject(err);
+    if (!err.response) {
+      return Promise.reject(err);
     }
+
+    if (originalRequest._retry) {
+      return Promise.reject(err);
+    }
+    if (err.response.status === 401) {
+      return reissue(originalRequest);
+    }
+
+    return Promise.reject(err);
+  }
 );
-
-
-
-
-
-
-
-
