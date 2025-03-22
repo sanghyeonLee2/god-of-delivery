06538154f197484d6ea75f4dc3afda82ec77a595
SanghyeonLee <hyeon012366@gmail.com>
2025-03-22 23:58:40 +0900

    style: 구조 수정

--- front-end/src/hooks/useSelectMapLocation.js
+++ front-end/src/hooks/useSelectMapLocation.js
@@ -1,26 +1,22 @@
-import {useQuery} from "react-query";
-import {QUERY_KEYS} from "../constants/queryKeys";
-import {API_URLS} from "../constants/urls";
-import {authGetApi} from "../api/user";
-import {useRecoilState} from "recoil";
-import {coordsState} from "../recoil/map/atoms";
+import { useQuery } from "react-query";
+import { QUERY_KEYS } from "../constants/queryKeys";
+import { API_URLS } from "../constants/urls";
+import { authGetApi } from "../api/user";
+import { useRecoilState } from "recoil";
+import { coordsState } from "../recoil/map/atoms";
 
 export const useSelectMapLocation = () => {
-    const [coords, setCoords] = useRecoilState(coordsState)
+  const [coords, setCoords] = useRecoilState(coordsState);
 
-    const onClickCurrentLocation = () => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            setCoords({lat: position.coords.latitude, lng: position.coords.longitude})
-        })
-    }
+  const onClickCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCoords({ lat: position.coords.latitude, lng: position.coords.longitude });
+    });
+  };
 
-    const {isLoading} = useQuery(
-        [QUERY_KEYS.COORDS, API_URLS.GET_COORDS],
-        () => authGetApi(API_URLS.GET_COORDS),
-        {
-            staleTime: 1000 * 60 * 5, // 5분 동안 신선한 데이터 유지
-            cacheTime: 1000 * 60 * 10,
-        }
-    );
-    return {isLoading, coords, onClickCurrentLocation, setCoords};
-}
+  const { isLoading } = useQuery([QUERY_KEYS.LOCATION], () => authGetApi(API_URLS.USER.LOCATION), {
+    staleTime: 1000 * 60 * 5, // 5분 동안 신선한 데이터 유지
+    cacheTime: 1000 * 60 * 10,
+  });
+  return { isLoading, coords, onClickCurrentLocation, setCoords };
+};
