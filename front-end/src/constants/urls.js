0bbe07f6233c6eac1c3468c31703a0cdb6c144c5
SanghyeonLee <hyeon012366@gmail.com>
2025-03-24 18:02:55 +0900

    refactor: url 수정

--- front-end/src/constants/urls.js
+++ front-end/src/constants/urls.js
@@ -1,49 +1,58 @@
-import {keywordIncludedUrl} from "../utils/transducer";
-
 export const API_URLS = {
-    GET_CART: "cart",
+  AUTH: {
     SIGN_IN: "auth/sign-in",
     SIGN_UP: "auth/sign-up",
     REISSUE: "auth/reissue",
-    POST_DIB:  `dibs`,
-    DELETE_DIB: (storeId)=> `dibs/${storeId}`,
-    GET_DIBS: (page) => `users/me/dibs?page=${page}`,
-    GET_ME: "users/me",
-    POST_ADDRESS: "users/me/address",
-    GET_COORDS: "users/me/location",
-    PATCH_OWNER_REVIEW: (reviewId) => `owners/me/reviews/${reviewId}`,
-    PATCH_MY_REVIEW: (reviewId) => `reviews/${reviewId}`,
-    DELETE_MY_REVIEW: (reviewId) => `reviews/${reviewId}`,
-    DELETE_OWNER_REVIEW: (reviewId) => `owners/me/reviews/${reviewId}`,
-    GET_MY_REVIEWS: (page) => `users/me/reviews?page=${page}`,
-    GET_STORE_REVIEWS: (storeId, page) => `stores/${storeId}/reviews?page=${page}`,
-    GET_OWNER_REVIEWS: (page) => `owners/me/reviews?page=${page}`,
-    GET_OWNER_MENUS: `owners/me/menus`,
-    GET_OWNER_MENUS_CATEGORY: (menuId) => `owners/me/menus/${menuId}/category`,
-    PUT_OWNER_MENUS_CATEGORY: (menuId) => `owners/me/menus/${menuId}/category`,
-    DELETE_OWNER_MENUS_CATEGORY: (menuId) => `owners/me/menus/${menuId}/category`,
-    GET_OWNER_MENUS_OPTION: (menuId, categoryId) => `owners/me/menus/${menuId}/categories/${categoryId}/option`,
-    PUT_OWNER_MENUS_OPTION: (menuId, categoryId) => `owners/me/menus/${menuId}/categories/${categoryId}/option`,
-    DELETE_OWNER_MENUS_OPTION: (menuId, categoryId) => `owners/me/menus/${menuId}/categories/${categoryId}/option`,
-    POST_OWNER_MENUS_CATEGORY: (menuId, categoryId) => `owners/me/menus/${menuId}/categories/${categoryId}/option`,
-    PUT_OWNER_MENUS: (menuId) => `owners/me/menus/${menuId}`,
-    DELETE_OWNER_MENUS: (menuId) => `owners/me/menus/${menuId}`,
-    POST_OWNER_REVIEW: `owners/me/reviews`,
-    GET_STORE: (storeId) => `stores/info/${storeId}`,
-    POST_ORDERS: "orders",
-    POST_REVIEW: "reviews",
-    PUT_CART: (cartItemId) => `cart/${cartItemId}`,
-    GET_CART_DETAIL: (menuId) => `cart/${menuId}`,
-    GET_MENUS: (menuId) => `menus/${menuId}`,
-    DELETE_CART: (cartItemId) => `cart/item/${cartItemId}`,
-    GET_ORDER: (orderId) => `orders/${orderId}`,
-    GET_ORDERS: (page) => `users/me/orders/?page=${page}`,
-    GET_OWNER_STORE: "owners/me/stores",
-    PUT_OWNER_STORE: "owners/me/stores",
-    GET_STORES: ({
-                     categoryId,
-                     page,
-                     sorting,
-                     keyword
-                 }) => `stores/${categoryId}?page=${page}&sorting=${sorting}${keywordIncludedUrl(keyword)}`, //
+  },
+
+  USER: {
+    ME: "users/me",
+    DIBS: "users/me/dibs",
+    ADDRESS: "users/me/address",
+    LOCATION: "users/me/location",
+    REVIEWS: "users/me/reviews",
+    ORDERS: "users/me/orders",
+  },
+
+  STORE: {
+    LIST: "stores",
+    DETAIL: (storeId) => `stores/info/${storeId}`,
+    REVIEWS: (storeId) => `stores/${storeId}/reviews`,
+    OWNER_STORE: "owners/me/stores",
+  },
+
+  CART: {
+    BASE: "cart",
+    DETAIL: (menuId) => `cart/${menuId}`,
+    ITEM: (cartItemId) => `cart/item/${cartItemId}`,
+    PUT_ITEM: (cartItemId) => `cart/${cartItemId}`,
+  },
+
+  MENU: {
+    GET: (menuId) => `menus/${menuId}`,
+    OWNER: {
+      MENU: (menuId) => `owners/me/menus/${menuId}`,
+      CATEGORY: (menuId) => `owners/me/menus/${menuId}/category`,
+      OPTION: (menuId, categoryId) => `owners/me/menus/${menuId}/categories/${categoryId}/option`,
+    },
+  },
+
+  REVIEW: {
+    MY: (reviewId) => `reviews/${reviewId}`,
+    POST: "reviews",
+    OWNER: {
+      BASE: "owners/me/reviews",
+      ITEM: (reviewId) => `owners/me/reviews/${reviewId}`,
+    },
+  },
+
+  ORDER: {
+    BASE: "orders",
+    DETAIL: (orderId) => `orders/${orderId}`,
+  },
+
+  DIB: {
+    POST: "dibs",
+    DELETE: (storeId) => `dibs/${storeId}`,
+  },
 };
