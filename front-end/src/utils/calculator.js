b126817c8e5968f29befc9df77a36184d9abb86f
SanghyeonLee <hyeon012366@gmail.com>
2025-03-27 23:40:50 +0900

    feat: 총합 금액 계산 로직 추가

--- front-end/src/utils/calculator.js
+++ front-end/src/utils/calculator.js
@@ -1,5 +1,13 @@
 const PAGE_SIZE = 10;
 
-export const pageCalculator = (totalItems) => {
-  return Math.ceil(totalItems / PAGE_SIZE);
-};
+export const pageCalculator = (totalItems) => Math.ceil(totalItems / PAGE_SIZE);
+
+export const cartTotalPrice = (cartData) =>
+  cartData?.CartItems.reduce(
+    (acc, cartItem) =>
+      cartItem.quantity *
+      (acc +
+        cartItem.Menu.price +
+        cartItem.CartItemOptions.reduce((acc, { price }) => acc + price, 0)),
+    cartData.Store.deliveryTip
+  );
