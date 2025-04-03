b73c2b56bf51982541794bda93451b28abc3f860
SanghyeonLee <hyeon012366@gmail.com>
2025-04-04 02:22:54 +0900

    refactor: guard 위치 수정

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,32 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SignUpPage from "pages/common/AuthPage/SignUpPage";
-import SignInPage from "pages/common/AuthPage/SignInPage";
-import HomePage from "pages/user/HomePage/HomePage";
-import StoresPage from "pages/user/StoresPage/StoresPage";
+import SignUpPage from "@pages/common/AuthPage/SignUpPage";
+import SignInPage from "@pages/common/AuthPage/SignInPage";
+import HomePage from "@pages/user/HomePage/HomePage";
+import StoresPage from "@pages/user/StoresPage/StoresPage";
 import { QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
-import StorePage from "pages/user/StorePage/StorePage";
-import CartPage from "pages/user/CartPage/CartPage";
-import PaymentPage from "pages/user/PaymentPage/PaymentPage";
-import OrderStatusPage from "pages/user/OrderStatusPage/OrderStatusPage";
-import MyOrdersPage from "pages/user/ManagementPages/MyOrdersPage";
-import OwnerHomePage from "pages/owner/OwnerHomePage/OwnerHomePage";
-import StoreManagementPage from "pages/owner/StoreManagementPage/StoreManagementPage";
-import ErrorPage from "pages/common/ErrorPage/ErrorPage";
-import MyReviewsPage from "pages/user/ManagementPages/MyReviewsPage";
+import StorePage from "@pages/user/StorePage/StorePage";
+import CartPage from "@pages/user/CartPage/CartPage";
+import PaymentPage from "@pages/user/PaymentPage/PaymentPage";
+import OrderStatusPage from "@pages/user/OrderStatusPage/OrderStatusPage";
+import MyOrdersPage from "@pages/user/ManagementPages/MyOrdersPage";
+import OwnerHomePage from "@pages/owner/OwnerHomePage/OwnerHomePage";
+import StoreManagementPage from "@pages/owner/StoreManagementPage/StoreManagementPage";
+import ErrorPage from "@pages/common/ErrorPage/ErrorPage";
+import MyReviewsPage from "@pages/user/ManagementPages/MyReviewsPage";
 import customQueryClient from "./react-query/queryClient";
 import { ToastContainer } from "react-toastify";
-import DibsPage from "pages/user/DibsPage/DibsPage";
-import OwnerReviewPage from "pages/owner/OwnerReviewPage/OwnerReviewPage";
-import MenuManagementPage from "pages/owner/MenuManagementPages/MenuManagementPage";
-import CreateMenuPage from "pages/owner/MenuManagementPages/CreateMenuPage";
-import UpdateMenuPage from "pages/owner/MenuManagementPages/UpdateMenuPage";
-import { GlobalStyle } from "./assets/styles/GlobalStyle";
-import RequireRole from "components/guards/RequireRole";
-import OwnerLayout from "pages/owner/Common/OwnerLayout";
-import UserLayout from "pages/user/UserLayout";
+import DibsPage from "@pages/user/DibsPage/DibsPage";
+import OwnerReviewPage from "@pages/owner/OwnerReviewPage/OwnerReviewPage";
+import MenuManagementPage from "@pages/owner/MenuManagementPages/MenuManagementPage";
+import CreateMenuPage from "@pages/owner/MenuManagementPages/CreateMenuPage";
+import UpdateMenuPage from "@pages/owner/MenuManagementPages/UpdateMenuPage";
+import { GlobalStyle } from "@assets/styles/GlobalStyle";
+import RequireRole from "@components/guards/RequireRole";
+import OwnerLayout from "@pages/owner/OwnerLayout";
+import UserLayout from "@pages/user/UserLayout";
+import AuthPageLayout from "@pages/common/AuthPage/AuthPageLayout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createBrowserRouter([
@@ -35,18 +36,15 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { path: "sign-in", element: <SignInPage /> },
-      { path: "sign-up", element: <SignUpPage /> },
-      // ✅ User Role Protected
       {
-        path: "users",
+        path: "",
         element: <RequireRole allowedRole="user" />,
         children: [
           {
-            path: "me",
-            element: <UserLayout />, // 📦 사용자 공통 레이아웃 (선택)
+            path: "",
+            element: <UserLayout />,
             children: [
-              { path: "", element: <HomePage /> },
+              { index: true, element: <HomePage /> },
               { path: "stores/:categoryId", element: <StoresPage /> },
               { path: "stores/:categoryId/:storeId", element: <StorePage /> },
               { path: "cart", element: <CartPage /> },
@@ -60,12 +58,20 @@ const router = createBrowserRouter([
         ],
       },
       {
+        path: "auth",
+        element: <AuthPageLayout />,
+        children: [
+          { path: "sign-in", element: <SignInPage /> },
+          { path: "sign-up", element: <SignUpPage /> },
+        ],
+      },
+      {
         path: "owners",
-        element: <RequireRole allowedRole="owner" />, // 🔐 보호 레이어
+        element: <RequireRole allowedRole="owner" />,
         children: [
           {
             path: "me",
-            element: <OwnerLayout />, // 📦 공통 레이아웃
+            element: <OwnerLayout />,
             children: [
               { path: "", element: <OwnerHomePage /> },
               { path: "store", element: <StoreManagementPage /> },
@@ -79,7 +85,6 @@ const router = createBrowserRouter([
       },
     ],
   },
-
   {
     path: "*",
     element: <ErrorPage />,
