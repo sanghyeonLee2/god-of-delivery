d0345d6aa8b0f748af5ab89919a6b2218a674d3f
SanghyeonLee <hyeon012366@gmail.com>
2025-04-04 02:39:02 +0900

    chore: 상수 데이터 디렉토리 구조 수정

---
+++ front-end/src/constants/dummyData.js
@@ -0,0 +1,142 @@
+export const HOME_MENUS = [
+  {
+    id: "all",
+    name: "전체보기",
+    url: "https://img.icons8.com/color/48/select-all.png",
+    alt: "select-all",
+  },
+  {
+    id: "fastFood",
+    name: "패스트푸드",
+    url: "https://img.icons8.com/arcade/64/fast-food-drive-thru.png",
+    alt: "fast-food-drive-thru",
+  },
+  {
+    id: "chicken",
+    name: "치킨",
+    url: "https://img.icons8.com/external-others-zufarizal-robiyanto/64/external-fast-fast-food-filled-others-zufarizal-robiyanto-11.png",
+    alt: "external-fast-fast-food-filled-others-zufarizal-robiyanto-11",
+  },
+  {
+    id: "western",
+    name: "피자/양식",
+    url: "https://img.icons8.com/papercut/60/pizza.png",
+    alt: "pizza",
+  },
+  {
+    id: "soup",
+    name: "찜/탕",
+    url: "https://img.icons8.com/external-flat-icon-mangsaabguru-/64/external-chinese-chinese-foods-flat-flat-icon-mangsaabguru-.png",
+    alt: "external-chinese-chinese-foods-flat-flat-icon-mangsaabguru-",
+  },
+  {
+    id: "chinese",
+    name: "중국집",
+    url: "https://img.icons8.com/external-konkapp-flat-konkapp/64/external-noodle-seafood-konkapp-flat-konkapp.png",
+    alt: "external-noodle-seafood-konkapp-flat-konkapp",
+  },
+  {
+    id: "korean",
+    name: "한식",
+    url: "https://img.icons8.com/external-justicon-flat-justicon/64/external-bibimbap-korea-justicon-flat-justicon.png",
+    alt: "external-bibimbap-korea-justicon-flat-justicon",
+  },
+  {
+    id: "japaneseAndCutlet",
+    name: "일식/돈까스",
+    url: "https://img.icons8.com/external-nawicon-flat-nawicon/64/external-sushi-fast-food-nawicon-flat-nawicon.png",
+    alt: "external-sushi-fast-food-nawicon-flat-nawicon",
+  },
+  {
+    id: "jokbalAndBossam",
+    name: "족발/보쌈",
+    url: "https://img.icons8.com/officel/80/rack-of-lamb.png",
+    alt: "rack-of-lamb",
+  },
+  {
+    id: "midnight",
+    name: "야식",
+    url: "https://img.icons8.com/external-bearicons-flat-bearicons/64/external-night-essential-collection-bearicons-flat-bearicons.png",
+    alt: "external-night-essential-collection-bearicons-flat-bearicons",
+  },
+  {
+    id: "cafeAndDessert",
+    name: "카페/디저트",
+    url: "https://img.icons8.com/external-justicon-flat-justicon/64/external-coffee-coffee-shop-justicon-flat-justicon-2.png",
+    alt: "external-coffee-coffee-shop-justicon-flat-justicon-2",
+  },
+  {
+    id: "convenienceStoreAndMart",
+    name: "편의점/마트",
+    url: "https://img.icons8.com/3d-fluency/94/shop.png",
+    alt: "shop",
+  },
+];
+export const STORE_SORTING = [
+  {
+    id: "기본 정렬순",
+    value: "basic",
+  },
+  {
+    id: "별점 순",
+    value: "ratingDesc",
+  },
+  {
+    id: "리뷰 많은 순",
+    value: "reviewCntDesc",
+  },
+  {
+    id: "최소 주문 금액 순",
+    value: "minDeliveryPriceAsc",
+  },
+];
+export const CATEGORIES = [
+  {
+    id: "all",
+    name: "전체보기",
+  },
+  {
+    id: "fastFood",
+    name: "패스트푸드",
+  },
+  {
+    id: "chicken",
+    name: "치킨",
+  },
+  {
+    id: "western",
+    name: "피자/양식",
+  },
+  {
+    id: "soup",
+    name: "찜/탕",
+  },
+  {
+    id: "chinese",
+    name: "중국집",
+  },
+  {
+    id: "korean",
+    name: "한식",
+  },
+  {
+    id: "japaneseAndCutlet",
+    name: "일식/돈까스",
+  },
+  {
+    id: "jokbalAndBossam",
+    name: "족발/보쌈",
+  },
+  {
+    id: "midnight",
+    name: "야식",
+  },
+  {
+    id: "cafeAndDessert",
+    name: "카페/디저트",
+  },
+  {
+    id: "convenienceStoreAndMart",
+    name: "편의점/마트",
+  },
+];
