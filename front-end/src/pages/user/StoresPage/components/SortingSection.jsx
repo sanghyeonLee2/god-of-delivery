0125dab0f3d73a046dc3dd7b69ca65844047cecd
SanghyeonLee <hyeon012366@gmail.com>
2025-04-04 02:41:02 +0900

    refactor: 상수 디렉토리 수정

--- front-end/src/pages/user/StoresPage/components/SortingSection.jsx
+++ front-end/src/pages/user/StoresPage/components/SortingSection.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import * as S from "./SortingSection.styles";
 import Select from "@components/common/Select/Select";
 import Option from "@components/common/Select/components/Option";
-import categoryDummy from "@assets/data/categoryDummy.json";
-import storeSortingDummy from "@assets/data/storeSorting.json";
+import { CATEGORIES, STORE_SORTING } from "@constants/dummyData";
 
 function SortingSection({ categoryId, setCategory, setSorting, sorting }) {
   return (
     <S.SortingOuter>
       <S.SortingInner>
         <Select defaultValue={sorting} onChange={(e) => setSorting(e.target.value)}>
-          {storeSortingDummy.map((e) => (
+          {STORE_SORTING.map((e) => (
             <Option key={e.id} text={e.id} value={e.value} />
           ))}
         </Select>
         <Select defaultValue={categoryId} onChange={(e) => setCategory(e.target.value)}>
-          {categoryDummy.map((category) => (
+          {CATEGORIES.map((category) => (
             <Option key={category.id} text={category.name} value={category.id} />
           ))}
         </Select>
