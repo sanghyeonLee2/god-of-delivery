2dfd12570c3842672ebb51f4d26fb8e4df71c3a7
SanghyeonLee <hyeon012366@gmail.com>
2025-04-04 05:36:25 +0900

    refactor: 임포트 경로 단순화

--- front-end/src/pages/owner/common/OwnerPageHeader.jsx
+++ front-end/src/pages/owner/common/OwnerPageHeader.jsx
@@ -3,12 +3,10 @@ import Logo from "@components/common/Logo/Logo";
 import { HeaderInner, HeaderOuter } from "@assets/styles/CommonStyle";
 import IconBtn from "@components/common/Button/icon/IconBtn";
 import { CiLogout } from "react-icons/ci";
-import useAuth from "@hooks/useAuth";
 import useSignOut from "@hooks/useSignOut";
 
 function OwnerPageHeader() {
   const handleSignOut = useSignOut();
-  useAuth();
   return (
     <HeaderOuter>
       <HeaderInner>
