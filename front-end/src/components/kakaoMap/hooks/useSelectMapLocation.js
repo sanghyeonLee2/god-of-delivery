64ea11af5e947bb38865678689152476af2f4657
SanghyeonLee <hyeon012366@gmail.com>
2025-04-06 03:51:15 +0900

    fix: 초기 위치 상태 값 저장

--- front-end/src/components/kakaoMap/hooks/useSelectMapLocation.js
+++ front-end/src/components/kakaoMap/hooks/useSelectMapLocation.js
@@ -4,16 +4,25 @@ import { API_URLS } from "@constants/urls";
 import { authGetApi } from "@api/request";
 import { useRecoilState } from "recoil";
 import { coordsState } from "@recoil/map/atoms";
+import { useCallback } from "react";
 
 export const useSelectMapLocation = () => {
   const [coords, setCoords] = useRecoilState(coordsState);
 
-  const onClickCurrentLocation = () => {
+  const onClickCurrentLocation = useCallback(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      setCoords({ lat: position.coords.latitude, lng: position.coords.longitude });
+      setCoords({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
     });
-  };
+  }, [setCoords]);
 
-  const { isLoading } = useQuery([QUERY_KEYS.LOCATION], () => authGetApi(API_URLS.USER.LOCATION));
+  const { isLoading } = useQuery([QUERY_KEYS.LOCATION], () => authGetApi(API_URLS.USER.LOCATION), {
+    onSuccess: (res) => {
+      const { lat, lng } = res.data;
+      setCoords({ lat, lng });
+    },
+  });
   return { isLoading, coords, onClickCurrentLocation, setCoords };
 };
