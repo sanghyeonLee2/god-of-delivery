e9b978111c2843f1eeab3e7a04c83218d1a964f3
SanghyeonLee <hyeon012366@gmail.com>
2025-04-06 03:53:23 +0900

    refactor: 한번 선언 해도 괜찮은 변수 ref 에 상태로 할당 및 useEffect 사용

--- front-end/src/components/modal/SelectMap/hooks/useSearchLocation.js
+++ front-end/src/components/modal/SelectMap/hooks/useSearchLocation.js
@@ -1,29 +1,33 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSetRecoilState } from "recoil";
 import { isSearchLoadingState } from "@recoil/flag/atoms";
 
 const useSearchLocation = () => {
-  const [locationInfo, setLocationInfo] = useState({ status: "", locations: [] });
+  const [locations, setLocations] = useState([]);
   const setIsLoading = useSetRecoilState(isSearchLoadingState);
   const { register, watch } = useForm();
+  const psRef = useRef(null);
 
   const searchKeyword = watch("searchKeyword");
+
+  useEffect(() => {
+    psRef.current = new window.kakao.maps.services.Places();
+  }, []);
 
   const searchLocation = useCallback(
     (keyword) => {
       if (!keyword) {
-        setLocationInfo({ status: "NO_KEYWORD", locations: [] });
+        setLocations([]);
         return;
       }
 
       setIsLoading(true);
-      const ps = new window.kakao.maps.services.Places();
-      ps.keywordSearch(keyword, (data, status) => {
+      psRef.current?.keywordSearch(keyword, (data, status) => {
         if (status === window.kakao.maps.services.Status.OK) {
-          setLocationInfo({ status: "OK", locations: data });
+          setLocations(data);
         } else {
-          setLocationInfo({ status: "ZERO_RESULT", locations: data });
+          setLocations([]);
         }
         setIsLoading(false);
       });
@@ -40,8 +44,8 @@ const useSearchLocation = () => {
   }, [searchKeyword, searchLocation]);
 
   return {
-    locationInfo,
-    setLocationInfo,
+    locations,
+    setLocations,
     register,
     isTyped: searchKeyword !== "",
   };
