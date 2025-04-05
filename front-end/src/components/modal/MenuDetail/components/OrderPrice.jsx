6d4c1809d39041d452f6b4e67e2a4a3d8f602ddc
SanghyeonLee <hyeon012366@gmail.com>
2025-04-06 04:03:51 +0900

    feat: useMemo 성능 최적화

--- front-end/src/components/modal/MenuDetail/components/OrderPrice.jsx
+++ front-end/src/components/modal/MenuDetail/components/OrderPrice.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useWatch } from "react-hook-form";
 import { Font } from "@assets/styles/CommonStyle";
 import { OrderPriceWrap } from "./ModalComponents.styles";
@@ -9,12 +9,14 @@ function OrderPrice({ control, menuCategories, defaultPrice = 0 }) {
   const { quantity = 1, options = {} } = useWatch({ control });
   const selectedOptionIds = extractSelectedOptionIds(options);
 
-  const orderPrice = menuDetailTotalPrice({
-    quantity,
-    defaultPrice,
-    menuCategories,
-    selectedOptionIds,
-  });
+  const orderPrice = useMemo(() => {
+    return menuDetailTotalPrice({
+      quantity,
+      defaultPrice,
+      menuCategories,
+      selectedOptionIds,
+    });
+  }, [quantity, defaultPrice, menuCategories, selectedOptionIds]);
 
   return (
     <OrderPriceWrap>
