ca7400c121a1e58462ec45c3af69e82ed9307bc6
SanghyeonLee <hyeon012366@gmail.com>
2025-04-06 04:07:27 +0900

    fix: 아톰 초기 객체 속성 수정

--- front-end/src/recoil/flag/atoms.js
+++ front-end/src/recoil/flag/atoms.js
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 
 export const isModalOpenState = atom({
   key: "isModalOpen",
-  default: { modalType: "", flag: false, modalData: null },
+  default: { modalType: "", flag: false, modalData: {} },
 });
 
 export const isSearchLoadingState = atom({
