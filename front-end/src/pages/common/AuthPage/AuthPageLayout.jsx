92bfe385e9923db89019637bd9acd9c6ee80e8c2
SanghyeonLee <hyeon012366@gmail.com>
2025-04-08 00:48:33 +0900

    fix: .gitignore 수정

--- front-end/src/pages/common/AuthPage/AuthPageLayout.jsx
+++ front-end/src/pages/common/AuthPage/AuthPageLayout.jsx
@@ -3,11 +3,8 @@ import { Outlet } from "react-router-dom";
 import Footer from "@components/common/Footer/Footer";
 import { Main } from "@assets/styles/CommonStyle";
 import AuthPageHeader from "@pages/common/AuthPage/components/AuthPageHeader";
-import useAuth from "@hooks/useAuth";
 
 function AuthPageLayout() {
-  useAuth();
-
   return (
     <>
       <AuthPageHeader />
