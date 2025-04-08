434bc0ef26cd07b26109ccd0575586d8dee27039
SanghyeonLee <hyeon012366@gmail.com>
2025-04-08 23:58:43 +0900

    fix: 에러 바운더리 위치 수정

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { isModalOpenState } from "@recoil/flag/atoms";
-import GlobalErrorBoundary from "@components/common/ErrorBoundary/GlobalErrorBoundary";
 import ModalLayout from "@components/modal/ModalLayout";
 
 function App() {
   const isModalOpen = useRecoilValue(isModalOpenState);
   return (
-    <GlobalErrorBoundary>
+    <>
       <Outlet />
       {isModalOpen.flag && (
         <ModalLayout modalType={isModalOpen.modalType} modalData={isModalOpen.modalData} />
       )}
-    </GlobalErrorBoundary>
+    </>
   );
 }
 
