fa9a35862204064ce069eb3857b3faa3887f0997
SanghyeonLee <hyeon012366@gmail.com>
2025-04-25 20:20:03 +0900

    refactor: 구조 수정

--- front-end/src/pages/user/OrderStatusPage/OrderStatusPage.jsx
+++ front-end/src/pages/user/OrderStatusPage/OrderStatusPage.jsx
@@ -8,11 +8,11 @@ import {
 } from "@assets/styles/CommonStyle";
 import Loading from "@components/common/Loading/Loading";
 import { useGetOrder } from "./hooks/useGetOrder";
-import OrderMenusDetail from "@pages/user/OrderStatusPage/components/OrderMenusDetail";
-import OrderAmount from "@pages/user/OrderStatusPage/components/OrderAmount";
 import UserPaymentInfo from "@pages/user/OrderStatusPage/components/UserPaymentInfo";
 import { SubBtn } from "@components/common/Button/main/MainButtons";
 import { detailDate } from "@utils/transducer";
+import OrderMenusDetail from "@pages/user/OrderStatusPage/components/OrderMenusDetail";
+import OrderAmount from "@pages/user/OrderStatusPage/components/OrderAmount";
 
 function OrderStatusPage() {
   const { orderData, navigateOrders, isLoading } = useGetOrder();
