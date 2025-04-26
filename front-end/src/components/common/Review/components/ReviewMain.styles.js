04e06b557fbe457df3f5a9e2e47fbb0874e88f24
SanghyeonLee <hyeon012366@gmail.com>
2025-04-26 14:41:13 +0900

    design: 리뷰 최소 높이 증가

--- front-end/src/components/common/Review/components/ReviewMain.styles.js
+++ front-end/src/components/common/Review/components/ReviewMain.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ReviewMainWrap = styled.div`
   margin: 1rem 0;
-  min-height: 15rem;
+  min-height: 20rem;
 `;
 export const ReviewContentWrap = styled.div`
   padding: 1.2rem 0;
