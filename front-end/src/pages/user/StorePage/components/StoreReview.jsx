882ef7253152a6da795e502488579c92dea5c06c
SanghyeonLee <hyeon012366@gmail.com>
2025-04-26 14:43:04 +0900

    fix: 불필요 파일 제거에 따른 코드 삭제

--- front-end/src/pages/user/StorePage/components/StoreReview.jsx
+++ front-end/src/pages/user/StorePage/components/StoreReview.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { VerticalSpace } from "@assets/styles/CommonStyle";
 import Loading from "@components/common/Loading/Loading";
 import Pagination from "@components/common/Pagination/Pagination";
 import Review from "@components/common/Review/Review";
-import StoreReviewHeader from "@pages/user/StorePage/components/StoreReviewHeader";
 import Empty from "@components/common/Empty/Empty";
 import { TabWrap } from "@pages/user/StorePage/StorePage.styles";
 import useGetStoreReviews from "@pages/user/StorePage/hooks/useGetStoreReviews";
+import Title from "@components/common/Title/Title";
 
-function StoreReview({ rating }) {
-  const { reviews, totalPages, isLoading, page, reviewStat, setPage } = useGetStoreReviews();
+function StoreReview() {
+  const { reviews, totalPages, isLoading, page, setPage } = useGetStoreReviews();
   if (isLoading) {
     return <Loading />;
   }
@@ -17,9 +16,8 @@ function StoreReview({ rating }) {
     <>
       {reviews.length > 0 ? (
         <>
-          <StoreReviewHeader reviewStat={reviewStat} rating={rating} />
-          <VerticalSpace />
           <TabWrap>
+            <Title size={"x-large"} text={"가게 리뷰"} />
             {reviews.map((review) => (
               <Review review={review} key={review.reviewId} />
             ))}
