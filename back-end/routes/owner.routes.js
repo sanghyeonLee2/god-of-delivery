83af151c8c390d8d46921f340915ae6e7ca37dad
SanghyeonLee <hyeon012366@gmail.com>
2025-04-29 17:16:32 +0900

    refactor: 불필요한 코드 수정 및 재배치

--- back-end/routes/owner.routes.js
+++ back-end/routes/owner.routes.js
@@ -4,34 +4,33 @@ const {
   getOwnerStore,
   postOwnerReview,
   getOwnerReview,
-  patchOwnerReview,
   deleteOwnerReview,
   getOwnerStoreMenus,
   postMenu,
   putMenu,
   deleteMenu,
-  getOwnerMenu
+  getOwnerMenu,
+  patchOwnerStore,
 } = require("../controllers/owner.controller");
 
 const router = express.Router();
 
 router.get("/me/store", [middleVerifyTokenIsOwner], getOwnerStore);
+router.patch("/me/store", [middleVerifyTokenIsOwner], patchOwnerStore);
+
 router.get("/me/reviews", [middleVerifyTokenIsOwner], getOwnerReview);
 router.get("/me/menus", [middleVerifyTokenIsOwner], getOwnerStoreMenus);
 router.post("/me/reviews", [middleVerifyTokenIsOwner], postOwnerReview);
-router.patch(
-  "/me/reviews/:reviewId",
-  [middleVerifyTokenIsOwner],
-  patchOwnerReview,
-);
+
 router.delete(
   "/me/reviews/:reviewId",
   [middleVerifyTokenIsOwner],
   deleteOwnerReview,
 );
+
 router.get("/me/menus/:menuId", [middleVerifyTokenIsOwner], getOwnerMenu);
-router.post('/me/menus', [middleVerifyTokenIsOwner], postMenu);
-router.put('/me/menus/:menuId', [middleVerifyTokenIsOwner], putMenu);
-router.delete('/me/menus/:menuId', [middleVerifyTokenIsOwner], deleteMenu);
+router.post("/me/menus", [middleVerifyTokenIsOwner], postMenu);
+router.put("/me/menus/:menuId", [middleVerifyTokenIsOwner], putMenu);
+router.delete("/me/menus/:menuId", [middleVerifyTokenIsOwner], deleteMenu);
 
 module.exports = router;
